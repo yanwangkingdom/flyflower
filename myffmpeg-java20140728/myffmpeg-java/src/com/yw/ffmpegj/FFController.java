d7f75f5f662f1cf395d8e35df9d78b34a045b6d8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-11 20:11:00 +0800

    Create FFController.java

---
+++ myffmpeg-java20140728/myffmpeg-java/src/com/yw/ffmpegj/FFController.java
@@ -0,0 +1,14 @@
+package com.yw.ffmpegj;
+
+public interface FFController {
+	
+	public void play();
+		
+    public void stop();
+    
+    public void pause();
+    
+    public void setTime(float time);
+	
+
+}
