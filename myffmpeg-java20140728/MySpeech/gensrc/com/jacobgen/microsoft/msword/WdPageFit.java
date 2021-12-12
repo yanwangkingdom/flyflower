f5d24cca1d7b2e94e75ae5e4f4c90790a21adcd5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:46:33 +0800

    Create WdPageFit.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPageFit.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPageFit {
+
+	public static final int wdPageFitNone = 0;
+	public static final int wdPageFitFullPage = 1;
+	public static final int wdPageFitBestFit = 2;
+	public static final int wdPageFitTextFit = 3;
+}
