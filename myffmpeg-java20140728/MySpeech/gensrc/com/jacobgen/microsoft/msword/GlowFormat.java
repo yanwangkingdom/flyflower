d77c776ffa265bbb9feea5dbe805ed387a113c82
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:14:10 +0800

    Create GlowFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/GlowFormat.java
@@ -0,0 +1,98 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class GlowFormat extends Dispatch {
+
+	public static final String componentName = "Word.GlowFormat";
+
+	public GlowFormat() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public GlowFormat(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public GlowFormat(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type float
+	 */
+	public float getRadius() {
+		return Dispatch.get(this, "Radius").changeType(Variant.VariantFloat).getFloat();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param radius an input-parameter of type float
+	 */
+	public void setRadius(float radius) {
+		Dispatch.put(this, "Radius", new Variant(radius));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type ColorFormat
+	 */
+	public ColorFormat getColor() {
+		return new ColorFormat(Dispatch.get(this, "Color").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type float
+	 */
+	public float getTransparency() {
+		return Dispatch.get(this, "Transparency").changeType(Variant.VariantFloat).getFloat();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param transparency an input-parameter of type float
+	 */
+	public void setTransparency(float transparency) {
+		Dispatch.put(this, "Transparency", new Variant(transparency));
+	}
+
+}
