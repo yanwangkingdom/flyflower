d62973d6f6e724d1ffd1e29b26ff1a95b25b5029
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:09 +0800

    Create WdSelectionFlags.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSelectionFlags.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSelectionFlags {
+
+	public static final int wdSelStartActive = 1;
+	public static final int wdSelAtEOL = 2;
+	public static final int wdSelOvertype = 4;
+	public static final int wdSelActive = 8;
+	public static final int wdSelReplace = 16;
+}
