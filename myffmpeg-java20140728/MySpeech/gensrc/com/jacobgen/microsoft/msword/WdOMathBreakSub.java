cfb4b6e5d87b8e7a8c7484db4dee599cf06d5e43
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:16 +0800

    Create WdOMathBreakSub.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathBreakSub.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathBreakSub {
+
+	public static final int wdOMathBreakSubMinusMinus = 0;
+	public static final int wdOMathBreakSubPlusMinus = 1;
+	public static final int wdOMathBreakSubMinusPlus = 2;
+}
