e0cdb455f83d0f3b108ce20625e8ae4986137a40
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:09 +0800

    Create WdOLEVerb.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOLEVerb.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOLEVerb {
+
+	public static final int wdOLEVerbPrimary = 0;
+	public static final int wdOLEVerbShow = -1;
+	public static final int wdOLEVerbOpen = -2;
+	public static final int wdOLEVerbHide = -3;
+	public static final int wdOLEVerbUIActivate = -4;
+	public static final int wdOLEVerbInPlaceActivate = -5;
+	public static final int wdOLEVerbDiscardUndoState = -6;
+}
