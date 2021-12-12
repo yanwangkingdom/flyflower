0146d1b06a94d3ddbba80a6b1c3a6a450f99e1ca
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:43 +0800

    Create WdFontBias.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFontBias.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFontBias {
+
+	public static final int wdFontBiasDontCare = 255;
+	public static final int wdFontBiasDefault = 0;
+	public static final int wdFontBiasFareast = 1;
+}
