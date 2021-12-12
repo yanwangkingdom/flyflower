1a82f87c455586e9a2184d5b0825e036d1159013
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:37 +0800

    Create WdEnableCancelKey.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEnableCancelKey.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEnableCancelKey {
+
+	public static final int wdCancelDisabled = 0;
+	public static final int wdCancelInterrupt = 1;
+}
