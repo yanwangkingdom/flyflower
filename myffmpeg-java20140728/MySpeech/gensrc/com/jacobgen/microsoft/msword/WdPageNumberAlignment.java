cd329ca6c720aca52791689a292770ef3589346c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:46:36 +0800

    Create WdPageNumberAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPageNumberAlignment.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPageNumberAlignment {
+
+	public static final int wdAlignPageNumberLeft = 0;
+	public static final int wdAlignPageNumberCenter = 1;
+	public static final int wdAlignPageNumberRight = 2;
+	public static final int wdAlignPageNumberInside = 3;
+	public static final int wdAlignPageNumberOutside = 4;
+}
