b356a10198cbcd6bdc405f9788d87ad078020297
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:37 +0800

    Create WdOMathSpacingRule.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathSpacingRule.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathSpacingRule {
+
+	public static final int wdOMathSpacingSingle = 0;
+	public static final int wdOMathSpacing1pt5 = 1;
+	public static final int wdOMathSpacingDouble = 2;
+	public static final int wdOMathSpacingExactly = 3;
+	public static final int wdOMathSpacingMultiple = 4;
+}
