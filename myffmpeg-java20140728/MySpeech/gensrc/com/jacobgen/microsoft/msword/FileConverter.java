21363913323c93ecc2e09d97433e90cfa14c403b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:12:29 +0800

    Create FileConverter.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/FileConverter.java
@@ -0,0 +1,130 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class FileConverter extends Dispatch {
+
+	public static final String componentName = "Word.FileConverter";
+
+	public FileConverter() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public FileConverter(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public FileConverter(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getFormatName() {
+		return Dispatch.get(this, "FormatName").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getClassName() {
+		return Dispatch.get(this, "ClassName").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getSaveFormat() {
+		return Dispatch.get(this, "SaveFormat").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getOpenFormat() {
+		return Dispatch.get(this, "OpenFormat").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type boolean
+	 */
+	public boolean getCanSave() {
+		return Dispatch.get(this, "CanSave").changeType(Variant.VariantBoolean).getBoolean();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type boolean
+	 */
+	public boolean getCanOpen() {
+		return Dispatch.get(this, "CanOpen").changeType(Variant.VariantBoolean).getBoolean();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getPath() {
+		return Dispatch.get(this, "Path").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getName() {
+		return Dispatch.get(this, "Name").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getExtensions() {
+		return Dispatch.get(this, "Extensions").toString();
+	}
+
+}
