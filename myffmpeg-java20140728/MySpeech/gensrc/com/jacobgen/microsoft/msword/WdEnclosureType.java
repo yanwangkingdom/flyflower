5d00229a57ff1141255a0299190c4d2e33114d70
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:45 +0800

    Create WdEnclosureType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEnclosureType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEnclosureType {
+
+	public static final int wdEnclosureCircle = 0;
+	public static final int wdEnclosureSquare = 1;
+	public static final int wdEnclosureTriangle = 2;
+	public static final int wdEnclosureDiamond = 3;
+}
