300153c60c21472aee99e2f65e9d16aed485420c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:54 +0800

    Create WdWrapType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdWrapType.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdWrapType {
+
+	public static final int wdWrapSquare = 0;
+	public static final int wdWrapTight = 1;
+	public static final int wdWrapThrough = 2;
+	public static final int wdWrapNone = 3;
+	public static final int wdWrapTopBottom = 4;
+	public static final int wdWrapBehind = 5;
+	public static final int wdWrapFront = 3;
+	public static final int wdWrapInline = 7;
+}
