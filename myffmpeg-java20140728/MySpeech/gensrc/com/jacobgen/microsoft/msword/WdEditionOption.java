29c25f7286419e482e303bd438ca590bb0ce5c3a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:19 +0800

    Create WdEditionOption.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEditionOption.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEditionOption {
+
+	public static final int wdCancelPublisher = 0;
+	public static final int wdSendPublisher = 1;
+	public static final int wdSelectPublisher = 2;
+	public static final int wdAutomaticUpdate = 3;
+	public static final int wdManualUpdate = 4;
+	public static final int wdChangeAttributes = 5;
+	public static final int wdUpdateSubscriber = 6;
+	public static final int wdOpenSource = 7;
+}
