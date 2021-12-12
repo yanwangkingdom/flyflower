867dec1ea29ff525b28f516090a44263b536578b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:11 +0800

    Create WdHorizontalInVerticalType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdHorizontalInVerticalType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdHorizontalInVerticalType {
+
+	public static final int wdHorizontalInVerticalNone = 0;
+	public static final int wdHorizontalInVerticalFitInLine = 1;
+	public static final int wdHorizontalInVerticalResizeLine = 2;
+}
