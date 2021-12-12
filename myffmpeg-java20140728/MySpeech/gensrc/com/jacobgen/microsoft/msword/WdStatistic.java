b1cced69108ba3136e5265826a4f16eb2b7abddb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:53:48 +0800

    Create WdStatistic.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdStatistic.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdStatistic {
+
+	public static final int wdStatisticWords = 0;
+	public static final int wdStatisticLines = 1;
+	public static final int wdStatisticPages = 2;
+	public static final int wdStatisticCharacters = 3;
+	public static final int wdStatisticParagraphs = 4;
+	public static final int wdStatisticCharactersWithSpaces = 5;
+	public static final int wdStatisticFarEastCharacters = 6;
+}
