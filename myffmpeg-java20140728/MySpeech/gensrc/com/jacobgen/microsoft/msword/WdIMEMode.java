4f51a5d26f8cba7368c5fc00f82314f593db977e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:23 +0800

    Create WdIMEMode.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdIMEMode.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdIMEMode {
+
+	public static final int wdIMEModeNoControl = 0;
+	public static final int wdIMEModeOn = 1;
+	public static final int wdIMEModeOff = 2;
+	public static final int wdIMEModeHiragana = 4;
+	public static final int wdIMEModeKatakana = 5;
+	public static final int wdIMEModeKatakanaHalf = 6;
+	public static final int wdIMEModeAlphaFull = 7;
+	public static final int wdIMEModeAlpha = 8;
+	public static final int wdIMEModeHangulFull = 9;
+	public static final int wdIMEModeHangul = 10;
+}
