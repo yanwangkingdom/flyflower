ea463a3929294eaf34c6fd4c4e397f2fdc8b3d97
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:29 +0800

    Create WdCompareTarget.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCompareTarget.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCompareTarget {
+
+	public static final int wdCompareTargetSelected = 0;
+	public static final int wdCompareTargetCurrent = 1;
+	public static final int wdCompareTargetNew = 2;
+}
