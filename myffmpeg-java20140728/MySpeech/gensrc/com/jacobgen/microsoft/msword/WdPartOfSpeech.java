c9561e63fdf5facb3986b054911c0c943ba7ace1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:00 +0800

    Create WdPartOfSpeech.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPartOfSpeech.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPartOfSpeech {
+
+	public static final int wdAdjective = 0;
+	public static final int wdNoun = 1;
+	public static final int wdAdverb = 2;
+	public static final int wdVerb = 3;
+	public static final int wdPronoun = 4;
+	public static final int wdConjunction = 5;
+	public static final int wdPreposition = 6;
+	public static final int wdInterjection = 7;
+	public static final int wdIdiom = 8;
+	public static final int wdOther = 9;
+}
