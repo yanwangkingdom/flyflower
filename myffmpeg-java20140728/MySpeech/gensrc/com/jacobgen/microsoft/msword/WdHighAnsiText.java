494d37471e0f25142d9c31645ac20524fc421db1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:08 +0800

    Create WdHighAnsiText.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdHighAnsiText.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdHighAnsiText {
+
+	public static final int wdHighAnsiIsFarEast = 0;
+	public static final int wdHighAnsiIsHighAnsi = 1;
+	public static final int wdAutoDetectHighAnsiFarEast = 2;
+}
