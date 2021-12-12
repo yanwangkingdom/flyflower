b51ad731460d5fd5c6ced1295eafe3e397f60360
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:58:53 +0800

    Create XlAxisType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlAxisType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlAxisType {
+
+	public static final int xlCategory = 1;
+	public static final int xlSeriesAxis = 3;
+	public static final int xlValue = 2;
+}
