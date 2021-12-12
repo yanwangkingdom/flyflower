0fec752b828570708d8024b16af93c4b24065a34
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:55 +0800

    Create WdLineWidth.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLineWidth.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLineWidth {
+
+	public static final int wdLineWidth025pt = 2;
+	public static final int wdLineWidth050pt = 4;
+	public static final int wdLineWidth075pt = 6;
+	public static final int wdLineWidth100pt = 8;
+	public static final int wdLineWidth150pt = 12;
+	public static final int wdLineWidth225pt = 18;
+	public static final int wdLineWidth300pt = 24;
+	public static final int wdLineWidth450pt = 36;
+	public static final int wdLineWidth600pt = 48;
+}
