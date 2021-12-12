8e9d5887e89d0dfbd8b98ac824bd0874286f94e8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:53:29 +0800

    Create WdSpanishSpeller.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSpanishSpeller.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSpanishSpeller {
+
+	public static final int wdSpanishTuteoOnly = 0;
+	public static final int wdSpanishTuteoAndVoseo = 1;
+	public static final int wdSpanishVoseoOnly = 2;
+}
