a4e2f49aba01d8421e76ab2bb519d4ead0ce1979
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:38 +0800

    Create WdSmartTagControlType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSmartTagControlType.java
@@ -0,0 +1,27 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSmartTagControlType {
+
+	public static final int wdControlSmartTag = 1;
+	public static final int wdControlLink = 2;
+	public static final int wdControlHelp = 3;
+	public static final int wdControlHelpURL = 4;
+	public static final int wdControlSeparator = 5;
+	public static final int wdControlButton = 6;
+	public static final int wdControlLabel = 7;
+	public static final int wdControlImage = 8;
+	public static final int wdControlCheckbox = 9;
+	public static final int wdControlTextbox = 10;
+	public static final int wdControlListbox = 11;
+	public static final int wdControlCombo = 12;
+	public static final int wdControlActiveX = 13;
+	public static final int wdControlDocumentFragment = 14;
+	public static final int wdControlDocumentFragmentURL = 15;
+	public static final int wdControlRadioGroup = 16;
+}
