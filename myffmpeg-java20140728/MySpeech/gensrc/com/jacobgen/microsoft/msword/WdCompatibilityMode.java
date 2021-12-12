559dd4fca85a0c9754705a3c960b3d07e6df9228
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:35 +0800

    Create WdCompatibilityMode.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCompatibilityMode.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCompatibilityMode {
+
+	public static final int wdWord2003 = 11;
+	public static final int wdWord2007 = 12;
+	public static final int wdWord2010 = 14;
+	public static final int wdWord2013 = 15;
+	public static final int wdCurrent = 65535;
+}
