15f6f6f3bb166831969e0e7ab2024830e4be59a5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:55:33 +0800

    Create WdTablePosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTablePosition.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTablePosition {
+
+	public static final int wdTableTop = -999999;
+	public static final int wdTableLeft = -999998;
+	public static final int wdTableBottom = -999997;
+	public static final int wdTableRight = -999996;
+	public static final int wdTableCenter = -999995;
+	public static final int wdTableInside = -999994;
+	public static final int wdTableOutside = -999993;
+}
