2e12bf0f6e4855e190f6958a2f74cad655174238
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:01:08 +0800

    Create XlErrorBarDirection.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlErrorBarDirection.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlErrorBarDirection {
+
+	public static final int xlChartX = -4168;
+	public static final int xlChartY = 1;
+}
