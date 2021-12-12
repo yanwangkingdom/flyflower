b1fd8802ccd564450396fcbdbfbda56ab611afc9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:08 +0800

    Create WdStyleSort.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdStyleSort.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdStyleSort {
+
+	public static final int wdStyleSortByName = 0;
+	public static final int wdStyleSortRecommended = 1;
+	public static final int wdStyleSortByFont = 2;
+	public static final int wdStyleSortByBasedOn = 3;
+	public static final int wdStyleSortByType = 4;
+}
