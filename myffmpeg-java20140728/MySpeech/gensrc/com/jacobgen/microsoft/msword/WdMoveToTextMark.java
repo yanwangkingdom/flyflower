dc346e4f07e5741d495994592a7c748ce9301a69
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:40:25 +0800

    Create WdMoveToTextMark.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMoveToTextMark.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMoveToTextMark {
+
+	public static final int wdMoveToTextMarkNone = 0;
+	public static final int wdMoveToTextMarkBold = 1;
+	public static final int wdMoveToTextMarkItalic = 2;
+	public static final int wdMoveToTextMarkUnderline = 3;
+	public static final int wdMoveToTextMarkDoubleUnderline = 4;
+	public static final int wdMoveToTextMarkColorOnly = 5;
+	public static final int wdMoveToTextMarkStrikeThrough = 6;
+	public static final int wdMoveToTextMarkDoubleStrikeThrough = 7;
+}
