099b643ae6af63dd22c507dceb3b689f9d93eee2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:22 +0800

    Create WdColumnWidth.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdColumnWidth.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdColumnWidth {
+
+	public static final int wdColumnWidthNarrow = 1;
+	public static final int wdColumnWidthDefault = 2;
+	public static final int wdColumnWidthWide = 3;
+}
