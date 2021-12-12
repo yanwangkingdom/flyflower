faf90f29fdb5cd822390c0d4b40986e2163cc35d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:22 +0800

    Create WdRelativeVerticalSize.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRelativeVerticalSize.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRelativeVerticalSize {
+
+	public static final int wdRelativeVerticalSizeMargin = 0;
+	public static final int wdRelativeVerticalSizePage = 1;
+	public static final int wdRelativeVerticalSizeTopMarginArea = 2;
+	public static final int wdRelativeVerticalSizeBottomMarginArea = 3;
+	public static final int wdRelativeVerticalSizeInnerMarginArea = 4;
+	public static final int wdRelativeVerticalSizeOuterMarginArea = 5;
+}
