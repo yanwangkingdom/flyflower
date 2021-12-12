c82d17e77b8883214c7cd104ca39e98eb460ff8f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:06 +0800

    Create WdCursorMovement.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCursorMovement.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCursorMovement {
+
+	public static final int wdCursorMovementLogical = 0;
+	public static final int wdCursorMovementVisual = 1;
+}
