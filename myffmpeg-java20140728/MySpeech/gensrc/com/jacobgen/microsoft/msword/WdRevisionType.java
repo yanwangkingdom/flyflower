41f94daf324a49746b3ed06edf049d64f29a824f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:10 +0800

    Create WdRevisionType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRevisionType.java
@@ -0,0 +1,33 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRevisionType {
+
+	public static final int wdNoRevision = 0;
+	public static final int wdRevisionInsert = 1;
+	public static final int wdRevisionDelete = 2;
+	public static final int wdRevisionProperty = 3;
+	public static final int wdRevisionParagraphNumber = 4;
+	public static final int wdRevisionDisplayField = 5;
+	public static final int wdRevisionReconcile = 6;
+	public static final int wdRevisionConflict = 7;
+	public static final int wdRevisionStyle = 8;
+	public static final int wdRevisionReplace = 9;
+	public static final int wdRevisionParagraphProperty = 10;
+	public static final int wdRevisionTableProperty = 11;
+	public static final int wdRevisionSectionProperty = 12;
+	public static final int wdRevisionStyleDefinition = 13;
+	public static final int wdRevisionMovedFrom = 14;
+	public static final int wdRevisionMovedTo = 15;
+	public static final int wdRevisionCellInsertion = 16;
+	public static final int wdRevisionCellDeletion = 17;
+	public static final int wdRevisionCellMerge = 18;
+	public static final int wdRevisionCellSplit = 19;
+	public static final int wdRevisionConflictInsert = 20;
+	public static final int wdRevisionConflictDelete = 21;
+}
