7d1490855440060447338b3a0840aeab9ceeac07
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:02 +0800

    Create WdCheckInVersionType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCheckInVersionType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCheckInVersionType {
+
+	public static final int wdCheckInMinorVersion = 0;
+	public static final int wdCheckInMajorVersion = 1;
+	public static final int wdCheckInOverwriteVersion = 2;
+}
