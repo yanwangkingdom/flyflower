b79dc342430a037fecbfe483b137c98071ce158d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:39 +0800

    Create WdSaveFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSaveFormat.java
@@ -0,0 +1,39 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSaveFormat {
+
+	public static final int wdFormatDocument = 0;
+	public static final int wdFormatDocument97 = 0;
+	public static final int wdFormatTemplate = 1;
+	public static final int wdFormatTemplate97 = 1;
+	public static final int wdFormatText = 2;
+	public static final int wdFormatTextLineBreaks = 3;
+	public static final int wdFormatDOSText = 4;
+	public static final int wdFormatDOSTextLineBreaks = 5;
+	public static final int wdFormatRTF = 6;
+	public static final int wdFormatUnicodeText = 7;
+	public static final int wdFormatEncodedText = 7;
+	public static final int wdFormatHTML = 8;
+	public static final int wdFormatWebArchive = 9;
+	public static final int wdFormatFilteredHTML = 10;
+	public static final int wdFormatXML = 11;
+	public static final int wdFormatXMLDocument = 12;
+	public static final int wdFormatXMLDocumentMacroEnabled = 13;
+	public static final int wdFormatXMLTemplate = 14;
+	public static final int wdFormatXMLTemplateMacroEnabled = 15;
+	public static final int wdFormatDocumentDefault = 16;
+	public static final int wdFormatPDF = 17;
+	public static final int wdFormatXPS = 18;
+	public static final int wdFormatFlatXML = 19;
+	public static final int wdFormatFlatXMLMacroEnabled = 20;
+	public static final int wdFormatFlatXMLTemplate = 21;
+	public static final int wdFormatFlatXMLTemplateMacroEnabled = 22;
+	public static final int wdFormatOpenDocumentText = 23;
+	public static final int wdFormatStrictOpenXMLDocument = 24;
+}
