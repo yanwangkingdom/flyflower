1ab459a68c616bd6de2a3738ce2fa9a67f1ade86
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 21:56:53 +0800

    Create AutoCaption.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/AutoCaption.java
@@ -0,0 +1,106 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class AutoCaption extends Dispatch {
+
+	public static final String componentName = "Word.AutoCaption";
+
+	public AutoCaption() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public AutoCaption(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public AutoCaption(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getName() {
+		return Dispatch.get(this, "Name").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type boolean
+	 */
+	public boolean getAutoInsert() {
+		return Dispatch.get(this, "AutoInsert").changeType(Variant.VariantBoolean).getBoolean();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param autoInsert an input-parameter of type boolean
+	 */
+	public void setAutoInsert(boolean autoInsert) {
+		Dispatch.put(this, "AutoInsert", new Variant(autoInsert));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getIndex() {
+		return Dispatch.get(this, "Index").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant getCaptionLabel() {
+		return Dispatch.get(this, "CaptionLabel");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param captionLabel an input-parameter of type Variant
+	 */
+	public void setCaptionLabel(Variant captionLabel) {
+		Dispatch.put(this, "CaptionLabel", captionLabel);
+	}
+
+}
