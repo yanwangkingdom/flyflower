87702892f7ff42af26159dc419130864aac5d14d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:46 +0800

    Create XlDataLabelPosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlDataLabelPosition.java
@@ -0,0 +1,22 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlDataLabelPosition {
+
+	public static final int xlLabelPositionCenter = -4108;
+	public static final int xlLabelPositionAbove = 0;
+	public static final int xlLabelPositionBelow = 1;
+	public static final int xlLabelPositionLeft = -4131;
+	public static final int xlLabelPositionRight = -4152;
+	public static final int xlLabelPositionOutsideEnd = 2;
+	public static final int xlLabelPositionInsideEnd = 3;
+	public static final int xlLabelPositionInsideBase = 4;
+	public static final int xlLabelPositionBestFit = 5;
+	public static final int xlLabelPositionMixed = 6;
+	public static final int xlLabelPositionCustom = 7;
+}
