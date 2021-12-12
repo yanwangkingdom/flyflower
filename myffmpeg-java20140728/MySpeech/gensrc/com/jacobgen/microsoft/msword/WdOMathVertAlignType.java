cccd19f38c3925f76e65cfe0fa33998eba302313
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:44 +0800

    Create WdOMathVertAlignType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathVertAlignType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathVertAlignType {
+
+	public static final int wdOMathVertAlignCenter = 0;
+	public static final int wdOMathVertAlignTop = 1;
+	public static final int wdOMathVertAlignBottom = 2;
+}
