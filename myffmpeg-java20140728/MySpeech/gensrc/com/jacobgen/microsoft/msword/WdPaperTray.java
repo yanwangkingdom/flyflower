a3d258a860e62bcb823770c2567e94ecf79013ec
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:46:49 +0800

    Create WdPaperTray.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPaperTray.java
@@ -0,0 +1,26 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPaperTray {
+
+	public static final int wdPrinterDefaultBin = 0;
+	public static final int wdPrinterUpperBin = 1;
+	public static final int wdPrinterOnlyBin = 1;
+	public static final int wdPrinterLowerBin = 2;
+	public static final int wdPrinterMiddleBin = 3;
+	public static final int wdPrinterManualFeed = 4;
+	public static final int wdPrinterEnvelopeFeed = 5;
+	public static final int wdPrinterManualEnvelopeFeed = 6;
+	public static final int wdPrinterAutomaticSheetFeed = 7;
+	public static final int wdPrinterTractorFeed = 8;
+	public static final int wdPrinterSmallFormatBin = 9;
+	public static final int wdPrinterLargeFormatBin = 10;
+	public static final int wdPrinterLargeCapacityBin = 11;
+	public static final int wdPrinterPaperCassette = 14;
+	public static final int wdPrinterFormSource = 15;
+}
