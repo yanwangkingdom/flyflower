4f37c58bdf7d6f3057a294ee0b6e5a71b82a5c4c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:29 +0800

    Create WdCaptionLabelID.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCaptionLabelID.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCaptionLabelID {
+
+	public static final int wdCaptionFigure = -1;
+	public static final int wdCaptionTable = -2;
+	public static final int wdCaptionEquation = -3;
+}
