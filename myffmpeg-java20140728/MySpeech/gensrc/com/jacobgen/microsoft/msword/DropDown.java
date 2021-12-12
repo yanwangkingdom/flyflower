ea3e3041f758d27de5fbb0324154faf885238869
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:10:12 +0800

    Create DropDown.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/DropDown.java
@@ -0,0 +1,106 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class DropDown extends Dispatch {
+
+	public static final String componentName = "Word.DropDown";
+
+	public DropDown() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public DropDown(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public DropDown(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type boolean
+	 */
+	public boolean getValid() {
+		return Dispatch.get(this, "Valid").changeType(Variant.VariantBoolean).getBoolean();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getDefault() {
+		return Dispatch.get(this, "Default").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param p_default an input-parameter of type int
+	 */
+	public void setDefault(int p_default) {
+		Dispatch.put(this, "Default", new Variant(p_default));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getValue() {
+		return Dispatch.get(this, "Value").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param value an input-parameter of type int
+	 */
+	public void setValue(int value) {
+		Dispatch.put(this, "Value", new Variant(value));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type ListEntries
+	 */
+	public ListEntries getListEntries() {
+		return new ListEntries(Dispatch.get(this, "ListEntries").toDispatch());
+	}
+
+}
