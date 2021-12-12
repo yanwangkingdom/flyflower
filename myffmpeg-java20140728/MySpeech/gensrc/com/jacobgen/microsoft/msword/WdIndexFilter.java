b232e671486af93811afe89ec2fa147918ddc38c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:26 +0800

    Create WdIndexFilter.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdIndexFilter.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdIndexFilter {
+
+	public static final int wdIndexFilterNone = 0;
+	public static final int wdIndexFilterAiueo = 1;
+	public static final int wdIndexFilterAkasatana = 2;
+	public static final int wdIndexFilterChosung = 3;
+	public static final int wdIndexFilterLow = 4;
+	public static final int wdIndexFilterMedium = 5;
+	public static final int wdIndexFilterFull = 6;
+}
