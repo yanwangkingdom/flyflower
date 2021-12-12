f13a3f4ea007c5106e1d7fed2340261448bd7f09
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:46 +0800

    Create WdMailMergeDefaultRecord.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailMergeDefaultRecord.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailMergeDefaultRecord {
+
+	public static final int wdDefaultFirstRecord = 1;
+	public static final int wdDefaultLastRecord = -16;
+}
