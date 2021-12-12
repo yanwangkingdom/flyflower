242ccc5fd6a3ede64641212ce47fbdbb0b4400bb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:35:42 +0800

    Create WdGutterStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdGutterStyle.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdGutterStyle {
+
+	public static final int wdGutterPosLeft = 0;
+	public static final int wdGutterPosTop = 1;
+	public static final int wdGutterPosRight = 2;
+}
