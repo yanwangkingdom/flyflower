40070b9ce84d3712a6da93dae4dde7c738bd02e0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:21 +0800

    Create WdPreferredWidthType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPreferredWidthType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPreferredWidthType {
+
+	public static final int wdPreferredWidthAuto = 1;
+	public static final int wdPreferredWidthPercent = 2;
+	public static final int wdPreferredWidthPoints = 3;
+}
