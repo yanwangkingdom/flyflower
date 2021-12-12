d5d8be39c25793da7e652dcd27bbb4a51a449078
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:50 +0800

    Create WdCharacterCase.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCharacterCase.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCharacterCase {
+
+	public static final int wdNextCase = -1;
+	public static final int wdLowerCase = 0;
+	public static final int wdUpperCase = 1;
+	public static final int wdTitleWord = 2;
+	public static final int wdTitleSentence = 4;
+	public static final int wdToggleCase = 5;
+	public static final int wdHalfWidth = 6;
+	public static final int wdFullWidth = 7;
+	public static final int wdKatakana = 8;
+	public static final int wdHiragana = 9;
+}
