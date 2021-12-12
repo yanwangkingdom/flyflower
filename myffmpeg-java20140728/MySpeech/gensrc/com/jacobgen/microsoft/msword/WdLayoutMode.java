24a2d176c890a87a12af85a21efc67d49ada5ef8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:25 +0800

    Create WdLayoutMode.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLayoutMode.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLayoutMode {
+
+	public static final int wdLayoutModeDefault = 0;
+	public static final int wdLayoutModeGrid = 1;
+	public static final int wdLayoutModeLineGrid = 2;
+	public static final int wdLayoutModeGenko = 3;
+}
