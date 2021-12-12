07ff05f1bcbba78523a5e62582258b29ed9f0431
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:13:11 +0800

    Create Font.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/Font.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class Font extends _Font {
+
+	public static final String componentName = "clsid:{000209F5-0000-0000-C000-000000000046}";
+
+	public Font() {
+		super(componentName);
+	}
+
+	public Font(Dispatch d) {
+		super(d);
+	}
+}
