48ce8cdfe2330add801f45433284e028e2f90b4d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:42 +0800

    Create WdCellColor.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCellColor.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCellColor {
+
+	public static final int wdCellColorByAuthor = -1;
+	public static final int wdCellColorNoHighlight = 0;
+	public static final int wdCellColorPink = 1;
+	public static final int wdCellColorLightBlue = 2;
+	public static final int wdCellColorLightYellow = 3;
+	public static final int wdCellColorLightPurple = 4;
+	public static final int wdCellColorLightOrange = 5;
+	public static final int wdCellColorLightGreen = 6;
+	public static final int wdCellColorLightGray = 7;
+}
