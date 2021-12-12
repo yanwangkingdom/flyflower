43f6a241b2df3f56752d01721dc6e7505f07ee4b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:15 +0800

    Create WdAnimation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdAnimation.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdAnimation {
+
+	public static final int wdAnimationNone = 0;
+	public static final int wdAnimationLasVegasLights = 1;
+	public static final int wdAnimationBlinkingBackground = 2;
+	public static final int wdAnimationSparkleText = 3;
+	public static final int wdAnimationMarchingBlackAnts = 4;
+	public static final int wdAnimationMarchingRedAnts = 5;
+	public static final int wdAnimationShimmer = 6;
+}
