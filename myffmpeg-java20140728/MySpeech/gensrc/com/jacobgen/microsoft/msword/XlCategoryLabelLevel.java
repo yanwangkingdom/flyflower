b029846f3e3fb3a8c54745b895bcc586103bf75a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:59:53 +0800

    Create XlCategoryLabelLevel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlCategoryLabelLevel.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlCategoryLabelLevel {
+
+	public static final int xlCategoryLabelLevelNone = -3;
+	public static final int xlCategoryLabelLevelCustom = -2;
+	public static final int xlCategoryLabelLevelAll = -1;
+}
