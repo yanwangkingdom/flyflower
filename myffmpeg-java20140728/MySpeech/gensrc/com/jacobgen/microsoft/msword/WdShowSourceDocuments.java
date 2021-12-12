f76fbcd4a7c66a82723821d1ecfa9e154f16e0b6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:34 +0800

    Create WdShowSourceDocuments.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdShowSourceDocuments.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdShowSourceDocuments {
+
+	public static final int wdShowSourceDocumentsNone = 0;
+	public static final int wdShowSourceDocumentsOriginal = 1;
+	public static final int wdShowSourceDocumentsRevised = 2;
+	public static final int wdShowSourceDocumentsBoth = 3;
+}
