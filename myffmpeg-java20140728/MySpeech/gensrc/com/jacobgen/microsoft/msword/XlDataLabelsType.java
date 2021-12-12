1d3bb13af96822c68b13aed5f9151be3ed109f83
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:54 +0800

    Create XlDataLabelsType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlDataLabelsType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlDataLabelsType {
+
+	public static final int xlDataLabelsShowNone = -4142;
+	public static final int xlDataLabelsShowValue = 2;
+	public static final int xlDataLabelsShowPercent = 3;
+	public static final int xlDataLabelsShowLabel = 4;
+	public static final int xlDataLabelsShowLabelAndPercent = 5;
+	public static final int xlDataLabelsShowBubbleSizes = 6;
+}
