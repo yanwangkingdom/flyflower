8fafa626f039a64ebe8afbc46ae432f8184193af
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:09:26 +0800

    Create Document.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/Document.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class Document extends DocumentEvents2 {
+
+	public static final String componentName = "clsid:{00020906-0000-0000-C000-000000000046}";
+
+	public Document() {
+		super(componentName);
+	}
+
+	public Document(Dispatch d) {
+		super(d);
+	}
+}
