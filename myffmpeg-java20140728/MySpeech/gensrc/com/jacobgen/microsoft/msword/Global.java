570118f99d916db00de5902c845cb777346c0636
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:14:06 +0800

    Create Global.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/Global.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class Global extends _Global {
+
+	public static final String componentName = "clsid:{000209F0-0000-0000-C000-000000000046}";
+
+	public Global() {
+		super(componentName);
+	}
+
+	public Global(Dispatch d) {
+		super(d);
+	}
+}
