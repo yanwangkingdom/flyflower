ae0b66a791753be73e3ac7d7124bdbe5c8c942e8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:03:48 +0800

    Create Category.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/Category.java
@@ -0,0 +1,90 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class Category extends Dispatch {
+
+	public static final String componentName = "Word.Category";
+
+	public Category() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public Category(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public Category(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getIndex() {
+		return Dispatch.get(this, "Index").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getName() {
+		return Dispatch.get(this, "Name").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type BuildingBlocks
+	 */
+	public BuildingBlocks getBuildingBlocks() {
+		return new BuildingBlocks(Dispatch.get(this, "BuildingBlocks").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type BuildingBlockType
+	 */
+	public BuildingBlockType getType() {
+		return new BuildingBlockType(Dispatch.get(this, "Type").toDispatch());
+	}
+
+}
