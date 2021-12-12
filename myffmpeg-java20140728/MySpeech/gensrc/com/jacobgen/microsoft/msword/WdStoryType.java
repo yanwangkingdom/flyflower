d671698dcc4623e83cf030f7f88eb100ca01d5b4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:53:56 +0800

    Create WdStoryType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdStoryType.java
@@ -0,0 +1,28 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdStoryType {
+
+	public static final int wdMainTextStory = 1;
+	public static final int wdFootnotesStory = 2;
+	public static final int wdEndnotesStory = 3;
+	public static final int wdCommentsStory = 4;
+	public static final int wdTextFrameStory = 5;
+	public static final int wdEvenPagesHeaderStory = 6;
+	public static final int wdPrimaryHeaderStory = 7;
+	public static final int wdEvenPagesFooterStory = 8;
+	public static final int wdPrimaryFooterStory = 9;
+	public static final int wdFirstPageHeaderStory = 10;
+	public static final int wdFirstPageFooterStory = 11;
+	public static final int wdFootnoteSeparatorStory = 12;
+	public static final int wdFootnoteContinuationSeparatorStory = 13;
+	public static final int wdFootnoteContinuationNoticeStory = 14;
+	public static final int wdEndnoteSeparatorStory = 15;
+	public static final int wdEndnoteContinuationSeparatorStory = 16;
+	public static final int wdEndnoteContinuationNoticeStory = 17;
+}
