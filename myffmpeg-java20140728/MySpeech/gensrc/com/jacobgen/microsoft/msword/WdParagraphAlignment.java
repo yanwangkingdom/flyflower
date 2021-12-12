d929b82dad7481460c8d2e65913de53450716950
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:46:53 +0800

    Create WdParagraphAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdParagraphAlignment.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdParagraphAlignment {
+
+	public static final int wdAlignParagraphLeft = 0;
+	public static final int wdAlignParagraphCenter = 1;
+	public static final int wdAlignParagraphRight = 2;
+	public static final int wdAlignParagraphJustify = 3;
+	public static final int wdAlignParagraphDistribute = 4;
+	public static final int wdAlignParagraphJustifyMed = 5;
+	public static final int wdAlignParagraphJustifyHi = 7;
+	public static final int wdAlignParagraphJustifyLow = 8;
+	public static final int wdAlignParagraphThaiJustify = 9;
+}
