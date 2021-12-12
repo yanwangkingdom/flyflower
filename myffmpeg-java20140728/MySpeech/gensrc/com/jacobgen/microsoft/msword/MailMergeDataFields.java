8b3dd7e3ccf83b2e0708b17c23501576247a78bd
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:18:13 +0800

    Create MailMergeDataFields.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/MailMergeDataFields.java
@@ -0,0 +1,83 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class MailMergeDataFields extends Dispatch {
+
+	public static final String componentName = "Word.MailMergeDataFields";
+
+	public MailMergeDataFields() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public MailMergeDataFields(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public MailMergeDataFields(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type Variant
+	 * @return the result is of type MailMergeDataField
+	 */
+	public MailMergeDataField item(Variant index) {
+		return new MailMergeDataField(Dispatch.call(this, "Item", index).toDispatch());
+	}
+
+}
