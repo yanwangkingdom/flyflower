7c612286fdade9abafc480395079848a43747cf7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:01 +0800

    Create WdTextOrientation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTextOrientation.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTextOrientation {
+
+	public static final int wdTextOrientationHorizontal = 0;
+	public static final int wdTextOrientationUpward = 2;
+	public static final int wdTextOrientationDownward = 3;
+	public static final int wdTextOrientationVerticalFarEast = 1;
+	public static final int wdTextOrientationHorizontalRotatedFarEast = 4;
+	public static final int wdTextOrientationVertical = 5;
+}
