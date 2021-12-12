5e51baabfea8ef7492dca9362044ad1232acc5ad
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:47 +0800

    Create WdOpenFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOpenFormat.java
@@ -0,0 +1,33 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOpenFormat {
+
+	public static final int wdOpenFormatAuto = 0;
+	public static final int wdOpenFormatDocument = 1;
+	public static final int wdOpenFormatTemplate = 2;
+	public static final int wdOpenFormatRTF = 3;
+	public static final int wdOpenFormatText = 4;
+	public static final int wdOpenFormatUnicodeText = 5;
+	public static final int wdOpenFormatEncodedText = 5;
+	public static final int wdOpenFormatAllWord = 6;
+	public static final int wdOpenFormatWebPages = 7;
+	public static final int wdOpenFormatXML = 8;
+	public static final int wdOpenFormatXMLDocument = 9;
+	public static final int wdOpenFormatXMLDocumentMacroEnabled = 10;
+	public static final int wdOpenFormatXMLTemplate = 11;
+	public static final int wdOpenFormatXMLTemplateMacroEnabled = 12;
+	public static final int wdOpenFormatDocument97 = 1;
+	public static final int wdOpenFormatTemplate97 = 2;
+	public static final int wdOpenFormatAllWordTemplates = 13;
+	public static final int wdOpenFormatXMLDocumentSerialized = 14;
+	public static final int wdOpenFormatXMLDocumentMacroEnabledSerialized = 15;
+	public static final int wdOpenFormatXMLTemplateSerialized = 16;
+	public static final int wdOpenFormatXMLTemplateMacroEnabledSerialized = 17;
+	public static final int wdOpenFormatOpenDocumentText = 18;
+}
