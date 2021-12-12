fba3c0b008622d3950c8cc1c7c9f09b823a859a3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:32 +0800

    Create WdPrintOutRange.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPrintOutRange.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPrintOutRange {
+
+	public static final int wdPrintAllDocument = 0;
+	public static final int wdPrintSelection = 1;
+	public static final int wdPrintCurrentPage = 2;
+	public static final int wdPrintFromTo = 3;
+	public static final int wdPrintRangeOfPages = 4;
+}
