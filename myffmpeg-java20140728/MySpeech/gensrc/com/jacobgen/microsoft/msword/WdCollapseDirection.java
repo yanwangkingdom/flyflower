6cb4c4cd0624efbf2c9ef2a5f1d5dc9151829d49
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:10 +0800

    Create WdCollapseDirection.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCollapseDirection.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCollapseDirection {
+
+	public static final int wdCollapseStart = 1;
+	public static final int wdCollapseEnd = 0;
+}
