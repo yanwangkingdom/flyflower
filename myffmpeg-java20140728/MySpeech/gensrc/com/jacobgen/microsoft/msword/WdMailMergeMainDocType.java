3eb7cefa11562f59ab47b41328fdc6bce03ba3a0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:58 +0800

    Create WdMailMergeMainDocType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailMergeMainDocType.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailMergeMainDocType {
+
+	public static final int wdNotAMergeDocument = -1;
+	public static final int wdFormLetters = 0;
+	public static final int wdMailingLabels = 1;
+	public static final int wdEnvelopes = 2;
+	public static final int wdCatalog = 3;
+	public static final int wdEMail = 4;
+	public static final int wdFax = 5;
+	public static final int wdDirectory = 3;
+}
