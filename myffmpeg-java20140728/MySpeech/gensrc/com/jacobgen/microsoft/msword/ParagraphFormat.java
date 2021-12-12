e2f79d1ac692c61630bb413e2355bfa6bfbe3f2e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:22:37 +0800

    Create ParagraphFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/ParagraphFormat.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class ParagraphFormat extends _ParagraphFormat {
+
+	public static final String componentName = "clsid:{000209F4-0000-0000-C000-000000000046}";
+
+	public ParagraphFormat() {
+		super(componentName);
+	}
+
+	public ParagraphFormat(Dispatch d) {
+		super(d);
+	}
+}
