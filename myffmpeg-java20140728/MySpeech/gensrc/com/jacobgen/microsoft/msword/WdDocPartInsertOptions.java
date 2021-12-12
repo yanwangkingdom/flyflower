c9f221caf709a1a7fbeacc2880d1faa2367ff10e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:50 +0800

    Create WdDocPartInsertOptions.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDocPartInsertOptions.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDocPartInsertOptions {
+
+	public static final int wdInsertContent = 0;
+	public static final int wdInsertParagraph = 1;
+	public static final int wdInsertPage = 2;
+}
