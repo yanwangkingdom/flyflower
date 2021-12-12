3bcd13a1b9eadd77978017b2b1d1ce92335ae701
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:31 +0800

    Create WdSalutationGender.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSalutationGender.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSalutationGender {
+
+	public static final int wdGenderFemale = 0;
+	public static final int wdGenderMale = 1;
+	public static final int wdGenderNeutral = 2;
+	public static final int wdGenderUnknown = 3;
+}
