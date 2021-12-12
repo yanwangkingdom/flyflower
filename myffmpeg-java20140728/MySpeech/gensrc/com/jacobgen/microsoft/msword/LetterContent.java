403245fac0fbeb5b364b6cea889f295095b96cfa
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:16:43 +0800

    Create LetterContent.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/LetterContent.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class LetterContent extends _LetterContent {
+
+	public static final String componentName = "clsid:{000209F1-0000-0000-C000-000000000046}";
+
+	public LetterContent() {
+		super(componentName);
+	}
+
+	public LetterContent(Dispatch d) {
+		super(d);
+	}
+}
