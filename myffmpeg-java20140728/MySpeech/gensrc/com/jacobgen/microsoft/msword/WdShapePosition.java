a27c293914a8f114bf1826affa7a8e08f95a2bf2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:21 +0800

    Create WdShapePosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdShapePosition.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdShapePosition {
+
+	public static final int wdShapeTop = -999999;
+	public static final int wdShapeLeft = -999998;
+	public static final int wdShapeBottom = -999997;
+	public static final int wdShapeRight = -999996;
+	public static final int wdShapeCenter = -999995;
+	public static final int wdShapeInside = -999994;
+	public static final int wdShapeOutside = -999993;
+}
