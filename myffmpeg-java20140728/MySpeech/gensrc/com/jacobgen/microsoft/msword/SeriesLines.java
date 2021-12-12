0a2a07fd949f99a67d70b94af577a59da04dcd2f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:25:10 +0800

    Create SeriesLines.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/SeriesLines.java
@@ -0,0 +1,98 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class SeriesLines extends Dispatch {
+
+	public static final String componentName = "Word.SeriesLines";
+
+	public SeriesLines() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public SeriesLines(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public SeriesLines(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getName() {
+		return Dispatch.get(this, "Name").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant select() {
+		return Dispatch.call(this, "Select");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type ChartBorder
+	 */
+	public ChartBorder getBorder() {
+		return new ChartBorder(Dispatch.get(this, "Border").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant delete() {
+		return Dispatch.call(this, "Delete");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type ChartFormat
+	 */
+	public ChartFormat getFormat() {
+		return new ChartFormat(Dispatch.get(this, "Format").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getApplication() {
+		return Dispatch.get(this, "Application");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+}
