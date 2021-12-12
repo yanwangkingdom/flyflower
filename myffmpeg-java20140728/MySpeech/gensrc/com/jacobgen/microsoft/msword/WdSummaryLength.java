ac9c96debb3d8e43850c1cf5b5c4f33c4aca10b2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:23 +0800

    Create WdSummaryLength.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSummaryLength.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSummaryLength {
+
+	public static final int wd10Sentences = -2;
+	public static final int wd20Sentences = -3;
+	public static final int wd100Words = -4;
+	public static final int wd500Words = -5;
+	public static final int wd10Percent = -6;
+	public static final int wd25Percent = -7;
+	public static final int wd50Percent = -8;
+	public static final int wd75Percent = -9;
+}
