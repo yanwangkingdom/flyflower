7f73f766d8c831393cd7ad6878358b94518ebb59
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:47 +0800

    Create WdCellVerticalAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCellVerticalAlignment.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCellVerticalAlignment {
+
+	public static final int wdCellAlignVerticalTop = 0;
+	public static final int wdCellAlignVerticalCenter = 1;
+	public static final int wdCellAlignVerticalBottom = 3;
+}
