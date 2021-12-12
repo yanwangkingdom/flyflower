7801181fb45cfa38ff7b9734ebc6d500675d396f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:27 +0800

    Create WdSummaryMode.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSummaryMode.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSummaryMode {
+
+	public static final int wdSummaryModeHighlight = 0;
+	public static final int wdSummaryModeHideAllButSummary = 1;
+	public static final int wdSummaryModeInsert = 2;
+	public static final int wdSummaryModeCreateNew = 3;
+}
