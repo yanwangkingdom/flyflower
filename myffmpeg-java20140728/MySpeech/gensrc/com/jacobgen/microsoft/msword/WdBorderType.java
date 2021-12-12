2cc5c8dcfacc3fde87c597c7aeee90e3cbb4d910
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:52 +0800

    Create WdBorderType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdBorderType.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdBorderType {
+
+	public static final int wdBorderTop = -1;
+	public static final int wdBorderLeft = -2;
+	public static final int wdBorderBottom = -3;
+	public static final int wdBorderRight = -4;
+	public static final int wdBorderHorizontal = -5;
+	public static final int wdBorderVertical = -6;
+	public static final int wdBorderDiagonalDown = -7;
+	public static final int wdBorderDiagonalUp = -8;
+}
