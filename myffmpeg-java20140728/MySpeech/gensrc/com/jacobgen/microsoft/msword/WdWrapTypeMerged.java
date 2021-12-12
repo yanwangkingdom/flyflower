657166e1ee7743591ec7ac600389a1f6e3d95475
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:58 +0800

    Create WdWrapTypeMerged.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdWrapTypeMerged.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdWrapTypeMerged {
+
+	public static final int wdWrapMergeInline = 0;
+	public static final int wdWrapMergeSquare = 1;
+	public static final int wdWrapMergeTight = 2;
+	public static final int wdWrapMergeBehind = 3;
+	public static final int wdWrapMergeFront = 4;
+	public static final int wdWrapMergeThrough = 5;
+	public static final int wdWrapMergeTopBottom = 6;
+}
