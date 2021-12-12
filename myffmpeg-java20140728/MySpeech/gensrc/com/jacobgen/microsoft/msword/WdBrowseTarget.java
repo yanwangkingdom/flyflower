0a5b7d8a30e0dedc13bdc813b15ed9a7482d0ff1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:06 +0800

    Create WdBrowseTarget.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdBrowseTarget.java
@@ -0,0 +1,23 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdBrowseTarget {
+
+	public static final int wdBrowsePage = 1;
+	public static final int wdBrowseSection = 2;
+	public static final int wdBrowseComment = 3;
+	public static final int wdBrowseFootnote = 4;
+	public static final int wdBrowseEndnote = 5;
+	public static final int wdBrowseField = 6;
+	public static final int wdBrowseTable = 7;
+	public static final int wdBrowseGraphic = 8;
+	public static final int wdBrowseHeading = 9;
+	public static final int wdBrowseEdit = 10;
+	public static final int wdBrowseFind = 11;
+	public static final int wdBrowseGoTo = 12;
+}
