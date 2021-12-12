c81d8d862a2bf5e7a76faa396d39ced33c07fa8e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:05 +0800

    Create WdChevronConvertRule.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdChevronConvertRule.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdChevronConvertRule {
+
+	public static final int wdNeverConvert = 0;
+	public static final int wdAlwaysConvert = 1;
+	public static final int wdAskToNotConvert = 2;
+	public static final int wdAskToConvert = 3;
+}
