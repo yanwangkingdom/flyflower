f370a1cb9e1582d314f0d04ba89537d24afff3eb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:55:36 +0800

    Create WdTaskPanes.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTaskPanes.java
@@ -0,0 +1,35 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTaskPanes {
+
+	public static final int wdTaskPaneFormatting = 0;
+	public static final int wdTaskPaneRevealFormatting = 1;
+	public static final int wdTaskPaneMailMerge = 2;
+	public static final int wdTaskPaneTranslate = 3;
+	public static final int wdTaskPaneSearch = 4;
+	public static final int wdTaskPaneXMLStructure = 5;
+	public static final int wdTaskPaneDocumentProtection = 6;
+	public static final int wdTaskPaneDocumentActions = 7;
+	public static final int wdTaskPaneSharedWorkspace = 8;
+	public static final int wdTaskPaneHelp = 9;
+	public static final int wdTaskPaneResearch = 10;
+	public static final int wdTaskPaneFaxService = 11;
+	public static final int wdTaskPaneXMLDocument = 12;
+	public static final int wdTaskPaneDocumentUpdates = 13;
+	public static final int wdTaskPaneSignature = 14;
+	public static final int wdTaskPaneStyleInspector = 15;
+	public static final int wdTaskPaneDocumentManagement = 16;
+	public static final int wdTaskPaneApplyStyles = 17;
+	public static final int wdTaskPaneNav = 18;
+	public static final int wdTaskPaneSelection = 19;
+	public static final int wdTaskPaneProofing = 20;
+	public static final int wdTaskPaneXMLMapping = 21;
+	public static final int wdTaskPaneRevPaneFlex = 22;
+	public static final int wdTaskPaneThesaurus = 23;
+}
