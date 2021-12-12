ccbe1b5d2eda9cb9f15d9343536dec6ca6967ae3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:24 +0800

    Create WdRowHeightRule.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRowHeightRule.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRowHeightRule {
+
+	public static final int wdRowHeightAuto = 0;
+	public static final int wdRowHeightAtLeast = 1;
+	public static final int wdRowHeightExactly = 2;
+}
