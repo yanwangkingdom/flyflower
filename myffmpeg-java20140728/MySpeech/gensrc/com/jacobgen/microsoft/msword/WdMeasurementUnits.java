cf912eeed990563fc0d2fedfcbf92026583a34f4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:39:32 +0800

    Create WdMeasurementUnits.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMeasurementUnits.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMeasurementUnits {
+
+	public static final int wdInches = 0;
+	public static final int wdCentimeters = 1;
+	public static final int wdMillimeters = 2;
+	public static final int wdPoints = 3;
+	public static final int wdPicas = 4;
+}
