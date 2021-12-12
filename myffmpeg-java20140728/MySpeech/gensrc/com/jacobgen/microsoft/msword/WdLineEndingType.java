65a4734babfe47b286be51f39388e63d4cd4bd7b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:41 +0800

    Create WdLineEndingType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLineEndingType.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLineEndingType {
+
+	public static final int wdCRLF = 0;
+	public static final int wdCROnly = 1;
+	public static final int wdLFOnly = 2;
+	public static final int wdLFCR = 3;
+	public static final int wdLSPS = 4;
+}
