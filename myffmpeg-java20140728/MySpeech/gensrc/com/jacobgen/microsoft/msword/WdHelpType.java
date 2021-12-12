1815b89bd90be83e1e3a785c794ac69a6a545ab5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:00 +0800

    Create WdHelpType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdHelpType.java
@@ -0,0 +1,25 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdHelpType {
+
+	public static final int wdHelp = 0;
+	public static final int wdHelpAbout = 1;
+	public static final int wdHelpActiveWindow = 2;
+	public static final int wdHelpContents = 3;
+	public static final int wdHelpExamplesAndDemos = 4;
+	public static final int wdHelpIndex = 5;
+	public static final int wdHelpKeyboard = 6;
+	public static final int wdHelpPSSHelp = 7;
+	public static final int wdHelpQuickPreview = 8;
+	public static final int wdHelpSearch = 9;
+	public static final int wdHelpUsingHelp = 10;
+	public static final int wdHelpIchitaro = 11;
+	public static final int wdHelpPE2 = 12;
+	public static final int wdHelpHWP = 13;
+}
