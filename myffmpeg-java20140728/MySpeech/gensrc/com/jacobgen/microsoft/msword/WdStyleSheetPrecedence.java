d0a72602e1c373c4efeb7d72846ed44828e0a3f7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:04 +0800

    Create WdStyleSheetPrecedence.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdStyleSheetPrecedence.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdStyleSheetPrecedence {
+
+	public static final int wdStyleSheetPrecedenceHigher = -1;
+	public static final int wdStyleSheetPrecedenceLower = -2;
+	public static final int wdStyleSheetPrecedenceHighest = 1;
+	public static final int wdStyleSheetPrecedenceLowest = 0;
+}
