5e39b7700081089d0508aa8c01a6dd70f5fd2374
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:38 +0800

    Create WdIndexType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdIndexType.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdIndexType {
+
+	public static final int wdIndexIndent = 0;
+	public static final int wdIndexRunin = 1;
+}
