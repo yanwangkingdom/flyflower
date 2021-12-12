8e87fe6e6b3be9e5877ca817498df5b1e1efe704
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:13:30 +0800

    Create FootnoteOptions.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/FootnoteOptions.java
@@ -0,0 +1,138 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class FootnoteOptions extends Dispatch {
+
+	public static final String componentName = "Word.FootnoteOptions";
+
+	public FootnoteOptions() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public FootnoteOptions(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public FootnoteOptions(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getLocation() {
+		return Dispatch.get(this, "Location").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param location an input-parameter of type int
+	 */
+	public void setLocation(int location) {
+		Dispatch.put(this, "Location", new Variant(location));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getNumberStyle() {
+		return Dispatch.get(this, "NumberStyle").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param numberStyle an input-parameter of type int
+	 */
+	public void setNumberStyle(int numberStyle) {
+		Dispatch.put(this, "NumberStyle", new Variant(numberStyle));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getStartingNumber() {
+		return Dispatch.get(this, "StartingNumber").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param startingNumber an input-parameter of type int
+	 */
+	public void setStartingNumber(int startingNumber) {
+		Dispatch.put(this, "StartingNumber", new Variant(startingNumber));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getNumberingRule() {
+		return Dispatch.get(this, "NumberingRule").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param numberingRule an input-parameter of type int
+	 */
+	public void setNumberingRule(int numberingRule) {
+		Dispatch.put(this, "NumberingRule", new Variant(numberingRule));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getLayoutColumns() {
+		return Dispatch.get(this, "LayoutColumns").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param layoutColumns an input-parameter of type int
+	 */
+	public void setLayoutColumns(int layoutColumns) {
+		Dispatch.put(this, "LayoutColumns", new Variant(layoutColumns));
+	}
+
+}
