c46251f90abf6f8cc4f6de690bba7daaca14a53b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:35:56 +0800

    Create WdHebSpellStart.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdHebSpellStart.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdHebSpellStart {
+
+	public static final int wdFullScript = 0;
+	public static final int wdPartialScript = 1;
+	public static final int wdMixedScript = 2;
+	public static final int wdMixedAuthorizedScript = 3;
+}
