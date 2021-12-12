b0136a5b9f698c69abb1978944983b2aa89d5481
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:00:36 +0800

    Create CaptionLabel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/CaptionLabel.java
@@ -0,0 +1,169 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class CaptionLabel extends Dispatch {
+
+	public static final String componentName = "Word.CaptionLabel";
+
+	public CaptionLabel() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public CaptionLabel(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public CaptionLabel(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getName() {
+		return Dispatch.get(this, "Name").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type boolean
+	 */
+	public boolean getBuiltIn() {
+		return Dispatch.get(this, "BuiltIn").changeType(Variant.VariantBoolean).getBoolean();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getID() {
+		return Dispatch.get(this, "ID").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type boolean
+	 */
+	public boolean getIncludeChapterNumber() {
+		return Dispatch.get(this, "IncludeChapterNumber").changeType(Variant.VariantBoolean).getBoolean();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param includeChapterNumber an input-parameter of type boolean
+	 */
+	public void setIncludeChapterNumber(boolean includeChapterNumber) {
+		Dispatch.put(this, "IncludeChapterNumber", new Variant(includeChapterNumber));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getNumberStyle() {
+		return Dispatch.get(this, "NumberStyle").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param numberStyle an input-parameter of type int
+	 */
+	public void setNumberStyle(int numberStyle) {
+		Dispatch.put(this, "NumberStyle", new Variant(numberStyle));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getChapterStyleLevel() {
+		return Dispatch.get(this, "ChapterStyleLevel").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param chapterStyleLevel an input-parameter of type int
+	 */
+	public void setChapterStyleLevel(int chapterStyleLevel) {
+		Dispatch.put(this, "ChapterStyleLevel", new Variant(chapterStyleLevel));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getSeparator() {
+		return Dispatch.get(this, "Separator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param separator an input-parameter of type int
+	 */
+	public void setSeparator(int separator) {
+		Dispatch.put(this, "Separator", new Variant(separator));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getPosition() {
+		return Dispatch.get(this, "Position").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param position an input-parameter of type int
+	 */
+	public void setPosition(int position) {
+		Dispatch.put(this, "Position", new Variant(position));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 */
+	public void delete() {
+		Dispatch.call(this, "Delete");
+	}
+
+}
