9e09dbe8b3dedbdce9b6f3d9663d353ca32d4d87
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:34 +0800

    Create WdEmphasisMark.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEmphasisMark.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEmphasisMark {
+
+	public static final int wdEmphasisMarkNone = 0;
+	public static final int wdEmphasisMarkOverSolidCircle = 1;
+	public static final int wdEmphasisMarkOverComma = 2;
+	public static final int wdEmphasisMarkOverWhiteCircle = 3;
+	public static final int wdEmphasisMarkUnderSolidCircle = 4;
+}
