8554143f977b7ef57d8dee067e8e4981c92404f6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:43 +0800

    Create WdConstants.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdConstants.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdConstants {
+
+	public static final int wdUndefined = 9999999;
+	public static final int wdToggle = 9999998;
+	public static final int wdForward = 1073741823;
+	public static final int wdBackward = -1073741823;
+	public static final int wdAutoPosition = 0;
+	public static final int wdFirst = 1;
+	public static final int wdCreatorCode = 1297307460;
+}
