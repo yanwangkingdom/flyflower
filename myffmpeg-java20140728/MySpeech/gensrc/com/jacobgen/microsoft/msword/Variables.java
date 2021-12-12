ecc07c4207fd5b4f3be1a4c97b24fe48b6a86dea
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:28:48 +0800

    Create Variables.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/Variables.java
@@ -0,0 +1,115 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class Variables extends Dispatch {
+
+	public static final String componentName = "Word.Variables";
+
+	public Variables() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public Variables(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public Variables(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type Variant
+	 * @return the result is of type Variable
+	 */
+	public Variable item(Variant index) {
+		return new Variable(Dispatch.call(this, "Item", index).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @param value an input-parameter of type Variant
+	 * @return the result is of type Variable
+	 */
+	public Variable add(String name, Variant value) {
+		return new Variable(Dispatch.call(this, "Add", name, value).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @return the result is of type Variable
+	 */
+	public Variable add(String name) {
+		return new Variable(Dispatch.call(this, "Add", name).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method and receiving the output-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @param value an input-parameter of type Variant
+	 * @return the result is of type Variable
+	 */
+	public Variable add(String name, Variant value) {
+		Variable result_of_Add = new Variable(Dispatch.call(this, "Add", name, value).toDispatch());
+
+
+		return result_of_Add;
+	}
+
+}
