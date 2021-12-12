ff5b70d787568df4a1c767450c176415ce3e8e41
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:40:43 +0800

    Create WdNumberForm.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdNumberForm.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdNumberForm {
+
+	public static final int wdNumberFormDefault = 0;
+	public static final int wdNumberFormLining = 1;
+	public static final int wdNumberFormOldStyle = 2;
+}
