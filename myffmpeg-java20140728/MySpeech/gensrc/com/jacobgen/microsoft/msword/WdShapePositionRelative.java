f6d4952d3784472ff17e3e89798caa72bde47f91
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:24 +0800

    Create WdShapePositionRelative.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdShapePositionRelative.java
@@ -0,0 +1,12 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdShapePositionRelative {
+
+	public static final int wdShapePositionRelativeNone = -999999;
+}
