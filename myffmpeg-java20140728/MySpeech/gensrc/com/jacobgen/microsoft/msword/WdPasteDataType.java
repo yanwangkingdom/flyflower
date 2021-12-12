dee5c21fe4dca5e10097c5b45bc4865d565b6a90
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:04 +0800

    Create WdPasteDataType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPasteDataType.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPasteDataType {
+
+	public static final int wdPasteOLEObject = 0;
+	public static final int wdPasteRTF = 1;
+	public static final int wdPasteText = 2;
+	public static final int wdPasteMetafilePicture = 3;
+	public static final int wdPasteBitmap = 4;
+	public static final int wdPasteDeviceIndependentBitmap = 5;
+	public static final int wdPasteHyperlink = 7;
+	public static final int wdPasteShape = 8;
+	public static final int wdPasteEnhancedMetafile = 9;
+	public static final int wdPasteHTML = 10;
+}
