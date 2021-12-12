a4ad1cf82f87fc76a54708bc5fefaa278020bae0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:58:56 +0800

    Create XlBackground.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlBackground.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlBackground {
+
+	public static final int xlBackgroundAutomatic = -4105;
+	public static final int xlBackgroundOpaque = 3;
+	public static final int xlBackgroundTransparent = 2;
+}
