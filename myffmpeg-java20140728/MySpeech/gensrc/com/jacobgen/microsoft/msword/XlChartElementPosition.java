071ab4553f7527b9b1fffba0ec6779e25bb435c5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:01 +0800

    Create XlChartElementPosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlChartElementPosition.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlChartElementPosition {
+
+	public static final int xlChartElementPositionAutomatic = -4105;
+	public static final int xlChartElementPositionCustom = -4114;
+}
