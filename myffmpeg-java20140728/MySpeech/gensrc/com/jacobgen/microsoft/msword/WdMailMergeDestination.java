a868c43dbb5692b6838027db09887a60836505e2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:51 +0800

    Create WdMailMergeDestination.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailMergeDestination.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailMergeDestination {
+
+	public static final int wdSendToNewDocument = 0;
+	public static final int wdSendToPrinter = 1;
+	public static final int wdSendToEmail = 2;
+	public static final int wdSendToFax = 3;
+}
