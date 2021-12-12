dddb02c535fde0f8ca8d562520ff16843ece07cb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 21:54:36 +0800

    Create Application.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/Application.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class Application extends ApplicationEvents4 {
+
+	public static final String componentName = "clsid:{000209FF-0000-0000-C000-000000000046}";
+
+	public Application() {
+		super(componentName);
+	}
+
+	public Application(Dispatch d) {
+		super(d);
+	}
+}
