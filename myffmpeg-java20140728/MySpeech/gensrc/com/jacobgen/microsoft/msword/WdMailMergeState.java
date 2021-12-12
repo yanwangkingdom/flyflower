de7e2a69675d2c4ff7cde42885ac5d0bb3f68713
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:39:01 +0800

    Create WdMailMergeState.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailMergeState.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailMergeState {
+
+	public static final int wdNormalDocument = 0;
+	public static final int wdMainDocumentOnly = 1;
+	public static final int wdMainAndDataSource = 2;
+	public static final int wdMainAndHeader = 3;
+	public static final int wdMainAndSourceAndHeader = 4;
+	public static final int wdDataSource = 5;
+}
