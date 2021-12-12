f07c437bc28740fe01544f3109f6ace102da3e29
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:40 +0800

    Create WdOMathType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathType.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathType {
+
+	public static final int wdOMathDisplay = 0;
+	public static final int wdOMathInline = 1;
+}
