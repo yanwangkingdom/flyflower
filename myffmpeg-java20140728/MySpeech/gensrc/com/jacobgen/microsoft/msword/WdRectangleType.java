ce1b2a66fccd2cc1e31da17d179bc0ea256a72b4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:58 +0800

    Create WdRectangleType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRectangleType.java
@@ -0,0 +1,25 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRectangleType {
+
+	public static final int wdTextRectangle = 0;
+	public static final int wdShapeRectangle = 1;
+	public static final int wdMarkupRectangle = 2;
+	public static final int wdMarkupRectangleButton = 3;
+	public static final int wdPageBorderRectangle = 4;
+	public static final int wdLineBetweenColumnRectangle = 5;
+	public static final int wdSelection = 6;
+	public static final int wdSystem = 7;
+	public static final int wdMarkupRectangleArea = 8;
+	public static final int wdReadingModeNavigation = 9;
+	public static final int wdMarkupRectangleMoveMatch = 10;
+	public static final int wdReadingModePanningArea = 11;
+	public static final int wdMailNavArea = 12;
+	public static final int wdDocumentControlRectangle = 13;
+}
