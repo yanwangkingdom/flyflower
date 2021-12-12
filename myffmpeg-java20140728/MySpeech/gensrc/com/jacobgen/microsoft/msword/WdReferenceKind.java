e0bb757ed6a1c2927b61157af4ec977ae2efe3c4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:02 +0800

    Create WdReferenceKind.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdReferenceKind.java
@@ -0,0 +1,24 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdReferenceKind {
+
+	public static final int wdContentText = -1;
+	public static final int wdNumberRelativeContext = -2;
+	public static final int wdNumberNoContext = -3;
+	public static final int wdNumberFullContext = -4;
+	public static final int wdEntireCaption = 2;
+	public static final int wdOnlyLabelAndNumber = 3;
+	public static final int wdOnlyCaptionText = 4;
+	public static final int wdFootnoteNumber = 5;
+	public static final int wdEndnoteNumber = 6;
+	public static final int wdPageNumber = 7;
+	public static final int wdPosition = 15;
+	public static final int wdFootnoteNumberFormatted = 16;
+	public static final int wdEndnoteNumberFormatted = 17;
+}
