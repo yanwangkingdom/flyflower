8a4138ffa4e7e933783b2bd47b3e1dbac45e789c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:13 +0800

    Create WdRoutingSlipDelivery.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRoutingSlipDelivery.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRoutingSlipDelivery {
+
+	public static final int wdOneAfterAnother = 0;
+	public static final int wdAllAtOnce = 1;
+}
