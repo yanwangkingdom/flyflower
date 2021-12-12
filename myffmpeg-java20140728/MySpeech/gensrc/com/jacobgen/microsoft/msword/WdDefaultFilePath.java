a87a47bf8da4c680239d1c15aa5aa188beb45ee6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:20 +0800

    Create WdDefaultFilePath.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDefaultFilePath.java
@@ -0,0 +1,28 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDefaultFilePath {
+
+	public static final int wdDocumentsPath = 0;
+	public static final int wdPicturesPath = 1;
+	public static final int wdUserTemplatesPath = 2;
+	public static final int wdWorkgroupTemplatesPath = 3;
+	public static final int wdUserOptionsPath = 4;
+	public static final int wdAutoRecoverPath = 5;
+	public static final int wdToolsPath = 6;
+	public static final int wdTutorialPath = 7;
+	public static final int wdStartupPath = 8;
+	public static final int wdProgramPath = 9;
+	public static final int wdGraphicsFiltersPath = 10;
+	public static final int wdTextConvertersPath = 11;
+	public static final int wdProofingToolsPath = 12;
+	public static final int wdTempFilePath = 13;
+	public static final int wdCurrentFolderPath = 14;
+	public static final int wdStyleGalleryPath = 15;
+	public static final int wdBorderArtPath = 19;
+}
