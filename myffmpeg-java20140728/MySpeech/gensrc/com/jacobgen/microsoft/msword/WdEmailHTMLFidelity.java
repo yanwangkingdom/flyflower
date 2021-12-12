b6d603653c4162bdb8fd2ee8047fdacb78d4ed99
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:30 +0800

    Create WdEmailHTMLFidelity.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEmailHTMLFidelity.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEmailHTMLFidelity {
+
+	public static final int wdEmailHTMLFidelityLow = 1;
+	public static final int wdEmailHTMLFidelityMedium = 2;
+	public static final int wdEmailHTMLFidelityHigh = 3;
+}
