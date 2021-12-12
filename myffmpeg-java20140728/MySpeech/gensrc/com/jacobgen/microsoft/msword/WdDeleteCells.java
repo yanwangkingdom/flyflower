ecb0fe753fdb401e2257b5d4985a2b12bbd077d2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:30 +0800

    Create WdDeleteCells.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDeleteCells.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDeleteCells {
+
+	public static final int wdDeleteCellsShiftLeft = 0;
+	public static final int wdDeleteCellsShiftUp = 1;
+	public static final int wdDeleteCellsEntireRow = 2;
+	public static final int wdDeleteCellsEntireColumn = 3;
+}
