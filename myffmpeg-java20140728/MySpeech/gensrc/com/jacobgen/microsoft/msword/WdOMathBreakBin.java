98e231559dbe5842ccc8b2453464356f71c943c2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:12 +0800

    Create WdOMathBreakBin.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathBreakBin.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathBreakBin {
+
+	public static final int wdOMathBreakBinBefore = 0;
+	public static final int wdOMathBreakBinAfter = 1;
+	public static final int wdOMathBreakBinRepeat = 2;
+}
