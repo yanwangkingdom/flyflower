1713d48e732c4bb22417a9b8ad3a0485a233bff6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:15 +0800

    Create WdHorizontalLineAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdHorizontalLineAlignment.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdHorizontalLineAlignment {
+
+	public static final int wdHorizontalLineAlignLeft = 0;
+	public static final int wdHorizontalLineAlignCenter = 1;
+	public static final int wdHorizontalLineAlignRight = 2;
+}
