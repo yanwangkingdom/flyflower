3f7563993f4fa4e0edf24294113d55a13b944fe0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:01:21 +0800

    Create XlHAlign.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlHAlign.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlHAlign {
+
+	public static final int xlHAlignCenter = -4108;
+	public static final int xlHAlignCenterAcrossSelection = 7;
+	public static final int xlHAlignDistributed = -4117;
+	public static final int xlHAlignFill = 5;
+	public static final int xlHAlignGeneral = 1;
+	public static final int xlHAlignJustify = -4130;
+	public static final int xlHAlignLeft = -4131;
+	public static final int xlHAlignRight = -4152;
+}
