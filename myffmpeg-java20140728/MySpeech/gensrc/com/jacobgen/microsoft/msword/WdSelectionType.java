42dac3d843a3d1977bb89b69864ec6296aebd71d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:13 +0800

    Create WdSelectionType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSelectionType.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSelectionType {
+
+	public static final int wdNoSelection = 0;
+	public static final int wdSelectionIP = 1;
+	public static final int wdSelectionNormal = 2;
+	public static final int wdSelectionFrame = 3;
+	public static final int wdSelectionColumn = 4;
+	public static final int wdSelectionRow = 5;
+	public static final int wdSelectionBlock = 6;
+	public static final int wdSelectionInlineShape = 7;
+	public static final int wdSelectionShape = 8;
+}
