9f95a9beac635c9ee7a2e0c90239380123e76684
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:31 +0800

    Create WdTabAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTabAlignment.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTabAlignment {
+
+	public static final int wdAlignTabLeft = 0;
+	public static final int wdAlignTabCenter = 1;
+	public static final int wdAlignTabRight = 2;
+	public static final int wdAlignTabDecimal = 3;
+	public static final int wdAlignTabBar = 4;
+	public static final int wdAlignTabList = 6;
+}
