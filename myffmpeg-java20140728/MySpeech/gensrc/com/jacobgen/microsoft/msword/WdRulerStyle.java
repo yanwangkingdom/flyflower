9d42f86c676ec1ab814f12e3f6854769f4c2890a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:27 +0800

    Create WdRulerStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRulerStyle.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRulerStyle {
+
+	public static final int wdAdjustNone = 0;
+	public static final int wdAdjustProportional = 1;
+	public static final int wdAdjustFirstColumn = 2;
+	public static final int wdAdjustSameWidth = 3;
+}
