3066a00cc46a6f3795a9fba20e4da4c985d85a4d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:14 +0800

    Create WdThemeColorIndex.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdThemeColorIndex.java
@@ -0,0 +1,28 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdThemeColorIndex {
+
+	public static final int wdNotThemeColor = -1;
+	public static final int wdThemeColorMainDark1 = 0;
+	public static final int wdThemeColorMainLight1 = 1;
+	public static final int wdThemeColorMainDark2 = 2;
+	public static final int wdThemeColorMainLight2 = 3;
+	public static final int wdThemeColorAccent1 = 4;
+	public static final int wdThemeColorAccent2 = 5;
+	public static final int wdThemeColorAccent3 = 6;
+	public static final int wdThemeColorAccent4 = 7;
+	public static final int wdThemeColorAccent5 = 8;
+	public static final int wdThemeColorAccent6 = 9;
+	public static final int wdThemeColorHyperlink = 10;
+	public static final int wdThemeColorHyperlinkFollowed = 11;
+	public static final int wdThemeColorBackground1 = 12;
+	public static final int wdThemeColorText1 = 13;
+	public static final int wdThemeColorBackground2 = 14;
+	public static final int wdThemeColorText2 = 15;
+}
