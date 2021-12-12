0912fcdc96f20987ff50e8c81a55e8e6a7939156
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:02 +0800

    Create WdListApplyTo.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdListApplyTo.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdListApplyTo {
+
+	public static final int wdListApplyToWholeList = 0;
+	public static final int wdListApplyToThisPointForward = 1;
+	public static final int wdListApplyToSelection = 2;
+}
