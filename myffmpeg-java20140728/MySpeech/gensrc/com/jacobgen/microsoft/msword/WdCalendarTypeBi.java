ff731494539166696c984d5f3a19fbf29a2ccbd7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:25 +0800

    Create WdCalendarTypeBi.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCalendarTypeBi.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCalendarTypeBi {
+
+	public static final int wdCalendarTypeBidi = 99;
+	public static final int wdCalendarTypeGregorian = 100;
+}
