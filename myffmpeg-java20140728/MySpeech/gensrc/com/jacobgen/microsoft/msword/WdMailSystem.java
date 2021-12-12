25acc12364da2616e852e91ec303a2c1a0f6757f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:39:05 +0800

    Create WdMailSystem.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailSystem.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailSystem {
+
+	public static final int wdNoMailSystem = 0;
+	public static final int wdMAPI = 1;
+	public static final int wdPowerTalk = 2;
+	public static final int wdMAPIandPowerTalk = 3;
+}
