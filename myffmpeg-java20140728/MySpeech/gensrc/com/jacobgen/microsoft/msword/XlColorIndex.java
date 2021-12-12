e1b9d5d0cbf95a03c722828278ccf08356dda0d9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:36 +0800

    Create XlColorIndex.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlColorIndex.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlColorIndex {
+
+	public static final int xlColorIndexAutomatic = -4105;
+	public static final int xlColorIndexNone = -4142;
+}
