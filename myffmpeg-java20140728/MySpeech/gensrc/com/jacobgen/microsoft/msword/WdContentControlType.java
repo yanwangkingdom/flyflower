be6a661fbc0f9a497edd93168e2d66140f2b4a40
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:56 +0800

    Create WdContentControlType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdContentControlType.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdContentControlType {
+
+	public static final int wdContentControlRichText = 0;
+	public static final int wdContentControlText = 1;
+	public static final int wdContentControlPicture = 2;
+	public static final int wdContentControlComboBox = 3;
+	public static final int wdContentControlDropdownList = 4;
+	public static final int wdContentControlBuildingBlockGallery = 5;
+	public static final int wdContentControlDate = 6;
+	public static final int wdContentControlGroup = 7;
+	public static final int wdContentControlCheckBox = 8;
+	public static final int wdContentControlRepeatingSection = 9;
+}
