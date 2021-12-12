fc7340633ff1a44b57fef8ce6c58e40e924522b3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:50 +0800

    Create WdFramePosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFramePosition.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFramePosition {
+
+	public static final int wdFrameTop = -999999;
+	public static final int wdFrameLeft = -999998;
+	public static final int wdFrameBottom = -999997;
+	public static final int wdFrameRight = -999996;
+	public static final int wdFrameCenter = -999995;
+	public static final int wdFrameInside = -999994;
+	public static final int wdFrameOutside = -999993;
+}
