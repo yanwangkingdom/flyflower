651865799775ebaa04d8b500a7cb15265fe4fb8b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:30 +0800

    Create WdFindMatch.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFindMatch.java
@@ -0,0 +1,32 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFindMatch {
+
+	public static final int wdMatchParagraphMark = 65551;
+	public static final int wdMatchTabCharacter = 9;
+	public static final int wdMatchCommentMark = 5;
+	public static final int wdMatchAnyCharacter = 65599;
+	public static final int wdMatchAnyDigit = 65567;
+	public static final int wdMatchAnyLetter = 65583;
+	public static final int wdMatchCaretCharacter = 11;
+	public static final int wdMatchColumnBreak = 14;
+	public static final int wdMatchEmDash = 8212;
+	public static final int wdMatchEnDash = 8211;
+	public static final int wdMatchEndnoteMark = 65555;
+	public static final int wdMatchField = 19;
+	public static final int wdMatchFootnoteMark = 65554;
+	public static final int wdMatchGraphic = 1;
+	public static final int wdMatchManualLineBreak = 65551;
+	public static final int wdMatchManualPageBreak = 65564;
+	public static final int wdMatchNonbreakingHyphen = 30;
+	public static final int wdMatchNonbreakingSpace = 160;
+	public static final int wdMatchOptionalHyphen = 31;
+	public static final int wdMatchSectionBreak = 65580;
+	public static final int wdMatchWhiteSpace = 65655;
+}
