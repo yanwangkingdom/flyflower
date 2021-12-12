5c7934d16954cbddf93a2f123df948efa2c60df4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:46:39 +0800

    Create WdPageNumberStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPageNumberStyle.java
@@ -0,0 +1,40 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPageNumberStyle {
+
+	public static final int wdPageNumberStyleArabic = 0;
+	public static final int wdPageNumberStyleUppercaseRoman = 1;
+	public static final int wdPageNumberStyleLowercaseRoman = 2;
+	public static final int wdPageNumberStyleUppercaseLetter = 3;
+	public static final int wdPageNumberStyleLowercaseLetter = 4;
+	public static final int wdPageNumberStyleArabicFullWidth = 14;
+	public static final int wdPageNumberStyleKanji = 10;
+	public static final int wdPageNumberStyleKanjiDigit = 11;
+	public static final int wdPageNumberStyleKanjiTraditional = 16;
+	public static final int wdPageNumberStyleNumberInCircle = 18;
+	public static final int wdPageNumberStyleHanjaRead = 41;
+	public static final int wdPageNumberStyleHanjaReadDigit = 42;
+	public static final int wdPageNumberStyleTradChinNum1 = 33;
+	public static final int wdPageNumberStyleTradChinNum2 = 34;
+	public static final int wdPageNumberStyleSimpChinNum1 = 37;
+	public static final int wdPageNumberStyleSimpChinNum2 = 38;
+	public static final int wdPageNumberStyleHebrewLetter1 = 45;
+	public static final int wdPageNumberStyleArabicLetter1 = 46;
+	public static final int wdPageNumberStyleHebrewLetter2 = 47;
+	public static final int wdPageNumberStyleArabicLetter2 = 48;
+	public static final int wdPageNumberStyleHindiLetter1 = 49;
+	public static final int wdPageNumberStyleHindiLetter2 = 50;
+	public static final int wdPageNumberStyleHindiArabic = 51;
+	public static final int wdPageNumberStyleHindiCardinalText = 52;
+	public static final int wdPageNumberStyleThaiLetter = 53;
+	public static final int wdPageNumberStyleThaiArabic = 54;
+	public static final int wdPageNumberStyleThaiCardinalText = 55;
+	public static final int wdPageNumberStyleVietCardinalText = 56;
+	public static final int wdPageNumberStyleNumberInDash = 57;
+}
