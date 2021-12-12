d1cc1b43ef3a01f0159e3f60f80674f0d8c4f480
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:49 +0800

    Create WdContentControlDateStorageFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdContentControlDateStorageFormat.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdContentControlDateStorageFormat {
+
+	public static final int wdContentControlDateStorageText = 0;
+	public static final int wdContentControlDateStorageDate = 1;
+	public static final int wdContentControlDateStorageDateTime = 2;
+}
