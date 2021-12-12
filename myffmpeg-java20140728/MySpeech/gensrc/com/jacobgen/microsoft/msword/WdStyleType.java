d965970d6327e472d6a4200003c48a2cee4af519
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:12 +0800

    Create WdStyleType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdStyleType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdStyleType {
+
+	public static final int wdStyleTypeParagraph = 1;
+	public static final int wdStyleTypeCharacter = 2;
+	public static final int wdStyleTypeTable = 3;
+	public static final int wdStyleTypeList = 4;
+	public static final int wdStyleTypeParagraphOnly = 5;
+	public static final int wdStyleTypeLinked = 6;
+}
