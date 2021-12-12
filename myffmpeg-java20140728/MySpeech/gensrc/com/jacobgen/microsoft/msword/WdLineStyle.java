98b1b5723f9bc3720ad57f0ec2247bb2635f81c8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:48 +0800

    Create WdLineStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLineStyle.java
@@ -0,0 +1,36 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLineStyle {
+
+	public static final int wdLineStyleNone = 0;
+	public static final int wdLineStyleSingle = 1;
+	public static final int wdLineStyleDot = 2;
+	public static final int wdLineStyleDashSmallGap = 3;
+	public static final int wdLineStyleDashLargeGap = 4;
+	public static final int wdLineStyleDashDot = 5;
+	public static final int wdLineStyleDashDotDot = 6;
+	public static final int wdLineStyleDouble = 7;
+	public static final int wdLineStyleTriple = 8;
+	public static final int wdLineStyleThinThickSmallGap = 9;
+	public static final int wdLineStyleThickThinSmallGap = 10;
+	public static final int wdLineStyleThinThickThinSmallGap = 11;
+	public static final int wdLineStyleThinThickMedGap = 12;
+	public static final int wdLineStyleThickThinMedGap = 13;
+	public static final int wdLineStyleThinThickThinMedGap = 14;
+	public static final int wdLineStyleThinThickLargeGap = 15;
+	public static final int wdLineStyleThickThinLargeGap = 16;
+	public static final int wdLineStyleThinThickThinLargeGap = 17;
+	public static final int wdLineStyleSingleWavy = 18;
+	public static final int wdLineStyleDoubleWavy = 19;
+	public static final int wdLineStyleDashDotStroked = 20;
+	public static final int wdLineStyleEmboss3D = 21;
+	public static final int wdLineStyleEngrave3D = 22;
+	public static final int wdLineStyleOutset = 23;
+	public static final int wdLineStyleInset = 24;
+}
