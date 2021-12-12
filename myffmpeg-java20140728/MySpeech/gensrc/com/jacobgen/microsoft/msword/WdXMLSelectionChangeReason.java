6cdbd76753d75eb975aabb09a02868c38971d53d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:58:09 +0800

    Create WdXMLSelectionChangeReason.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdXMLSelectionChangeReason.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdXMLSelectionChangeReason {
+
+	public static final int wdXMLSelectionChangeReasonMove = 0;
+	public static final int wdXMLSelectionChangeReasonInsert = 1;
+	public static final int wdXMLSelectionChangeReasonDelete = 2;
+}
