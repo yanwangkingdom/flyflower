9738666f67087386b6e0062d57682b30a134388c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:40:47 +0800

    Create WdNumberingRule.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdNumberingRule.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdNumberingRule {
+
+	public static final int wdRestartContinuous = 0;
+	public static final int wdRestartSection = 1;
+	public static final int wdRestartPage = 2;
+}
