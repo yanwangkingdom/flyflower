cd6132d512961bedd37946ff56342818bd8b98fc
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:59:57 +0800

    Create XlCategoryType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlCategoryType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlCategoryType {
+
+	public static final int xlCategoryScale = 2;
+	public static final int xlTimeScale = 3;
+	public static final int xlAutomaticScale = -4105;
+}
