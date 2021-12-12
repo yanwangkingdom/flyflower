7d04008f0f49b5d4be845b0f974a596749ba2904
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:33 +0800

    Create WdReplace.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdReplace.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdReplace {
+
+	public static final int wdReplaceNone = 0;
+	public static final int wdReplaceOne = 1;
+	public static final int wdReplaceAll = 2;
+}
