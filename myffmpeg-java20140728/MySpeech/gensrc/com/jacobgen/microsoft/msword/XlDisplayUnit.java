95224e7c4145758e83543d95af9f7a42663f4bf3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:01:01 +0800

    Create XlDisplayUnit.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlDisplayUnit.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlDisplayUnit {
+
+	public static final int xlHundreds = -2;
+	public static final int xlThousands = -3;
+	public static final int xlTenThousands = -4;
+	public static final int xlHundredThousands = -5;
+	public static final int xlMillions = -6;
+	public static final int xlTenMillions = -7;
+	public static final int xlHundredMillions = -8;
+	public static final int xlThousandMillions = -9;
+	public static final int xlMillionMillions = -10;
+}
