77a0b8a802745ebbdefe84b99e894f3323290edb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:21 +0800

    Create WdCalendarType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCalendarType.java
@@ -0,0 +1,22 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCalendarType {
+
+	public static final int wdCalendarWestern = 0;
+	public static final int wdCalendarArabic = 1;
+	public static final int wdCalendarHebrew = 2;
+	public static final int wdCalendarTaiwan = 3;
+	public static final int wdCalendarJapan = 4;
+	public static final int wdCalendarThai = 5;
+	public static final int wdCalendarKorean = 6;
+	public static final int wdCalendarSakaEra = 7;
+	public static final int wdCalendarTranslitEnglish = 8;
+	public static final int wdCalendarTranslitFrench = 9;
+	public static final int wdCalendarUmalqura = 13;
+}
