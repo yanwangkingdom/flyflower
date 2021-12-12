893b26bd6960a72c652a1f6bacae5fa4b1f2a462
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:40 +0800

    Create WdDictionaryType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDictionaryType.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDictionaryType {
+
+	public static final int wdSpelling = 0;
+	public static final int wdGrammar = 1;
+	public static final int wdThesaurus = 2;
+	public static final int wdHyphenation = 3;
+	public static final int wdSpellingComplete = 4;
+	public static final int wdSpellingCustom = 5;
+	public static final int wdSpellingLegal = 6;
+	public static final int wdSpellingMedical = 7;
+	public static final int wdHangulHanjaConversion = 8;
+	public static final int wdHangulHanjaConversionCustom = 9;
+}
