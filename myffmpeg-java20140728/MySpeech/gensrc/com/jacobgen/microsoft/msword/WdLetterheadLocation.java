3badf8d2434a23b37b3e0829892dd172d0d72a6c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:29 +0800

    Create WdLetterheadLocation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLetterheadLocation.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLetterheadLocation {
+
+	public static final int wdLetterTop = 0;
+	public static final int wdLetterBottom = 1;
+	public static final int wdLetterLeft = 2;
+	public static final int wdLetterRight = 3;
+}
