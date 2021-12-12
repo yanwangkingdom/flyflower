d64c78ee976703200ce2af381e561b2b50cafe95
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:36 +0800

    Create WdRevisedLinesMark.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRevisedLinesMark.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRevisedLinesMark {
+
+	public static final int wdRevisedLinesMarkNone = 0;
+	public static final int wdRevisedLinesMarkLeftBorder = 1;
+	public static final int wdRevisedLinesMarkRightBorder = 2;
+	public static final int wdRevisedLinesMarkOutsideBorder = 3;
+}
