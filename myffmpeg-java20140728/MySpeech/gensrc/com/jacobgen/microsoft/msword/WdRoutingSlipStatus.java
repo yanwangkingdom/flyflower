6feddca07d60ec4f618a745b1b4d542fef4690dd
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:17 +0800

    Create WdRoutingSlipStatus.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRoutingSlipStatus.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRoutingSlipStatus {
+
+	public static final int wdNotYetRouted = 0;
+	public static final int wdRouteInProgress = 1;
+	public static final int wdRouteComplete = 2;
+}
