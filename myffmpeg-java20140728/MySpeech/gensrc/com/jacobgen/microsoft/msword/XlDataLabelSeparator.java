5bfc5069fbbbb53fbe8f1807e7998d5165ed25e9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:50 +0800

    Create XlDataLabelSeparator.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlDataLabelSeparator.java
@@ -0,0 +1,12 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlDataLabelSeparator {
+
+	public static final int xlDataLabelSeparatorDefault = 1;
+}
