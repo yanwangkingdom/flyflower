fd0e8faf9357905dc5ebc8082834a03014248071
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:41 +0800

    Create WdEncloseStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEncloseStyle.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEncloseStyle {
+
+	public static final int wdEncloseStyleNone = 0;
+	public static final int wdEncloseStyleSmall = 1;
+	public static final int wdEncloseStyleLarge = 2;
+}
