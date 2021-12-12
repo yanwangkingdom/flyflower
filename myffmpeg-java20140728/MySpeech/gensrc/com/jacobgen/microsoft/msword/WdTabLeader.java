1ef59ed42905ac2c60d2da14075a848b054b42b1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:35 +0800

    Create WdTabLeader.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTabLeader.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTabLeader {
+
+	public static final int wdTabLeaderSpaces = 0;
+	public static final int wdTabLeaderDots = 1;
+	public static final int wdTabLeaderDashes = 2;
+	public static final int wdTabLeaderLines = 3;
+	public static final int wdTabLeaderHeavy = 4;
+	public static final int wdTabLeaderMiddleDot = 5;
+}
