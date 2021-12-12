383d8cb4d5269f41383a42e527334bceab436430
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:44 +0800

    Create WdTwoLinesInOneType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTwoLinesInOneType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTwoLinesInOneType {
+
+	public static final int wdTwoLinesInOneNone = 0;
+	public static final int wdTwoLinesInOneNoBrackets = 1;
+	public static final int wdTwoLinesInOneParentheses = 2;
+	public static final int wdTwoLinesInOneSquareBrackets = 3;
+	public static final int wdTwoLinesInOneAngleBrackets = 4;
+	public static final int wdTwoLinesInOneCurlyBrackets = 5;
+}
