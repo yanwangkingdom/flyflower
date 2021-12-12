03d3d1f8e26d693122b8d51f31c1304fdc999355
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:05 +0800

    Create WdOLEType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOLEType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOLEType {
+
+	public static final int wdOLELink = 0;
+	public static final int wdOLEEmbed = 1;
+	public static final int wdOLEControl = 2;
+}
