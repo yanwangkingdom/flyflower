5e7a46cb62d2d4e03080fc40a2819676cadfea15
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:50 +0800

    Create WdUnderline.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdUnderline.java
@@ -0,0 +1,29 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdUnderline {
+
+	public static final int wdUnderlineNone = 0;
+	public static final int wdUnderlineSingle = 1;
+	public static final int wdUnderlineWords = 2;
+	public static final int wdUnderlineDouble = 3;
+	public static final int wdUnderlineDotted = 4;
+	public static final int wdUnderlineThick = 6;
+	public static final int wdUnderlineDash = 7;
+	public static final int wdUnderlineDotDash = 9;
+	public static final int wdUnderlineDotDotDash = 10;
+	public static final int wdUnderlineWavy = 11;
+	public static final int wdUnderlineWavyHeavy = 27;
+	public static final int wdUnderlineDottedHeavy = 20;
+	public static final int wdUnderlineDashHeavy = 23;
+	public static final int wdUnderlineDotDashHeavy = 25;
+	public static final int wdUnderlineDotDotDashHeavy = 26;
+	public static final int wdUnderlineDashLong = 39;
+	public static final int wdUnderlineDashLongHeavy = 55;
+	public static final int wdUnderlineWavyDouble = 43;
+}
