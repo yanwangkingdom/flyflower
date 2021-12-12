b066802c3feccaf9d0a66694267cc56c4def39aa
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:53:11 +0800

    Create WdSortSeparator.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSortSeparator.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSortSeparator {
+
+	public static final int wdSortSeparateByTabs = 0;
+	public static final int wdSortSeparateByCommas = 1;
+	public static final int wdSortSeparateByDefaultTableSeparator = 2;
+}
