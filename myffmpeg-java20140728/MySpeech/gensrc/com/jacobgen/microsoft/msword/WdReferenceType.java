6c97b73656ea361ed3df6a1e383730a6584155b3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:05 +0800

    Create WdReferenceType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdReferenceType.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdReferenceType {
+
+	public static final int wdRefTypeNumberedItem = 0;
+	public static final int wdRefTypeHeading = 1;
+	public static final int wdRefTypeBookmark = 2;
+	public static final int wdRefTypeFootnote = 3;
+	public static final int wdRefTypeEndnote = 4;
+}
