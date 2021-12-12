5b4f2c352e82b9e990f92c8c0fdf73f331303d97
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:18 +0800

    Create WdViewTypeOld.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdViewTypeOld.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdViewTypeOld {
+
+	public static final int wdPageView = 3;
+	public static final int wdOnlineView = 6;
+}
