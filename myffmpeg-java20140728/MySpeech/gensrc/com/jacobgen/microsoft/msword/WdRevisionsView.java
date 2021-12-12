94b2ffc175288185bfc5b90fed92b50192840852
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:02 +0800

    Create WdRevisionsView.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRevisionsView.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRevisionsView {
+
+	public static final int wdRevisionsViewFinal = 0;
+	public static final int wdRevisionsViewOriginal = 1;
+}
