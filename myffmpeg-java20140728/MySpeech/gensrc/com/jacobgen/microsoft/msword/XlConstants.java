53bf8642446828e55d5cee1d9234399d60ed3a81
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:39 +0800

    Create XlConstants.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlConstants.java
@@ -0,0 +1,74 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlConstants {
+
+	public static final int xlAutomatic = -4105;
+	public static final int xlCombination = -4111;
+	public static final int xlCustom = -4114;
+	public static final int xlBar = 2;
+	public static final int xlColumn = 3;
+	public static final int xl3DBar = -4099;
+	public static final int xl3DSurface = -4103;
+	public static final int xlDefaultAutoFormat = -1;
+	public static final int xlNone = -4142;
+	public static final int xlAbove = 0;
+	public static final int xlBelow = 1;
+	public static final int xlBoth = 1;
+	public static final int xlBottom = -4107;
+	public static final int xlCenter = -4108;
+	public static final int xlChecker = 9;
+	public static final int xlCircle = 8;
+	public static final int xlCorner = 2;
+	public static final int xlCrissCross = 16;
+	public static final int xlCross = 4;
+	public static final int xlDiamond = 2;
+	public static final int xlDistributed = -4117;
+	public static final int xlFill = 5;
+	public static final int xlFixedValue = 1;
+	public static final int xlGeneral = 1;
+	public static final int xlGray16 = 17;
+	public static final int xlGray25 = -4124;
+	public static final int xlGray50 = -4125;
+	public static final int xlGray75 = -4126;
+	public static final int xlGray8 = 18;
+	public static final int xlGrid = 15;
+	public static final int xlHigh = -4127;
+	public static final int xlInside = 2;
+	public static final int xlJustify = -4130;
+	public static final int xlLeft = -4131;
+	public static final int xlLightDown = 13;
+	public static final int xlLightHorizontal = 11;
+	public static final int xlLightUp = 14;
+	public static final int xlLightVertical = 12;
+	public static final int xlLow = -4134;
+	public static final int xlMaximum = 2;
+	public static final int xlMinimum = 4;
+	public static final int xlMinusValues = 3;
+	public static final int xlNextToAxis = 4;
+	public static final int xlOpaque = 3;
+	public static final int xlOutside = 3;
+	public static final int xlPercent = 2;
+	public static final int xlPlus = 9;
+	public static final int xlPlusValues = 2;
+	public static final int xlRight = -4152;
+	public static final int xlScale = 3;
+	public static final int xlSemiGray75 = 10;
+	public static final int xlShowLabel = 4;
+	public static final int xlShowLabelAndPercent = 5;
+	public static final int xlShowPercent = 3;
+	public static final int xlShowValue = 2;
+	public static final int xlSingle = 2;
+	public static final int xlSolid = 1;
+	public static final int xlSquare = 1;
+	public static final int xlStar = 5;
+	public static final int xlStError = 4;
+	public static final int xlTop = -4160;
+	public static final int xlTransparent = 2;
+	public static final int xlTriangle = 3;
+}
