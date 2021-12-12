538712f07398bfa050759464d000a432d0341d84
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:39:39 +0800

    Create WdMergeFormatFrom.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMergeFormatFrom.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMergeFormatFrom {
+
+	public static final int wdMergeFormatFromOriginal = 0;
+	public static final int wdMergeFormatFromRevised = 1;
+	public static final int wdMergeFormatFromPrompt = 2;
+}
