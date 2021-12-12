8a7dfd3da135612c6a0615615f97990a61d17c85
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:22 +0800

    Create WdArabicNumeral.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdArabicNumeral.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdArabicNumeral {
+
+	public static final int wdNumeralArabic = 0;
+	public static final int wdNumeralHindi = 1;
+	public static final int wdNumeralContext = 2;
+	public static final int wdNumeralSystem = 3;
+}
