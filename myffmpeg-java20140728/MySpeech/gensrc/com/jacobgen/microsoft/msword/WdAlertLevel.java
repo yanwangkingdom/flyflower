f51f2d2cc239d664ce9b97cb08a9dbf3f3bab121
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:04 +0800

    Create WdAlertLevel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdAlertLevel.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdAlertLevel {
+
+	public static final int wdAlertsNone = 0;
+	public static final int wdAlertsMessageBox = -2;
+	public static final int wdAlertsAll = -1;
+}
