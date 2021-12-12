4694ca630ecb1fa9eb78992d3b268527bef567fb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:39 +0800

    Create WdRevisedPropertiesMark.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRevisedPropertiesMark.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRevisedPropertiesMark {
+
+	public static final int wdRevisedPropertiesMarkNone = 0;
+	public static final int wdRevisedPropertiesMarkBold = 1;
+	public static final int wdRevisedPropertiesMarkItalic = 2;
+	public static final int wdRevisedPropertiesMarkUnderline = 3;
+	public static final int wdRevisedPropertiesMarkDoubleUnderline = 4;
+	public static final int wdRevisedPropertiesMarkColorOnly = 5;
+	public static final int wdRevisedPropertiesMarkStrikeThrough = 6;
+	public static final int wdRevisedPropertiesMarkDoubleStrikeThrough = 7;
+}
