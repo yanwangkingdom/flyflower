387c8db1d364f56e754a85a9134fddaba21b2f7f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:09 +0800

    Create WdExportRange.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdExportRange.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdExportRange {
+
+	public static final int wdExportAllDocument = 0;
+	public static final int wdExportSelection = 1;
+	public static final int wdExportCurrentPage = 2;
+	public static final int wdExportFromTo = 3;
+}
