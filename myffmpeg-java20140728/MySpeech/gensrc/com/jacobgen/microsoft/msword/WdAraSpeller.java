36414a5ad1287a2635c2b5f4cd7a7bdebd811c4c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:25 +0800

    Create WdAraSpeller.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdAraSpeller.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdAraSpeller {
+
+	public static final int wdNone = 0;
+	public static final int wdInitialAlef = 1;
+	public static final int wdFinalYaa = 2;
+	public static final int wdBoth = 3;
+}
