045117dbb2fc073cd9215a5a4caeb0da83c76f35
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:30 +0800

    Create WdIndexFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdIndexFormat.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdIndexFormat {
+
+	public static final int wdIndexTemplate = 0;
+	public static final int wdIndexClassic = 1;
+	public static final int wdIndexFancy = 2;
+	public static final int wdIndexModern = 3;
+	public static final int wdIndexBulleted = 4;
+	public static final int wdIndexFormal = 5;
+	public static final int wdIndexSimple = 6;
+}
