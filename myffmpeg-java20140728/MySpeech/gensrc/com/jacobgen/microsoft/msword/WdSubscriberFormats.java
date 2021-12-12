9fbd2c138369a9e5287096f2ad4c555b38cded1c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:19 +0800

    Create WdSubscriberFormats.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSubscriberFormats.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSubscriberFormats {
+
+	public static final int wdSubscriberBestFormat = 0;
+	public static final int wdSubscriberRTF = 1;
+	public static final int wdSubscriberText = 2;
+	public static final int wdSubscriberPict = 4;
+}
