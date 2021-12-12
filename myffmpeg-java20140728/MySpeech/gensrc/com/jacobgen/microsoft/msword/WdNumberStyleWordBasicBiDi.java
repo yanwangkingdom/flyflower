85061f0018cb4073d74ba2ae39ba614fc5bfeae1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:40:54 +0800

    Create WdNumberStyleWordBasicBiDi.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdNumberStyleWordBasicBiDi.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdNumberStyleWordBasicBiDi {
+
+	public static final int wdListNumberStyleBidi1 = 49;
+	public static final int wdListNumberStyleBidi2 = 50;
+	public static final int wdCaptionNumberStyleBidiLetter1 = 49;
+	public static final int wdCaptionNumberStyleBidiLetter2 = 50;
+	public static final int wdNoteNumberStyleBidiLetter1 = 49;
+	public static final int wdNoteNumberStyleBidiLetter2 = 50;
+	public static final int wdPageNumberStyleBidiLetter1 = 49;
+	public static final int wdPageNumberStyleBidiLetter2 = 50;
+}
