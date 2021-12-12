42047e37fe9f0da9afd4074d50d6fcf1ebeafd2b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:56 +0800

    Create WdInternationalIndex.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdInternationalIndex.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdInternationalIndex {
+
+	public static final int wdListSeparator = 17;
+	public static final int wdDecimalSeparator = 18;
+	public static final int wdThousandsSeparator = 19;
+	public static final int wdCurrencyCode = 20;
+	public static final int wd24HourClock = 21;
+	public static final int wdInternationalAM = 22;
+	public static final int wdInternationalPM = 23;
+	public static final int wdTimeSeparator = 24;
+	public static final int wdDateSeparator = 25;
+	public static final int wdProductLanguageID = 26;
+}
