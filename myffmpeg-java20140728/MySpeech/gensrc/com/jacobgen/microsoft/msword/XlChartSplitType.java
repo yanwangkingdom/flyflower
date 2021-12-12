393b91fcf9876935d4c71cd2e05d0d5e0c703674
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:32 +0800

    Create XlChartSplitType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlChartSplitType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlChartSplitType {
+
+	public static final int xlSplitByPosition = 1;
+	public static final int xlSplitByPercentValue = 3;
+	public static final int xlSplitByCustomSplit = 4;
+	public static final int xlSplitByValue = 2;
+}
