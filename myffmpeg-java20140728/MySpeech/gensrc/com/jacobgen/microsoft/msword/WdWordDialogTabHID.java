c1e7d84a8be17187ac5d308acc2637c9801501d5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:45 +0800

    Create WdWordDialogTabHID.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdWordDialogTabHID.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdWordDialogTabHID {
+
+	public static final int wdDialogFilePageSetupTabPaperSize = 150001;
+	public static final int wdDialogFilePageSetupTabPaperSource = 150002;
+}
