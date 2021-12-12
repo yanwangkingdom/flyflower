b21fb1e14c9cf0bdf5417a0c3639db2efb5a07c7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:55:52 +0800

    Create WdTextFormFieldType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTextFormFieldType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTextFormFieldType {
+
+	public static final int wdRegularText = 0;
+	public static final int wdNumberText = 1;
+	public static final int wdDateText = 2;
+	public static final int wdCurrentDateText = 3;
+	public static final int wdCurrentTimeText = 4;
+	public static final int wdCalculationText = 5;
+}
