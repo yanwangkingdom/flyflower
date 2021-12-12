58128b9d0d3fff5143c4713d031e01ab472366b6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:23 +0800

    Create WdListType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdListType.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdListType {
+
+	public static final int wdListNoNumbering = 0;
+	public static final int wdListListNumOnly = 1;
+	public static final int wdListBullet = 2;
+	public static final int wdListSimpleNumbering = 3;
+	public static final int wdListOutlineNumbering = 4;
+	public static final int wdListMixedNumbering = 5;
+	public static final int wdListPictureBullet = 6;
+}
