8f0efc576b19993a9ed712eefad09b740372df96
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:28 +0800

    Create XlChartPictureType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlChartPictureType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlChartPictureType {
+
+	public static final int xlStackScale = 3;
+	public static final int xlStack = 2;
+	public static final int xlStretch = 1;
+}
