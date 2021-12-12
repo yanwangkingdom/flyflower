003357d6d40b2739b7a1da4df66342630783ef08
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:50 +0800

    Create WdWrapSideType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdWrapSideType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdWrapSideType {
+
+	public static final int wdWrapBoth = 0;
+	public static final int wdWrapLeft = 1;
+	public static final int wdWrapRight = 2;
+	public static final int wdWrapLargest = 3;
+}
