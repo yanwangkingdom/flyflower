682662420634695ae8c40baaf981621c6edc049e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:09 +0800

    Create WdRelativeHorizontalPosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRelativeHorizontalPosition.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRelativeHorizontalPosition {
+
+	public static final int wdRelativeHorizontalPositionMargin = 0;
+	public static final int wdRelativeHorizontalPositionPage = 1;
+	public static final int wdRelativeHorizontalPositionColumn = 2;
+	public static final int wdRelativeHorizontalPositionCharacter = 3;
+	public static final int wdRelativeHorizontalPositionLeftMarginArea = 4;
+	public static final int wdRelativeHorizontalPositionRightMarginArea = 5;
+	public static final int wdRelativeHorizontalPositionInnerMarginArea = 6;
+	public static final int wdRelativeHorizontalPositionOuterMarginArea = 7;
+}
