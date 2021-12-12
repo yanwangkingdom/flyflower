fcebd3ae3a41820b7a9dd72a0cd374d5e655cc1f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:54 +0800

    Create WdFramesetNewFrameLocation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFramesetNewFrameLocation.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFramesetNewFrameLocation {
+
+	public static final int wdFramesetNewFrameAbove = 0;
+	public static final int wdFramesetNewFrameBelow = 1;
+	public static final int wdFramesetNewFrameRight = 2;
+	public static final int wdFramesetNewFrameLeft = 3;
+}
