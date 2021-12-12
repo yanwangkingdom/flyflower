b0ceb44339f1b87f9b053145a47c0e99b22eabd0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:31 +0800

    Create WdShowFilter.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdShowFilter.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdShowFilter {
+
+	public static final int wdShowFilterStylesAvailable = 0;
+	public static final int wdShowFilterStylesInUse = 1;
+	public static final int wdShowFilterStylesAll = 2;
+	public static final int wdShowFilterFormattingInUse = 3;
+	public static final int wdShowFilterFormattingAvailable = 4;
+	public static final int wdShowFilterFormattingRecommended = 5;
+}
