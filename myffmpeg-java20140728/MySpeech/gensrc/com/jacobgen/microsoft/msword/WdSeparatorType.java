96353995043afcc00cc74fdfd2bb147f74ef2543
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:17 +0800

    Create WdSeparatorType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSeparatorType.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSeparatorType {
+
+	public static final int wdSeparatorHyphen = 0;
+	public static final int wdSeparatorPeriod = 1;
+	public static final int wdSeparatorColon = 2;
+	public static final int wdSeparatorEmDash = 3;
+	public static final int wdSeparatorEnDash = 4;
+}
