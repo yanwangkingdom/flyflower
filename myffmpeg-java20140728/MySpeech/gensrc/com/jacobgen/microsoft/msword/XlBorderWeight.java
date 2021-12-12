b74a3513b3ae06102bde326c214485fde367203d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:59:49 +0800

    Create XlBorderWeight.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlBorderWeight.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlBorderWeight {
+
+	public static final int xlHairline = 1;
+	public static final int xlMedium = -4138;
+	public static final int xlThick = 4;
+	public static final int xlThin = 2;
+}
