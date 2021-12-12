856bdb2057e5c0cc4604cb5c4a486c4911096314
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:06 +0800

    Create WdSeekView.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSeekView.java
@@ -0,0 +1,22 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSeekView {
+
+	public static final int wdSeekMainDocument = 0;
+	public static final int wdSeekPrimaryHeader = 1;
+	public static final int wdSeekFirstPageHeader = 2;
+	public static final int wdSeekEvenPagesHeader = 3;
+	public static final int wdSeekPrimaryFooter = 4;
+	public static final int wdSeekFirstPageFooter = 5;
+	public static final int wdSeekEvenPagesFooter = 6;
+	public static final int wdSeekFootnotes = 7;
+	public static final int wdSeekEndnotes = 8;
+	public static final int wdSeekCurrentPageHeader = 9;
+	public static final int wdSeekCurrentPageFooter = 10;
+}
