9d3dbb71f561e83117808faa9617920c60e99774
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:07:41 +0800

    Create ContentControls.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/ContentControls.java
@@ -0,0 +1,123 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class ContentControls extends Dispatch {
+
+	public static final String componentName = "Word.ContentControls";
+
+	public ContentControls() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public ContentControls(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public ContentControls(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type Variant
+	 * @return the result is of type ContentControl
+	 */
+	public ContentControl item(Variant index) {
+		return new ContentControl(Dispatch.call(this, "Item", index).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param type an input-parameter of type int
+	 * @param range an input-parameter of type Variant
+	 * @return the result is of type ContentControl
+	 */
+	public ContentControl add(int type, Variant range) {
+		return new ContentControl(Dispatch.call(this, "Add", new Variant(type), range).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param type an input-parameter of type int
+	 * @return the result is of type ContentControl
+	 */
+	public ContentControl add(int type) {
+		return new ContentControl(Dispatch.call(this, "Add", new Variant(type)).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type ContentControl
+	 */
+	public ContentControl add() {
+		return new ContentControl(Dispatch.call(this, "Add").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method and receiving the output-parameter(s).
+	 * @param type an input-parameter of type int
+	 * @param range an input-parameter of type Variant
+	 * @return the result is of type ContentControl
+	 */
+	public ContentControl add(int type, Variant range) {
+		ContentControl result_of_Add = new ContentControl(Dispatch.call(this, "Add", new Variant(type), range).toDispatch());
+
+
+		return result_of_Add;
+	}
+
+}
