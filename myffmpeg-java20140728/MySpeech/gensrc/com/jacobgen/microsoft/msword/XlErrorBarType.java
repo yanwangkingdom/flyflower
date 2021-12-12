5aa2fda7b17da269303dd5073284dfda125483fb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:01:16 +0800

    Create XlErrorBarType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlErrorBarType.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlErrorBarType {
+
+	public static final int xlErrorBarTypeCustom = -4114;
+	public static final int xlErrorBarTypeFixedValue = 1;
+	public static final int xlErrorBarTypePercent = 2;
+	public static final int xlErrorBarTypeStDev = -4155;
+	public static final int xlErrorBarTypeStError = 4;
+}
