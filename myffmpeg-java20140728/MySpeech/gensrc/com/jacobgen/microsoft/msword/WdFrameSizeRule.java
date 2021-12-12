4576c7cda0f372a47bd534e407870e5801ae3460
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:35:25 +0800

    Create WdFrameSizeRule.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFrameSizeRule.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFrameSizeRule {
+
+	public static final int wdFrameAuto = 0;
+	public static final int wdFrameAtLeast = 1;
+	public static final int wdFrameExact = 2;
+}
