7a098e8e81e22a0da09bab993e3214ca60af04b8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:45 +0800

    Create WdLineSpacing.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLineSpacing.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLineSpacing {
+
+	public static final int wdLineSpaceSingle = 0;
+	public static final int wdLineSpace1pt5 = 1;
+	public static final int wdLineSpaceDouble = 2;
+	public static final int wdLineSpaceAtLeast = 3;
+	public static final int wdLineSpaceExactly = 4;
+	public static final int wdLineSpaceMultiple = 5;
+}
