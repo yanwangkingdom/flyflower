248cb3f617eada15140aec0157aaab291bcc1744
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:57 +0800

    Create WdFramesetSizeType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFramesetSizeType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFramesetSizeType {
+
+	public static final int wdFramesetSizeTypePercent = 0;
+	public static final int wdFramesetSizeTypeFixed = 1;
+	public static final int wdFramesetSizeTypeRelative = 2;
+}
