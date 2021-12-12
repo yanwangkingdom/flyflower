35b55acb5f63ffaf18154e0eaacef1ccfbd8dd03
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:32 +0800

    Create WdCompatibility.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCompatibility.java
@@ -0,0 +1,80 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCompatibility {
+
+	public static final int wdNoTabHangIndent = 1;
+	public static final int wdNoSpaceRaiseLower = 2;
+	public static final int wdPrintColBlack = 3;
+	public static final int wdWrapTrailSpaces = 4;
+	public static final int wdNoColumnBalance = 5;
+	public static final int wdConvMailMergeEsc = 6;
+	public static final int wdSuppressSpBfAfterPgBrk = 7;
+	public static final int wdSuppressTopSpacing = 8;
+	public static final int wdOrigWordTableRules = 9;
+	public static final int wdTransparentMetafiles = 10;
+	public static final int wdShowBreaksInFrames = 11;
+	public static final int wdSwapBordersFacingPages = 12;
+	public static final int wdLeaveBackslashAlone = 13;
+	public static final int wdExpandShiftReturn = 14;
+	public static final int wdDontULTrailSpace = 15;
+	public static final int wdDontBalanceSingleByteDoubleByteWidth = 16;
+	public static final int wdSuppressTopSpacingMac5 = 17;
+	public static final int wdSpacingInWholePoints = 18;
+	public static final int wdPrintBodyTextBeforeHeader = 19;
+	public static final int wdNoLeading = 20;
+	public static final int wdNoSpaceForUL = 21;
+	public static final int wdMWSmallCaps = 22;
+	public static final int wdNoExtraLineSpacing = 23;
+	public static final int wdTruncateFontHeight = 24;
+	public static final int wdSubFontBySize = 25;
+	public static final int wdUsePrinterMetrics = 26;
+	public static final int wdWW6BorderRules = 27;
+	public static final int wdExactOnTop = 28;
+	public static final int wdSuppressBottomSpacing = 29;
+	public static final int wdWPSpaceWidth = 30;
+	public static final int wdWPJustification = 31;
+	public static final int wdLineWrapLikeWord6 = 32;
+	public static final int wdShapeLayoutLikeWW8 = 33;
+	public static final int wdFootnoteLayoutLikeWW8 = 34;
+	public static final int wdDontUseHTMLParagraphAutoSpacing = 35;
+	public static final int wdDontAdjustLineHeightInTable = 36;
+	public static final int wdForgetLastTabAlignment = 37;
+	public static final int wdAutospaceLikeWW7 = 38;
+	public static final int wdAlignTablesRowByRow = 39;
+	public static final int wdLayoutRawTableWidth = 40;
+	public static final int wdLayoutTableRowsApart = 41;
+	public static final int wdUseWord97LineBreakingRules = 42;
+	public static final int wdDontBreakWrappedTables = 43;
+	public static final int wdDontSnapTextToGridInTableWithObjects = 44;
+	public static final int wdSelectFieldWithFirstOrLastCharacter = 45;
+	public static final int wdApplyBreakingRules = 46;
+	public static final int wdDontWrapTextWithPunctuation = 47;
+	public static final int wdDontUseAsianBreakRulesInGrid = 48;
+	public static final int wdUseWord2002TableStyleRules = 49;
+	public static final int wdGrowAutofit = 50;
+	public static final int wdUseNormalStyleForList = 51;
+	public static final int wdDontUseIndentAsNumberingTabStop = 52;
+	public static final int wdFELineBreak11 = 53;
+	public static final int wdAllowSpaceOfSameStyleInTable = 54;
+	public static final int wdWW11IndentRules = 55;
+	public static final int wdDontAutofitConstrainedTables = 56;
+	public static final int wdAutofitLikeWW11 = 57;
+	public static final int wdUnderlineTabInNumList = 58;
+	public static final int wdHangulWidthLikeWW11 = 59;
+	public static final int wdSplitPgBreakAndParaMark = 60;
+	public static final int wdDontVertAlignCellWithShape = 61;
+	public static final int wdDontBreakConstrainedForcedTables = 62;
+	public static final int wdDontVertAlignInTextbox = 63;
+	public static final int wdWord11KerningPairs = 64;
+	public static final int wdCachedColBalance = 65;
+	public static final int wdDisableOTKerning = 66;
+	public static final int wdFlipMirrorIndents = 67;
+	public static final int wdDontOverrideTableStyleFontSzAndJustification = 68;
+	public static final int wdUseWord2010TableStyleRules = 69;
+}
