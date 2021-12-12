8430ebe79b5a54a87939783087b396f0ac49db06
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:25 +0800

    Create WdPrintOutItem.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPrintOutItem.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPrintOutItem {
+
+	public static final int wdPrintDocumentContent = 0;
+	public static final int wdPrintProperties = 1;
+	public static final int wdPrintComments = 2;
+	public static final int wdPrintMarkup = 2;
+	public static final int wdPrintStyles = 3;
+	public static final int wdPrintAutoTextEntries = 4;
+	public static final int wdPrintKeyAssignments = 5;
+	public static final int wdPrintEnvelope = 6;
+	public static final int wdPrintDocumentWithMarkup = 7;
+}
