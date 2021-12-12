8c6e7a07ca77465b81ffbf6aaa8d4d13638a025e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:08 +0800

    Create WdDocumentViewDirection.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDocumentViewDirection.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDocumentViewDirection {
+
+	public static final int wdDocumentViewRtl = 0;
+	public static final int wdDocumentViewLtr = 1;
+}
