a10dc5dbc503d0b4316bc0cf4b6599be78dce44d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:02 +0800

    Create WdExportItem.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdExportItem.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdExportItem {
+
+	public static final int wdExportDocumentContent = 0;
+	public static final int wdExportDocumentWithMarkup = 7;
+}
