2c15ecbceab9751c47bc6202bc3db2cac754d8f9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:45:52 +0800

    Create WdOriginalFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOriginalFormat.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOriginalFormat {
+
+	public static final int wdWordDocument = 0;
+	public static final int wdOriginalDocumentFormat = 1;
+	public static final int wdPromptUser = 2;
+}
