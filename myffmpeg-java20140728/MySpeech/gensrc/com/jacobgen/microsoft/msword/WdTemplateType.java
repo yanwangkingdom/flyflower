b5fcb76da59c02b627ccbee7f6af1cbfd468a7fd
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:55:45 +0800

    Create WdTemplateType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTemplateType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTemplateType {
+
+	public static final int wdNormalTemplate = 0;
+	public static final int wdGlobalTemplate = 1;
+	public static final int wdAttachedTemplate = 2;
+}
