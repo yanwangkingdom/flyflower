64f14f7c3e3fd98225715e62ca8343c6e2684ee3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:14 +0800

    Create WdViewType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdViewType.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdViewType {
+
+	public static final int wdNormalView = 1;
+	public static final int wdOutlineView = 2;
+	public static final int wdPrintView = 3;
+	public static final int wdPrintPreview = 4;
+	public static final int wdMasterView = 5;
+	public static final int wdWebView = 6;
+	public static final int wdReadingView = 7;
+	public static final int wdConflictView = 8;
+}
