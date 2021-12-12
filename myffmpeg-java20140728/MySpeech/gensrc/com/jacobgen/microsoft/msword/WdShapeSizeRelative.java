e8ff96776d0bd5adbbf8beba381b486b351ea843
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:27 +0800

    Create WdShapeSizeRelative.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdShapeSizeRelative.java
@@ -0,0 +1,12 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdShapeSizeRelative {
+
+	public static final int wdShapeSizeRelativeNone = -999999;
+}
