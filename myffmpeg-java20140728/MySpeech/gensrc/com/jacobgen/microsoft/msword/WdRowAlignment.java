50afe00ebd67e685fa0c784e111e13fb4d221233
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:20 +0800

    Create WdRowAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRowAlignment.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRowAlignment {
+
+	public static final int wdAlignRowLeft = 0;
+	public static final int wdAlignRowCenter = 1;
+	public static final int wdAlignRowRight = 2;
+}
