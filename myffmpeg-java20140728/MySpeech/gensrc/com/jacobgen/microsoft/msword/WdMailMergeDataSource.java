90031fe26c6adef974a38003489af83d55b7e7a0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:41 +0800

    Create WdMailMergeDataSource.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailMergeDataSource.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailMergeDataSource {
+
+	public static final int wdNoMergeInfo = -1;
+	public static final int wdMergeInfoFromWord = 0;
+	public static final int wdMergeInfoFromAccessDDE = 1;
+	public static final int wdMergeInfoFromExcelDDE = 2;
+	public static final int wdMergeInfoFromMSQueryDDE = 3;
+	public static final int wdMergeInfoFromODBC = 4;
+	public static final int wdMergeInfoFromODSO = 5;
+}
