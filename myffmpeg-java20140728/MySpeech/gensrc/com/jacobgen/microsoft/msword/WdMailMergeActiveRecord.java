20c1048af721d01d2c56c0751e32aea8438d66b2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:34 +0800

    Create WdMailMergeActiveRecord.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailMergeActiveRecord.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailMergeActiveRecord {
+
+	public static final int wdNoActiveRecord = -1;
+	public static final int wdNextRecord = -2;
+	public static final int wdPreviousRecord = -3;
+	public static final int wdFirstRecord = -4;
+	public static final int wdLastRecord = -5;
+	public static final int wdFirstDataSourceRecord = -6;
+	public static final int wdLastDataSourceRecord = -7;
+	public static final int wdNextDataSourceRecord = -8;
+	public static final int wdPreviousDataSourceRecord = -9;
+}
