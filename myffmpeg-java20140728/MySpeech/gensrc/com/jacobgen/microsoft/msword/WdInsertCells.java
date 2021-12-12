e147d1f47c9422d53a4f9db95dfc747e69163f42
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:49 +0800

    Create WdInsertCells.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdInsertCells.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdInsertCells {
+
+	public static final int wdInsertCellsShiftRight = 0;
+	public static final int wdInsertCellsShiftDown = 1;
+	public static final int wdInsertCellsEntireRow = 2;
+	public static final int wdInsertCellsEntireColumn = 3;
+}
