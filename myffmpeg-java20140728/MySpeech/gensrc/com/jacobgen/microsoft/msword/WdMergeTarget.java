0bc67c2ebe745d3d61a5c900524a249b4bf72fea
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:40:11 +0800

    Create WdMergeTarget.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMergeTarget.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMergeTarget {
+
+	public static final int wdMergeTargetSelected = 0;
+	public static final int wdMergeTargetCurrent = 1;
+	public static final int wdMergeTargetNew = 2;
+}
