029a4f56a433abce0246ec527e6eec5b36cfaf2e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:45 +0800

    Create WdInlineShapeType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdInlineShapeType.java
@@ -0,0 +1,27 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdInlineShapeType {
+
+	public static final int wdInlineShapeEmbeddedOLEObject = 1;
+	public static final int wdInlineShapeLinkedOLEObject = 2;
+	public static final int wdInlineShapePicture = 3;
+	public static final int wdInlineShapeLinkedPicture = 4;
+	public static final int wdInlineShapeOLEControlObject = 5;
+	public static final int wdInlineShapeHorizontalLine = 6;
+	public static final int wdInlineShapePictureHorizontalLine = 7;
+	public static final int wdInlineShapeLinkedPictureHorizontalLine = 8;
+	public static final int wdInlineShapePictureBullet = 9;
+	public static final int wdInlineShapeScriptAnchor = 10;
+	public static final int wdInlineShapeOWSAnchor = 11;
+	public static final int wdInlineShapeChart = 12;
+	public static final int wdInlineShapeDiagram = 13;
+	public static final int wdInlineShapeLockedCanvas = 14;
+	public static final int wdInlineShapeSmartArt = 15;
+	public static final int wdInlineShapeWebVideo = 16;
+}
