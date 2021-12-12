edf0d3bd62afc5ecab4916e5d373219625d0d351
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:46:23 +0800

    Create WdOutlineLevel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOutlineLevel.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOutlineLevel {
+
+	public static final int wdOutlineLevel1 = 1;
+	public static final int wdOutlineLevel2 = 2;
+	public static final int wdOutlineLevel3 = 3;
+	public static final int wdOutlineLevel4 = 4;
+	public static final int wdOutlineLevel5 = 5;
+	public static final int wdOutlineLevel6 = 6;
+	public static final int wdOutlineLevel7 = 7;
+	public static final int wdOutlineLevel8 = 8;
+	public static final int wdOutlineLevel9 = 9;
+	public static final int wdOutlineLevelBodyText = 10;
+}
