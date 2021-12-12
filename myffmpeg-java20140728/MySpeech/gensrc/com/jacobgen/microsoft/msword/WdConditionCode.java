835c7bfd3dd71314b81f23a3c7f5a20fa5eecb65
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:39 +0800

    Create WdConditionCode.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdConditionCode.java
@@ -0,0 +1,23 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdConditionCode {
+
+	public static final int wdFirstRow = 0;
+	public static final int wdLastRow = 1;
+	public static final int wdOddRowBanding = 2;
+	public static final int wdEvenRowBanding = 3;
+	public static final int wdFirstColumn = 4;
+	public static final int wdLastColumn = 5;
+	public static final int wdOddColumnBanding = 6;
+	public static final int wdEvenColumnBanding = 7;
+	public static final int wdNECell = 8;
+	public static final int wdNWCell = 9;
+	public static final int wdSECell = 10;
+	public static final int wdSWCell = 11;
+}
