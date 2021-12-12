3c47fe64c798b03e3c02792f5faaaeafccf76e30
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:54 +0800

    Create WdUnits.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdUnits.java
@@ -0,0 +1,27 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdUnits {
+
+	public static final int wdCharacter = 1;
+	public static final int wdWord = 2;
+	public static final int wdSentence = 3;
+	public static final int wdParagraph = 4;
+	public static final int wdLine = 5;
+	public static final int wdStory = 6;
+	public static final int wdScreen = 7;
+	public static final int wdSection = 8;
+	public static final int wdColumn = 9;
+	public static final int wdRow = 10;
+	public static final int wdWindow = 11;
+	public static final int wdCell = 12;
+	public static final int wdCharacterFormatting = 13;
+	public static final int wdParagraphFormatting = 14;
+	public static final int wdTable = 15;
+	public static final int wdItem = 16;
+}
