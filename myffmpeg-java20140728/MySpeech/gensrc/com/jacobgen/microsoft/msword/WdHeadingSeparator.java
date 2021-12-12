fd4c072eda406350533e9105e457c6db60c7f85d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:35:53 +0800

    Create WdHeadingSeparator.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdHeadingSeparator.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdHeadingSeparator {
+
+	public static final int wdHeadingSeparatorNone = 0;
+	public static final int wdHeadingSeparatorBlankLine = 1;
+	public static final int wdHeadingSeparatorLetter = 2;
+	public static final int wdHeadingSeparatorLetterLow = 3;
+	public static final int wdHeadingSeparatorLetterFull = 4;
+}
