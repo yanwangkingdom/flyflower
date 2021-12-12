b674318213b153dda0bf5ebdd4314939af701734
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:45 +0800

    Create WdProtectionType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdProtectionType.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdProtectionType {
+
+	public static final int wdNoProtection = -1;
+	public static final int wdAllowOnlyRevisions = 0;
+	public static final int wdAllowOnlyComments = 1;
+	public static final int wdAllowOnlyFormFields = 2;
+	public static final int wdAllowOnlyReading = 3;
+}
