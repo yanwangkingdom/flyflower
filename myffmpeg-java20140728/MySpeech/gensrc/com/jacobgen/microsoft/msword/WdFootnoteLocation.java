3cae0ba535d2be256f82a8c4d14fe72aba2f1186
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:47 +0800

    Create WdFootnoteLocation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFootnoteLocation.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFootnoteLocation {
+
+	public static final int wdBottomOfPage = 0;
+	public static final int wdBeneathText = 1;
+}
