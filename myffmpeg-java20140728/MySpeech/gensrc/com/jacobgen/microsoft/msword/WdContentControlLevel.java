f5b5d3ff17001ed9d98bc44e03c6008fc8ebbbe2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:52 +0800

    Create WdContentControlLevel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdContentControlLevel.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdContentControlLevel {
+
+	public static final int wdContentControlLevelInline = 0;
+	public static final int wdContentControlLevelParagraph = 1;
+	public static final int wdContentControlLevelRow = 2;
+	public static final int wdContentControlLevelCell = 3;
+}
