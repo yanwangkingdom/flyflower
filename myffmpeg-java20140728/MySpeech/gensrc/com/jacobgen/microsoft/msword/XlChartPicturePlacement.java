d271a4bb2db292dba90f3e0f8684c737579e01dc
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:12 +0800

    Create XlChartPicturePlacement.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlChartPicturePlacement.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlChartPicturePlacement {
+
+	public static final int xlSides = 1;
+	public static final int xlEnd = 2;
+	public static final int xlEndSides = 3;
+	public static final int xlFront = 4;
+	public static final int xlFrontSides = 5;
+	public static final int xlFrontEnd = 6;
+	public static final int xlAllFaces = 7;
+}
