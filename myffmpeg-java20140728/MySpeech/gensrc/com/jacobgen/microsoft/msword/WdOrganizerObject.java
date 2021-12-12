d7980b0a6757c94433545a25ade51d28fdc09f6e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:45:42 +0800

    Create WdOrganizerObject.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOrganizerObject.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOrganizerObject {
+
+	public static final int wdOrganizerObjectStyles = 0;
+	public static final int wdOrganizerObjectAutoText = 1;
+	public static final int wdOrganizerObjectCommandBars = 2;
+	public static final int wdOrganizerObjectProjectItems = 3;
+}
