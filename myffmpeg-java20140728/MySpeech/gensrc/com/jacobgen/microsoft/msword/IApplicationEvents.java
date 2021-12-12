25d97b066524506296c5a799e28d6eb5cd403a80
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:15:14 +0800

    Create IApplicationEvents.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/IApplicationEvents.java
@@ -0,0 +1,55 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class IApplicationEvents extends Dispatch {
+
+	public static final String componentName = "Word.IApplicationEvents";
+
+	public IApplicationEvents() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public IApplicationEvents(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public IApplicationEvents(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 */
+	public void startup() {
+		Dispatch.call(this, "Startup");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 */
+	public void quit() {
+		Dispatch.call(this, "Quit");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 */
+	public void documentChange() {
+		Dispatch.call(this, "DocumentChange");
+	}
+
+}
