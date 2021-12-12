3f1437e97a6abd08c9f60664b3fd42bde83e88c4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:52 +0800

    Create WdInsertedTextMark.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdInsertedTextMark.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdInsertedTextMark {
+
+	public static final int wdInsertedTextMarkNone = 0;
+	public static final int wdInsertedTextMarkBold = 1;
+	public static final int wdInsertedTextMarkItalic = 2;
+	public static final int wdInsertedTextMarkUnderline = 3;
+	public static final int wdInsertedTextMarkDoubleUnderline = 4;
+	public static final int wdInsertedTextMarkColorOnly = 5;
+	public static final int wdInsertedTextMarkStrikeThrough = 6;
+	public static final int wdInsertedTextMarkDoubleStrikeThrough = 7;
+}
