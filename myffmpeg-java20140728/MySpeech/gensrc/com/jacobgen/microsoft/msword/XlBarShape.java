40da7f94e14830fd66626fa4f5ab36acf88f4108
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:59:00 +0800

    Create XlBarShape.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlBarShape.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlBarShape {
+
+	public static final int xlBox = 0;
+	public static final int xlPyramidToPoint = 1;
+	public static final int xlPyramidToMax = 2;
+	public static final int xlCylinder = 3;
+	public static final int xlConeToPoint = 4;
+	public static final int xlConeToMax = 5;
+}
