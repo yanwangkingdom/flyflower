d51c91c16eff1105b96761565dd7ddc8fb85c90f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:35 +0800

    Create WdSalutationType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSalutationType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSalutationType {
+
+	public static final int wdSalutationInformal = 0;
+	public static final int wdSalutationFormal = 1;
+	public static final int wdSalutationBusiness = 2;
+	public static final int wdSalutationOther = 3;
+}
