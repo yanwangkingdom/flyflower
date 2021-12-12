f1b76ab10e4734a30e968b039f0fe9cb4a73856b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:52 +0800

    Create WdEnvelopeOrientation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEnvelopeOrientation.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEnvelopeOrientation {
+
+	public static final int wdLeftPortrait = 0;
+	public static final int wdCenterPortrait = 1;
+	public static final int wdRightPortrait = 2;
+	public static final int wdLeftLandscape = 3;
+	public static final int wdCenterLandscape = 4;
+	public static final int wdRightLandscape = 5;
+	public static final int wdLeftClockwise = 6;
+	public static final int wdCenterClockwise = 7;
+	public static final int wdRightClockwise = 8;
+}
