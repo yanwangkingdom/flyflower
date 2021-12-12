e605145c70796700f743fe2cc7ee4b46d373cb74
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:59 +0800

    Create WdExportFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdExportFormat.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdExportFormat {
+
+	public static final int wdExportFormatPDF = 17;
+	public static final int wdExportFormatXPS = 18;
+}
