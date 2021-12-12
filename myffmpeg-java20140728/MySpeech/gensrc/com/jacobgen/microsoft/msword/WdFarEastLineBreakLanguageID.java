0bfb8eec273416ef9b88b1cb960428cc7bbef192
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:12 +0800

    Create WdFarEastLineBreakLanguageID.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFarEastLineBreakLanguageID.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFarEastLineBreakLanguageID {
+
+	public static final int wdLineBreakJapanese = 1041;
+	public static final int wdLineBreakKorean = 1042;
+	public static final int wdLineBreakSimplifiedChinese = 2052;
+	public static final int wdLineBreakTraditionalChinese = 1028;
+}
