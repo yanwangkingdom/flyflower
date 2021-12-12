e25c60c4e7f449a37d0e3540b92396c8e189b44e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:35 +0800

    Create WdAutoMacros.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdAutoMacros.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdAutoMacros {
+
+	public static final int wdAutoExec = 0;
+	public static final int wdAutoNew = 1;
+	public static final int wdAutoOpen = 2;
+	public static final int wdAutoClose = 3;
+	public static final int wdAutoExit = 4;
+	public static final int wdAutoSync = 5;
+}
