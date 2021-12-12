7438bbf5ad77885b3ecfe4ce1fcfbbe7d448f437
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:12 +0800

    Create WdRelativeHorizontalSize.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRelativeHorizontalSize.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRelativeHorizontalSize {
+
+	public static final int wdRelativeHorizontalSizeMargin = 0;
+	public static final int wdRelativeHorizontalSizePage = 1;
+	public static final int wdRelativeHorizontalSizeLeftMarginArea = 2;
+	public static final int wdRelativeHorizontalSizeRightMarginArea = 3;
+	public static final int wdRelativeHorizontalSizeInnerMarginArea = 4;
+	public static final int wdRelativeHorizontalSizeOuterMarginArea = 5;
+}
