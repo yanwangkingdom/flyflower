4ab504c8b4a0b8020563dc549cb8041f8abf504e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:26 +0800

    Create WdOMathHorizAlignType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathHorizAlignType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathHorizAlignType {
+
+	public static final int wdOMathHorizAlignCenter = 0;
+	public static final int wdOMathHorizAlignLeft = 1;
+	public static final int wdOMathHorizAlignRight = 2;
+}
