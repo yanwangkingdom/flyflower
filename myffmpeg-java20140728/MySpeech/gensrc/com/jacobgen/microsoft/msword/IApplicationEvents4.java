c8b8909563185da91837c81295991d8bb8ce6f87
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:15:27 +0800

    Create IApplicationEvents4.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/IApplicationEvents4.java
@@ -0,0 +1,34 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class IApplicationEvents4 extends Dispatch {
+
+	public static final String componentName = "Word.IApplicationEvents4";
+
+	public IApplicationEvents4() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public IApplicationEvents4(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public IApplicationEvents4(String compName) {
+		super(compName);
+	}
+
+}
