9b4ffc6d30a6296a82771a3737174169b7d55844
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:46 +0800

    Create WdContentControlAppearance.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdContentControlAppearance.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdContentControlAppearance {
+
+	public static final int wdContentControlBoundingBox = 0;
+	public static final int wdContentControlTags = 1;
+	public static final int wdContentControlHidden = 2;
+}
