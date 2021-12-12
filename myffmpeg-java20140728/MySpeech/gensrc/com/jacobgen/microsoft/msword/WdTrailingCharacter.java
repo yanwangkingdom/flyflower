2ac3dfee166bfbe2bc391f2099ea0187a876eb3e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:39 +0800

    Create WdTrailingCharacter.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTrailingCharacter.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTrailingCharacter {
+
+	public static final int wdTrailingTab = 0;
+	public static final int wdTrailingSpace = 1;
+	public static final int wdTrailingNone = 2;
+}
