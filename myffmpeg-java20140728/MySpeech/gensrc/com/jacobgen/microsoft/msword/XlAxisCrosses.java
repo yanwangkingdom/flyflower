2948df0230f5a6da9e49fb40ca30fbe4bb9ea9f4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:58:45 +0800

    Create XlAxisCrosses.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlAxisCrosses.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlAxisCrosses {
+
+	public static final int xlAxisCrossesAutomatic = -4105;
+	public static final int xlAxisCrossesCustom = -4114;
+	public static final int xlAxisCrossesMaximum = 2;
+	public static final int xlAxisCrossesMinimum = 4;
+}
