ba52016a16fb6c96b7531eb92c1824d44719247f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:55 +0800

    Create WdRecoveryType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRecoveryType.java
@@ -0,0 +1,29 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRecoveryType {
+
+	public static final int wdPasteDefault = 0;
+	public static final int wdSingleCellText = 5;
+	public static final int wdSingleCellTable = 6;
+	public static final int wdListContinueNumbering = 7;
+	public static final int wdListRestartNumbering = 8;
+	public static final int wdTableInsertAsRows = 11;
+	public static final int wdTableAppendTable = 10;
+	public static final int wdTableOriginalFormatting = 12;
+	public static final int wdChartPicture = 13;
+	public static final int wdChart = 14;
+	public static final int wdChartLinked = 15;
+	public static final int wdFormatOriginalFormatting = 16;
+	public static final int wdFormatSurroundingFormattingWithEmphasis = 20;
+	public static final int wdFormatPlainText = 22;
+	public static final int wdTableOverwriteCells = 23;
+	public static final int wdListCombineWithExistingList = 24;
+	public static final int wdListDontMerge = 25;
+	public static final int wdUseDestinationStylesRecovery = 19;
+}
