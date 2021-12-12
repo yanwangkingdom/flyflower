afa3e8315ddd5ac136957a1a10808b2f7a2078e7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:40:58 +0800

    Create WdNumberType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdNumberType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdNumberType {
+
+	public static final int wdNumberParagraph = 1;
+	public static final int wdNumberListNum = 2;
+	public static final int wdNumberAllNumbers = 3;
+}
