e78f0d7349116955491f17a24c6ecd1ba3240e25
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:16 +0800

    Create WdStylisticSet.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdStylisticSet.java
@@ -0,0 +1,32 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdStylisticSet {
+
+	public static final int wdStylisticSetDefault = 0;
+	public static final int wdStylisticSet01 = 1;
+	public static final int wdStylisticSet02 = 2;
+	public static final int wdStylisticSet03 = 4;
+	public static final int wdStylisticSet04 = 8;
+	public static final int wdStylisticSet05 = 16;
+	public static final int wdStylisticSet06 = 32;
+	public static final int wdStylisticSet07 = 64;
+	public static final int wdStylisticSet08 = 128;
+	public static final int wdStylisticSet09 = 256;
+	public static final int wdStylisticSet10 = 512;
+	public static final int wdStylisticSet11 = 1024;
+	public static final int wdStylisticSet12 = 2048;
+	public static final int wdStylisticSet13 = 4096;
+	public static final int wdStylisticSet14 = 8192;
+	public static final int wdStylisticSet15 = 16384;
+	public static final int wdStylisticSet16 = 32768;
+	public static final int wdStylisticSet17 = 65536;
+	public static final int wdStylisticSet18 = 131072;
+	public static final int wdStylisticSet19 = 262144;
+	public static final int wdStylisticSet20 = 524288;
+}
