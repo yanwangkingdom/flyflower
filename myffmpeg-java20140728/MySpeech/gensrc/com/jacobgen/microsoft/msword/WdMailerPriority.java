5f0ce2371759db067061302dc776efdfbf35ce40
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:30 +0800

    Create WdMailerPriority.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailerPriority.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailerPriority {
+
+	public static final int wdPriorityNormal = 1;
+	public static final int wdPriorityLow = 2;
+	public static final int wdPriorityHigh = 3;
+}
