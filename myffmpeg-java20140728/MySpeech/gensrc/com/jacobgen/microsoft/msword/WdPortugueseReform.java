b6bc4e1823560042f378d978424a2e584405a6b4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:18 +0800

    Create WdPortugueseReform.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPortugueseReform.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPortugueseReform {
+
+	public static final int wdPortuguesePreReform = 1;
+	public static final int wdPortuguesePostReform = 2;
+	public static final int wdPortugueseBoth = 3;
+}
