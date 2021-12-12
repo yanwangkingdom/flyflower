6a68ffe6367e1673b1c0785d8f09219cc3f79408
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:16 +0800

    Create WdDropPosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDropPosition.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDropPosition {
+
+	public static final int wdDropNone = 0;
+	public static final int wdDropNormal = 1;
+	public static final int wdDropMargin = 2;
+}
