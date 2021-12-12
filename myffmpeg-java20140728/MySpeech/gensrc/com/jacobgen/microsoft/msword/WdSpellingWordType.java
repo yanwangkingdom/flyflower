91d2c16adaddd80e5ba1f46ab0421266273ac05d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:53:45 +0800

    Create WdSpellingWordType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSpellingWordType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSpellingWordType {
+
+	public static final int wdSpellword = 0;
+	public static final int wdWildcard = 1;
+	public static final int wdAnagram = 2;
+}
