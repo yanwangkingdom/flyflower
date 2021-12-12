71d124a01dbe54098e79a2108212f090bcb30779
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:18 +0800

    Create WdRelativeVerticalPosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRelativeVerticalPosition.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRelativeVerticalPosition {
+
+	public static final int wdRelativeVerticalPositionMargin = 0;
+	public static final int wdRelativeVerticalPositionPage = 1;
+	public static final int wdRelativeVerticalPositionParagraph = 2;
+	public static final int wdRelativeVerticalPositionLine = 3;
+	public static final int wdRelativeVerticalPositionTopMarginArea = 4;
+	public static final int wdRelativeVerticalPositionBottomMarginArea = 5;
+	public static final int wdRelativeVerticalPositionInnerMarginArea = 6;
+	public static final int wdRelativeVerticalPositionOuterMarginArea = 7;
+}
