2d6b91b46f0959f8d127e390a88cb5f41ab6e35a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:19 +0800

    Create WdOMathFracType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathFracType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathFracType {
+
+	public static final int wdOMathFracBar = 0;
+	public static final int wdOMathFracNoBar = 1;
+	public static final int wdOMathFracSkw = 2;
+	public static final int wdOMathFracLin = 3;
+}
