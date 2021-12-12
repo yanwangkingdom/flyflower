d271f78a8fe0cf356ec59dfd48d05d4fa8ac4ab5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:12 +0800

    Create WdListLevelAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdListLevelAlignment.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdListLevelAlignment {
+
+	public static final int wdListLevelAlignLeft = 0;
+	public static final int wdListLevelAlignCenter = 1;
+	public static final int wdListLevelAlignRight = 2;
+}
