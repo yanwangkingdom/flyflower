d31f0f148c88a515c4203908fe4de25f27121e90
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:30 +0800

    Create WdOMathJc.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOMathJc.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOMathJc {
+
+	public static final int wdOMathJcCenterGroup = 1;
+	public static final int wdOMathJcCenter = 2;
+	public static final int wdOMathJcLeft = 3;
+	public static final int wdOMathJcRight = 4;
+	public static final int wdOMathJcInline = 7;
+}
