24c02f65fb8f8e2174cb5605abfefb726561668d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:59 +0800

    Create WdContinue.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdContinue.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdContinue {
+
+	public static final int wdContinueDisabled = 0;
+	public static final int wdResetList = 1;
+	public static final int wdContinueList = 2;
+}
