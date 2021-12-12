7bde306ce2477d57c1ab7e4f72abdd2887c3164b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:40:15 +0800

    Create WdMonthNames.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMonthNames.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMonthNames {
+
+	public static final int wdMonthNamesArabic = 0;
+	public static final int wdMonthNamesEnglish = 1;
+	public static final int wdMonthNamesFrench = 2;
+}
