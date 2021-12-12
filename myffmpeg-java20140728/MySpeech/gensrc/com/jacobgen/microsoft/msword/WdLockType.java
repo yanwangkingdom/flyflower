8571cd9d5054a28ffb501a576d798b92d61106f8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:27 +0800

    Create WdLockType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLockType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLockType {
+
+	public static final int wdLockNone = 0;
+	public static final int wdLockReservation = 1;
+	public static final int wdLockEphemeral = 2;
+	public static final int wdLockChanged = 3;
+}
