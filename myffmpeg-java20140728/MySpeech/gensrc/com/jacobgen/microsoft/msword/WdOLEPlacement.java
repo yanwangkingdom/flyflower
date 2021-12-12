d54a0ded591d1231302390356979a9821d975fcb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:41:02 +0800

    Create WdOLEPlacement.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdOLEPlacement.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdOLEPlacement {
+
+	public static final int wdInLine = 0;
+	public static final int wdFloatOverText = 1;
+}
