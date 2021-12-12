ea59df0a8d21e9c9adbe10d9a4fea081e6812207
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:54:54 +0800

    Create WdTableFieldSeparator.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTableFieldSeparator.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTableFieldSeparator {
+
+	public static final int wdSeparateByParagraphs = 0;
+	public static final int wdSeparateByTabs = 1;
+	public static final int wdSeparateByCommas = 2;
+	public static final int wdSeparateByDefaultListSeparator = 3;
+}
