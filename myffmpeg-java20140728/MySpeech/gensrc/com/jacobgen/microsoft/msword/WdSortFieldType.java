ccab1190f2b43bf6c98743783beb6e4c2447dd07
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:42 +0800

    Create WdSortFieldType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSortFieldType.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSortFieldType {
+
+	public static final int wdSortFieldAlphanumeric = 0;
+	public static final int wdSortFieldNumeric = 1;
+	public static final int wdSortFieldDate = 2;
+	public static final int wdSortFieldSyllable = 3;
+	public static final int wdSortFieldJapanJIS = 4;
+	public static final int wdSortFieldStroke = 5;
+	public static final int wdSortFieldKoreaKS = 6;
+}
