43872cc5a38c6aa1a611a47e8dee3fcffba26554
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:36:19 +0800

    Create WdHorizontalLineWidthType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdHorizontalLineWidthType.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdHorizontalLineWidthType {
+
+	public static final int wdHorizontalLinePercentWidth = -1;
+	public static final int wdHorizontalLineFixedWidth = -2;
+}
