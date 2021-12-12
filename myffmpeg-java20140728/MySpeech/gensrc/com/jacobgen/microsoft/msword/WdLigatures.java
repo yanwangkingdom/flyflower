9c7076953498c2510d683560f593e9a598562343
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:36 +0800

    Create WdLigatures.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLigatures.java
@@ -0,0 +1,27 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLigatures {
+
+	public static final int wdLigaturesNone = 0;
+	public static final int wdLigaturesStandard = 1;
+	public static final int wdLigaturesContextual = 2;
+	public static final int wdLigaturesHistorical = 4;
+	public static final int wdLigaturesDiscretional = 8;
+	public static final int wdLigaturesStandardContextual = 3;
+	public static final int wdLigaturesStandardHistorical = 5;
+	public static final int wdLigaturesContextualHistorical = 6;
+	public static final int wdLigaturesStandardDiscretional = 9;
+	public static final int wdLigaturesContextualDiscretional = 10;
+	public static final int wdLigaturesHistoricalDiscretional = 12;
+	public static final int wdLigaturesStandardContextualHistorical = 7;
+	public static final int wdLigaturesStandardContextualDiscretional = 11;
+	public static final int wdLigaturesStandardHistoricalDiscretional = 13;
+	public static final int wdLigaturesContextualHistoricalDiscretional = 14;
+	public static final int wdLigaturesAll = 15;
+}
