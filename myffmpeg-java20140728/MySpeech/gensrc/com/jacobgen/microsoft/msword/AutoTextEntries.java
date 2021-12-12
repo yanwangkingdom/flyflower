b0c10780005e525367820f3fdf6cd4a8eca0be7f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 21:57:19 +0800

    Create AutoTextEntries.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/AutoTextEntries.java
@@ -0,0 +1,102 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class AutoTextEntries extends Dispatch {
+
+	public static final String componentName = "Word.AutoTextEntries";
+
+	public AutoTextEntries() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public AutoTextEntries(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public AutoTextEntries(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type Variant
+	 * @return the result is of type AutoTextEntry
+	 */
+	public AutoTextEntry item(Variant index) {
+		return new AutoTextEntry(Dispatch.call(this, "Item", index).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @param range an input-parameter of type Range
+	 * @return the result is of type AutoTextEntry
+	 */
+	public AutoTextEntry add(String name, Range range) {
+		return new AutoTextEntry(Dispatch.call(this, "Add", name, range).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param range an input-parameter of type Range
+	 * @return the result is of type AutoTextEntry
+	 */
+	public AutoTextEntry appendToSpike(Range range) {
+		return new AutoTextEntry(Dispatch.call(this, "AppendToSpike", range).toDispatch());
+	}
+
+}
