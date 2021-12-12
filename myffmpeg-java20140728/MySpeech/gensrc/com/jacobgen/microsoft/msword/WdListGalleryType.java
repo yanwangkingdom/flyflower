ae2338abadb0f4b1ae2381e181c22fd0fffa563c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:08 +0800

    Create WdListGalleryType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdListGalleryType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdListGalleryType {
+
+	public static final int wdBulletGallery = 1;
+	public static final int wdNumberGallery = 2;
+	public static final int wdOutlineNumberGallery = 3;
+}
