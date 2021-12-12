296bc4514d9a742afd2dbbc16147e07d6d6fa7b5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:35 +0800

    Create WdTofFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTofFormat.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTofFormat {
+
+	public static final int wdTOFTemplate = 0;
+	public static final int wdTOFClassic = 1;
+	public static final int wdTOFDistinctive = 2;
+	public static final int wdTOFCentered = 3;
+	public static final int wdTOFFormal = 4;
+	public static final int wdTOFSimple = 5;
+}
