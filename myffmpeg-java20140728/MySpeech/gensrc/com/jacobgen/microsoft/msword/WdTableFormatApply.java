eb72d2014cf8a0ee87e017166848a1f75ca6be2d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:55:28 +0800

    Create WdTableFormatApply.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdTableFormatApply.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdTableFormatApply {
+
+	public static final int wdTableFormatApplyBorders = 1;
+	public static final int wdTableFormatApplyShading = 2;
+	public static final int wdTableFormatApplyFont = 4;
+	public static final int wdTableFormatApplyColor = 8;
+	public static final int wdTableFormatApplyAutoFit = 16;
+	public static final int wdTableFormatApplyHeadingRows = 32;
+	public static final int wdTableFormatApplyLastRow = 64;
+	public static final int wdTableFormatApplyFirstColumn = 128;
+	public static final int wdTableFormatApplyLastColumn = 256;
+}
