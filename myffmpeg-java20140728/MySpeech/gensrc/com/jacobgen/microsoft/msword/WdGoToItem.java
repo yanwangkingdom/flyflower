277bffbd2a274d6a03813b35796aab96ebe8e564
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:35:36 +0800

    Create WdGoToItem.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdGoToItem.java
@@ -0,0 +1,28 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdGoToItem {
+
+	public static final int wdGoToBookmark = -1;
+	public static final int wdGoToSection = 0;
+	public static final int wdGoToPage = 1;
+	public static final int wdGoToTable = 2;
+	public static final int wdGoToLine = 3;
+	public static final int wdGoToFootnote = 4;
+	public static final int wdGoToEndnote = 5;
+	public static final int wdGoToComment = 6;
+	public static final int wdGoToField = 7;
+	public static final int wdGoToGraphic = 8;
+	public static final int wdGoToObject = 9;
+	public static final int wdGoToEquation = 10;
+	public static final int wdGoToHeading = 11;
+	public static final int wdGoToPercent = 12;
+	public static final int wdGoToSpellingError = 13;
+	public static final int wdGoToGrammaticalError = 14;
+	public static final int wdGoToProofreadingError = 15;
+}
