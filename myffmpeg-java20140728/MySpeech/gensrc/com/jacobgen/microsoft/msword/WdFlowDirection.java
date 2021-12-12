759fc6f5026e4971f014403867eca072d4426a40
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:40 +0800

    Create WdFlowDirection.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFlowDirection.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFlowDirection {
+
+	public static final int wdFlowLtr = 0;
+	public static final int wdFlowRtl = 1;
+}
