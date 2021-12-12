c130d5b278b04417c70dc7ed63c95791d595ecf0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:55 +0800

    Create WdExportCreateBookmarks.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdExportCreateBookmarks.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdExportCreateBookmarks {
+
+	public static final int wdExportCreateNoBookmarks = 0;
+	public static final int wdExportCreateHeadingBookmarks = 1;
+	public static final int wdExportCreateWordBookmarks = 2;
+}
