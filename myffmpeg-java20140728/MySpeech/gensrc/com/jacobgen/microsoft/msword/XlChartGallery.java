54e460eb2798822437a5eecf6c9d01dd2882c8ff
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:04 +0800

    Create XlChartGallery.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlChartGallery.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlChartGallery {
+
+	public static final int xlBuiltIn = 21;
+	public static final int xlUserDefined = 22;
+	public static final int xlAnyGallery = 23;
+}
