9d47d7befb51ac9fd4d4513e0f730fa790251f2a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:16 +0800

    Create WdDateLanguage.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDateLanguage.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDateLanguage {
+
+	public static final int wdDateLanguageBidi = 10;
+	public static final int wdDateLanguageLatin = 1033;
+}
