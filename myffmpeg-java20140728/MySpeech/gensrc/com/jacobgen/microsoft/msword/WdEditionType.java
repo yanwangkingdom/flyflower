dbac399ebdb6a2441bb517a019306bbc9d3da049
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:23 +0800

    Create WdEditionType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEditionType.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEditionType {
+
+	public static final int wdPublisher = 0;
+	public static final int wdSubscriber = 1;
+}
