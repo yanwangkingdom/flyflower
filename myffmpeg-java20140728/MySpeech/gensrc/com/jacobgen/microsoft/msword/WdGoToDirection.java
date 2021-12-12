8e412f95437b4afae38e451a2bbe1c2fa1f282a3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:35:32 +0800

    Create WdGoToDirection.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdGoToDirection.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdGoToDirection {
+
+	public static final int wdGoToFirst = 1;
+	public static final int wdGoToLast = -1;
+	public static final int wdGoToNext = 2;
+	public static final int wdGoToRelative = 2;
+	public static final int wdGoToPrevious = 3;
+	public static final int wdGoToAbsolute = 1;
+}
