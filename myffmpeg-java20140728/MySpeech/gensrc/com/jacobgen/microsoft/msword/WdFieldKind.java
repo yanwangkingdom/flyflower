0f8986d92e6749e5ab3e50f28ae97f9aef0895cd
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:34:19 +0800

    Create WdFieldKind.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdFieldKind.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdFieldKind {
+
+	public static final int wdFieldKindNone = 0;
+	public static final int wdFieldKindHot = 1;
+	public static final int wdFieldKindWarm = 2;
+	public static final int wdFieldKindCold = 3;
+}
