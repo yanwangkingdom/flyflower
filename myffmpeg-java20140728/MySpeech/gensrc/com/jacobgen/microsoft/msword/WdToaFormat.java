b21ca54f3c96350e4d6bf9e2a9d4a084e07bacdc
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:56:21 +0800

    Create WdToaFormat.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdToaFormat.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdToaFormat {
+
+	public static final int wdTOATemplate = 0;
+	public static final int wdTOAClassic = 1;
+	public static final int wdTOADistinctive = 2;
+	public static final int wdTOAFormal = 3;
+	public static final int wdTOASimple = 4;
+}
