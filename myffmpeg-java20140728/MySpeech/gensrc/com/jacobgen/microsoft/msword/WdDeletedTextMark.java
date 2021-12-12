4eaa7b24dd983f598a7975c0bf5e8ca73fe3beed
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:33 +0800

    Create WdDeletedTextMark.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDeletedTextMark.java
@@ -0,0 +1,22 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDeletedTextMark {
+
+	public static final int wdDeletedTextMarkHidden = 0;
+	public static final int wdDeletedTextMarkStrikeThrough = 1;
+	public static final int wdDeletedTextMarkCaret = 2;
+	public static final int wdDeletedTextMarkPound = 3;
+	public static final int wdDeletedTextMarkNone = 4;
+	public static final int wdDeletedTextMarkBold = 5;
+	public static final int wdDeletedTextMarkItalic = 6;
+	public static final int wdDeletedTextMarkUnderline = 7;
+	public static final int wdDeletedTextMarkDoubleUnderline = 8;
+	public static final int wdDeletedTextMarkColorOnly = 9;
+	public static final int wdDeletedTextMarkDoubleStrikeThrough = 10;
+}
