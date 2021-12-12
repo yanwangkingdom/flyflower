1b1e14316347c3148a355436a411b61bc72d3711
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:27 +0800

    Create WdEditorType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdEditorType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdEditorType {
+
+	public static final int wdEditorEveryone = -1;
+	public static final int wdEditorOwners = -4;
+	public static final int wdEditorEditors = -5;
+	public static final int wdEditorCurrent = -6;
+}
