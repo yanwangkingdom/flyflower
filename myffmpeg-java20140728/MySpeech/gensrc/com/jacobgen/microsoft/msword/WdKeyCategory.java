5a9c7873b43ddbda11466717efa8da17d6c5b825
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:10 +0800

    Create WdKeyCategory.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdKeyCategory.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdKeyCategory {
+
+	public static final int wdKeyCategoryNil = -1;
+	public static final int wdKeyCategoryDisable = 0;
+	public static final int wdKeyCategoryCommand = 1;
+	public static final int wdKeyCategoryMacro = 2;
+	public static final int wdKeyCategoryFont = 3;
+	public static final int wdKeyCategoryAutoText = 4;
+	public static final int wdKeyCategoryStyle = 5;
+	public static final int wdKeyCategorySymbol = 6;
+	public static final int wdKeyCategoryPrefix = 7;
+}
