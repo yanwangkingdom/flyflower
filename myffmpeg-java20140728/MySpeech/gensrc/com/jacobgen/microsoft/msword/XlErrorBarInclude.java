c87c2f02001421e146fb0444af0825a51833483d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:01:12 +0800

    Create XlErrorBarInclude.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlErrorBarInclude.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlErrorBarInclude {
+
+	public static final int xlErrorBarIncludeBoth = 1;
+	public static final int xlErrorBarIncludeMinusValues = 3;
+	public static final int xlErrorBarIncludeNone = -4142;
+	public static final int xlErrorBarIncludePlusValues = 2;
+}
