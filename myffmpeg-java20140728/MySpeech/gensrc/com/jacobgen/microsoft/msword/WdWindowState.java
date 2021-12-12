471c67507b6ed715736141458e9e9109311fd952
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:25 +0800

    Create WdWindowState.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdWindowState.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdWindowState {
+
+	public static final int wdWindowStateNormal = 0;
+	public static final int wdWindowStateMaximize = 1;
+	public static final int wdWindowStateMinimize = 2;
+}
