bf0f218432a576af1a648c82b92ad7f5db409295
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:01:05 +0800

    Create XlEndStyleCap.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlEndStyleCap.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlEndStyleCap {
+
+	public static final int xlCap = 1;
+	public static final int xlNoCap = 2;
+}
