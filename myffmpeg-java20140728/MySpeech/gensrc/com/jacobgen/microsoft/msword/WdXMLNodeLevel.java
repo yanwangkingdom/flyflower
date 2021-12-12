b94dbc524cb19348e0ac2a682f715927b0e2d89b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:58:01 +0800

    Create WdXMLNodeLevel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdXMLNodeLevel.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdXMLNodeLevel {
+
+	public static final int wdXMLNodeLevelInline = 0;
+	public static final int wdXMLNodeLevelParagraph = 1;
+	public static final int wdXMLNodeLevelRow = 2;
+	public static final int wdXMLNodeLevelCell = 3;
+}
