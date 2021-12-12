77f84b6495151c1cc0f6a7d24d9a1368a1ba3414
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:56 +0800

    Create WdCharacterWidth.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCharacterWidth.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCharacterWidth {
+
+	public static final int wdWidthHalfWidth = 6;
+	public static final int wdWidthFullWidth = 7;
+}
