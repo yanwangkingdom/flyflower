f6f0d40ecc35a653acaa7c3280da2ecb75d2f2b0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:53:33 +0800

    Create WdSpecialPane.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSpecialPane.java
@@ -0,0 +1,32 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSpecialPane {
+
+	public static final int wdPaneNone = 0;
+	public static final int wdPanePrimaryHeader = 1;
+	public static final int wdPaneFirstPageHeader = 2;
+	public static final int wdPaneEvenPagesHeader = 3;
+	public static final int wdPanePrimaryFooter = 4;
+	public static final int wdPaneFirstPageFooter = 5;
+	public static final int wdPaneEvenPagesFooter = 6;
+	public static final int wdPaneFootnotes = 7;
+	public static final int wdPaneEndnotes = 8;
+	public static final int wdPaneFootnoteContinuationNotice = 9;
+	public static final int wdPaneFootnoteContinuationSeparator = 10;
+	public static final int wdPaneFootnoteSeparator = 11;
+	public static final int wdPaneEndnoteContinuationNotice = 12;
+	public static final int wdPaneEndnoteContinuationSeparator = 13;
+	public static final int wdPaneEndnoteSeparator = 14;
+	public static final int wdPaneComments = 15;
+	public static final int wdPaneCurrentPageHeader = 16;
+	public static final int wdPaneCurrentPageFooter = 17;
+	public static final int wdPaneRevisions = 18;
+	public static final int wdPaneRevisionsHoriz = 19;
+	public static final int wdPaneRevisionsVert = 20;
+}
