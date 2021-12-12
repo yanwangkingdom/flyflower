ccfba7cb2ad40d97e4578d3b42a6808c4e717721
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:05 +0800

    Create WdUseFormattingFrom.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdUseFormattingFrom.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdUseFormattingFrom {
+
+	public static final int wdFormattingFromCurrent = 0;
+	public static final int wdFormattingFromSelected = 1;
+	public static final int wdFormattingFromPrompt = 2;
+}
