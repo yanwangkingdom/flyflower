b4390e8c03a69f61e1356578120b21961faed987
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:32:13 +0800

    Create WdCustomLabelPageSize.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdCustomLabelPageSize.java
@@ -0,0 +1,25 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdCustomLabelPageSize {
+
+	public static final int wdCustomLabelLetter = 0;
+	public static final int wdCustomLabelLetterLS = 1;
+	public static final int wdCustomLabelA4 = 2;
+	public static final int wdCustomLabelA4LS = 3;
+	public static final int wdCustomLabelA5 = 4;
+	public static final int wdCustomLabelA5LS = 5;
+	public static final int wdCustomLabelB5 = 6;
+	public static final int wdCustomLabelMini = 7;
+	public static final int wdCustomLabelFanfold = 8;
+	public static final int wdCustomLabelVertHalfSheet = 9;
+	public static final int wdCustomLabelVertHalfSheetLS = 10;
+	public static final int wdCustomLabelHigaki = 11;
+	public static final int wdCustomLabelHigakiLS = 12;
+	public static final int wdCustomLabelB4JIS = 13;
+}
