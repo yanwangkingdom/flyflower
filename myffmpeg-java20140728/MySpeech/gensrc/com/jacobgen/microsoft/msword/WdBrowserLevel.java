645a77814474f2cacebaa6123aa0ff4ddd79755e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:30:02 +0800

    Create WdBrowserLevel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdBrowserLevel.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdBrowserLevel {
+
+	public static final int wdBrowserLevelV4 = 0;
+	public static final int wdBrowserLevelMicrosoftInternetExplorer5 = 1;
+	public static final int wdBrowserLevelMicrosoftInternetExplorer6 = 2;
+}
