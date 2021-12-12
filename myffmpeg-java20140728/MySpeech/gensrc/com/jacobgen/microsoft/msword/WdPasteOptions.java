52795ad6eac9eb9259ffd5845c0bee296ba31a7c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:07 +0800

    Create WdPasteOptions.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPasteOptions.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPasteOptions {
+
+	public static final int wdKeepSourceFormatting = 0;
+	public static final int wdMatchDestinationFormatting = 1;
+	public static final int wdKeepTextOnly = 2;
+	public static final int wdUseDestinationStyles = 3;
+}
