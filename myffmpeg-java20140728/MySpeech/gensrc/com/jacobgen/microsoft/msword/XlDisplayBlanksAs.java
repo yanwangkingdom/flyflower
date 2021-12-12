fec89c4fface7a547f254dac9cf1bf9cd5d4ccdb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 23:00:57 +0800

    Create XlDisplayBlanksAs.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlDisplayBlanksAs.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlDisplayBlanksAs {
+
+	public static final int xlInterpolated = 3;
+	public static final int xlNotPlotted = 1;
+	public static final int xlZero = 2;
+}
