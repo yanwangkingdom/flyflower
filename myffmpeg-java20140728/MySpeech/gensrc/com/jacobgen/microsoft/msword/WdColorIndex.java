86427ab897c7a5a9c3ba6271c7e7096c867afea1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:31:18 +0800

    Create WdColorIndex.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdColorIndex.java
@@ -0,0 +1,30 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdColorIndex {
+
+	public static final int wdAuto = 0;
+	public static final int wdBlack = 1;
+	public static final int wdBlue = 2;
+	public static final int wdTurquoise = 3;
+	public static final int wdBrightGreen = 4;
+	public static final int wdPink = 5;
+	public static final int wdRed = 6;
+	public static final int wdYellow = 7;
+	public static final int wdWhite = 8;
+	public static final int wdDarkBlue = 9;
+	public static final int wdTeal = 10;
+	public static final int wdGreen = 11;
+	public static final int wdViolet = 12;
+	public static final int wdDarkRed = 13;
+	public static final int wdDarkYellow = 14;
+	public static final int wdGray50 = 15;
+	public static final int wdGray25 = 16;
+	public static final int wdByAuthor = -1;
+	public static final int wdNoHighlight = 0;
+}
