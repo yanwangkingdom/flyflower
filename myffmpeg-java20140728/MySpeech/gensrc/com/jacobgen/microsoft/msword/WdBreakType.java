272b8628e45fcd52dabbb5ce45cab2620112df1d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:59 +0800

    Create WdBreakType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdBreakType.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdBreakType {
+
+	public static final int wdSectionBreakNextPage = 2;
+	public static final int wdSectionBreakContinuous = 3;
+	public static final int wdSectionBreakEvenPage = 4;
+	public static final int wdSectionBreakOddPage = 5;
+	public static final int wdLineBreak = 6;
+	public static final int wdPageBreak = 7;
+	public static final int wdColumnBreak = 8;
+	public static final int wdLineBreakClearLeft = 9;
+	public static final int wdLineBreakClearRight = 10;
+	public static final int wdTextWrappingBreak = 11;
+}
