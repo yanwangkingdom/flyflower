8a425fd50cd2ed22eea50523bdfc45a1fe3c5e18
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:52:02 +0800

    Create WdSectionStart.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSectionStart.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSectionStart {
+
+	public static final int wdSectionContinuous = 0;
+	public static final int wdSectionNewColumn = 1;
+	public static final int wdSectionNewPage = 2;
+	public static final int wdSectionEvenPage = 3;
+	public static final int wdSectionOddPage = 4;
+}
