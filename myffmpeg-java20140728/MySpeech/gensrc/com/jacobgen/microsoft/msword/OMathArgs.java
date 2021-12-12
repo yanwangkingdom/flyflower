c9b47b91b7c23e6ea8606c0e758505971d93c342
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:20:15 +0800

    Create OMathArgs.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/OMathArgs.java
@@ -0,0 +1,104 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class OMathArgs extends Dispatch {
+
+	public static final String componentName = "Word.OMathArgs";
+
+	public OMathArgs() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public OMathArgs(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public OMathArgs(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type int
+	 * @return the result is of type OMath
+	 */
+	public OMath item(int index) {
+		return new OMath(Dispatch.call(this, "Item", new Variant(index)).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param beforeArg an input-parameter of type Variant
+	 * @return the result is of type OMath
+	 */
+	public OMath add(Variant beforeArg) {
+		return new OMath(Dispatch.call(this, "Add", beforeArg).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type OMath
+	 */
+	public OMath add() {
+		return new OMath(Dispatch.call(this, "Add").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method and receiving the output-parameter(s).
+	 * @param beforeArg an input-parameter of type Variant
+	 * @return the result is of type OMath
+	 */
+	public OMath add(Variant beforeArg) {
+		OMath result_of_Add = new OMath(Dispatch.call(this, "Add", beforeArg).toDispatch());
+
+
+		return result_of_Add;
+	}
+
+}
