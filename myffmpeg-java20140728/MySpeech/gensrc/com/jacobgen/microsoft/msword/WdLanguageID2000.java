f46f92bc9ff016f99aef76feb7d71cf25e8682fb
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:37:22 +0800

    Create WdLanguageID2000.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdLanguageID2000.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdLanguageID2000 {
+
+	public static final int wdChineseHongKong = 3076;
+	public static final int wdChineseMacao = 5124;
+	public static final int wdEnglishTrinidad = 11273;
+}
