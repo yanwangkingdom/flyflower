e97e69160ebf3f5e626d3f732624abbbe567fb58
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:57:10 +0800

    Create WdVerticalAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdVerticalAlignment.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdVerticalAlignment {
+
+	public static final int wdAlignVerticalTop = 0;
+	public static final int wdAlignVerticalCenter = 1;
+	public static final int wdAlignVerticalJustify = 2;
+	public static final int wdAlignVerticalBottom = 3;
+}
