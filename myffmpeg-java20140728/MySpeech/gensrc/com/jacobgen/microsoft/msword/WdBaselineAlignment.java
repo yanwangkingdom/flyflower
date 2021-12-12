e42dc270dcb465c5f8967779f9fc2209857ff99e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:42 +0800

    Create WdBaselineAlignment.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdBaselineAlignment.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdBaselineAlignment {
+
+	public static final int wdBaselineAlignTop = 0;
+	public static final int wdBaselineAlignCenter = 1;
+	public static final int wdBaselineAlignBaseline = 2;
+	public static final int wdBaselineAlignFarEast50 = 3;
+	public static final int wdBaselineAlignAuto = 4;
+}
