fc2f3258c63f329dd4d1f9f380572c4526578fd5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:58:49 +0800

    Create XlAxisGroup.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlAxisGroup.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlAxisGroup {
+
+	public static final int xlPrimary = 1;
+	public static final int xlSecondary = 2;
+}
