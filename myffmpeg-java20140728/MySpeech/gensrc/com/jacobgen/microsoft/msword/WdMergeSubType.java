88b34650be84025af86683804f1c03ee90785b48
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:39:43 +0800

    Create WdMergeSubType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMergeSubType.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMergeSubType {
+
+	public static final int wdMergeSubTypeOther = 0;
+	public static final int wdMergeSubTypeAccess = 1;
+	public static final int wdMergeSubTypeOAL = 2;
+	public static final int wdMergeSubTypeOLEDBWord = 3;
+	public static final int wdMergeSubTypeWorks = 4;
+	public static final int wdMergeSubTypeOLEDBText = 5;
+	public static final int wdMergeSubTypeOutlook = 6;
+	public static final int wdMergeSubTypeWord = 7;
+	public static final int wdMergeSubTypeWord2000 = 8;
+}
