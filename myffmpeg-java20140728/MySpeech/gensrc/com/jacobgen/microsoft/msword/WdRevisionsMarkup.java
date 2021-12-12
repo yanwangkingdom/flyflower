46d56bbd12f880893837b97e63082556900e404d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:55 +0800

    Create WdRevisionsMarkup.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRevisionsMarkup.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRevisionsMarkup {
+
+	public static final int wdRevisionsMarkupNone = 0;
+	public static final int wdRevisionsMarkupSimple = 1;
+	public static final int wdRevisionsMarkupAll = 2;
+}
