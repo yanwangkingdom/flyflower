6fcc1475854640251928df85f58786707235498f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:38:37 +0800

    Create WdMailMergeComparison.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdMailMergeComparison.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdMailMergeComparison {
+
+	public static final int wdMergeIfEqual = 0;
+	public static final int wdMergeIfNotEqual = 1;
+	public static final int wdMergeIfLessThan = 2;
+	public static final int wdMergeIfGreaterThan = 3;
+	public static final int wdMergeIfLessThanOrEqual = 4;
+	public static final int wdMergeIfGreaterThanOrEqual = 5;
+	public static final int wdMergeIfIsBlank = 6;
+	public static final int wdMergeIfIsNotBlank = 7;
+}
