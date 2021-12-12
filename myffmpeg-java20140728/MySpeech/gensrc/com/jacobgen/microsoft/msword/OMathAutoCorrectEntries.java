1086f88a6c25b5b1fb179736aa7a61907bdc8db7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:20:22 +0800

    Create OMathAutoCorrectEntries.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/OMathAutoCorrectEntries.java
@@ -0,0 +1,93 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class OMathAutoCorrectEntries extends Dispatch {
+
+	public static final String componentName = "Word.OMathAutoCorrectEntries";
+
+	public OMathAutoCorrectEntries() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public OMathAutoCorrectEntries(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public OMathAutoCorrectEntries(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type Variant
+	 * @return the result is of type OMathAutoCorrectEntry
+	 */
+	public OMathAutoCorrectEntry item(Variant index) {
+		return new OMathAutoCorrectEntry(Dispatch.call(this, "Item", index).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @param value an input-parameter of type String
+	 * @return the result is of type OMathAutoCorrectEntry
+	 */
+	public OMathAutoCorrectEntry add(String name, String value) {
+		return new OMathAutoCorrectEntry(Dispatch.call(this, "Add", name, value).toDispatch());
+	}
+
+}
