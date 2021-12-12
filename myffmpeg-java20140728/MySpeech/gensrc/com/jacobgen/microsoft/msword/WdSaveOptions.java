dc63264e7b15b225c634e536f69a5b9b9f3c6b10
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:49:43 +0800

    Create WdSaveOptions.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdSaveOptions.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdSaveOptions {
+
+	public static final int wdDoNotSaveChanges = 0;
+	public static final int wdSaveChanges = -1;
+	public static final int wdPromptToSaveChanges = -2;
+}
