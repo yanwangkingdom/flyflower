7ee88e328135ba788cf301cef19e42e36a93a8e1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:58:12 +0800

    Create WdXMLValidationStatus.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdXMLValidationStatus.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdXMLValidationStatus {
+
+	public static final int wdXMLValidationStatusOK = 0;
+	public static final int wdXMLValidationStatusCustom = -1072898048;
+}
