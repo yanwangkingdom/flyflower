a3544ceaba87f799e17fdb771394c9e5a82eca44
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:33:05 +0800

    Create WdDocumentType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdDocumentType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdDocumentType {
+
+	public static final int wdTypeDocument = 0;
+	public static final int wdTypeTemplate = 1;
+	public static final int wdTypeFrameset = 2;
+}
