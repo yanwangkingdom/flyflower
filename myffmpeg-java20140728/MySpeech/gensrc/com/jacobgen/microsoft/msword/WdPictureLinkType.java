26f8a367825697049c548cc0bb633ba05e2811ae
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:14 +0800

    Create WdPictureLinkType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPictureLinkType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPictureLinkType {
+
+	public static final int wdLinkNone = 0;
+	public static final int wdLinkDataInDoc = 1;
+	public static final int wdLinkDataOnDisk = 2;
+}
