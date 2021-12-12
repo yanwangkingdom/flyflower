c1b8ecc46ae638b7c7539afd3d7e65c1954b3867
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:28:54 +0800

    Create Versions.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/Versions.java
@@ -0,0 +1,123 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class Versions extends Dispatch {
+
+	public static final String componentName = "Word.Versions";
+
+	public Versions() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public Versions(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public Versions(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getAutoVersion() {
+		return Dispatch.get(this, "AutoVersion").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param autoVersion an input-parameter of type int
+	 */
+	public void setAutoVersion(int autoVersion) {
+		Dispatch.put(this, "AutoVersion", new Variant(autoVersion));
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type int
+	 * @return the result is of type Version
+	 */
+	public Version item(int index) {
+		return new Version(Dispatch.call(this, "Item", new Variant(index)).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param comment an input-parameter of type Variant
+	 */
+	public void save(Variant comment) {
+		Dispatch.call(this, "Save", comment);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 */
+	public void save() {
+		Dispatch.call(this, "Save");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method and receiving the output-parameter(s).
+	 * @param comment an input-parameter of type Variant
+	 */
+	public void save(Variant comment) {
+		Dispatch.call(this, "Save", comment);
+
+	}
+
+}
