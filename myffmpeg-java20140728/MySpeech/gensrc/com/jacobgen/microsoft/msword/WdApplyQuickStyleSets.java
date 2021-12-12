9a62ed667687690ec1f261d48fac101fcbe9db22
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:29:18 +0800

    Create WdApplyQuickStyleSets.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdApplyQuickStyleSets.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdApplyQuickStyleSets {
+
+	public static final int wdSessionStartSet = 1;
+	public static final int wdTemplateSet = 2;
+}
