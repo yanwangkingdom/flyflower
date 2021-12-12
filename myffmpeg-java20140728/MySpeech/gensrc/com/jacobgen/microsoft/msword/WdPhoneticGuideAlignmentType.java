93f0db1956529aa8f9f1ed9256b062628803017c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:47:11 +0800

    Create WdPhoneticGuideAlignmentType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdPhoneticGuideAlignmentType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdPhoneticGuideAlignmentType {
+
+	public static final int wdPhoneticGuideAlignmentCenter = 0;
+	public static final int wdPhoneticGuideAlignmentZeroOneZero = 1;
+	public static final int wdPhoneticGuideAlignmentOneTwoOne = 2;
+	public static final int wdPhoneticGuideAlignmentLeft = 3;
+	public static final int wdPhoneticGuideAlignmentRight = 4;
+	public static final int wdPhoneticGuideAlignmentRightVertical = 5;
+}
