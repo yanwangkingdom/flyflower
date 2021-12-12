1490bad1e43a653b105664769162d9119dd502ca
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:19:05 +0800

    Create OLEControl.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/OLEControl.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class OLEControl extends _OLEControl {
+
+	public static final String componentName = "clsid:{000209F2-0000-0000-C000-000000000046}";
+
+	public OLEControl() {
+		super(componentName);
+	}
+
+	public OLEControl(Dispatch d) {
+		super(d);
+	}
+}
