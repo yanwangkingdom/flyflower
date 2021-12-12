d6575063280e767f1041275ae931f0d71d5b0bc6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-12 22:48:29 +0800

    Create WdRemoveDocInfoType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/WdRemoveDocInfoType.java
@@ -0,0 +1,27 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface WdRemoveDocInfoType {
+
+	public static final int wdRDIComments = 1;
+	public static final int wdRDIRevisions = 2;
+	public static final int wdRDIVersions = 3;
+	public static final int wdRDIRemovePersonalInformation = 4;
+	public static final int wdRDIEmailHeader = 5;
+	public static final int wdRDIRoutingSlip = 6;
+	public static final int wdRDISendForReview = 7;
+	public static final int wdRDIDocumentProperties = 8;
+	public static final int wdRDITemplate = 9;
+	public static final int wdRDIDocumentWorkspace = 10;
+	public static final int wdRDIInkAnnotations = 11;
+	public static final int wdRDIDocumentServerProperties = 14;
+	public static final int wdRDIDocumentManagementPolicy = 15;
+	public static final int wdRDIContentType = 16;
+	public static final int wdRDITaskpaneWebExtensions = 17;
+	public static final int wdRDIAll = 99;
+}
