c13458f772a786e66e6a3e6772284fedac328f72
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:22:20 +0800

    Create DISPID_SpeechLexicon.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechLexicon.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechLexicon {
+
+	public static final int DISPID_SLGenerationId = 1;
+	public static final int DISPID_SLGetWords = 2;
+	public static final int DISPID_SLAddPronunciation = 3;
+	public static final int DISPID_SLAddPronunciationByPhoneIds = 4;
+	public static final int DISPID_SLRemovePronunciation = 5;
+	public static final int DISPID_SLRemovePronunciationByPhoneIds = 6;
+	public static final int DISPID_SLGetPronunciations = 7;
+	public static final int DISPID_SLGetGenerationChange = 8;
+}
