96c56d3dd40800721f63548c17a0fa445ee3516a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:34 +0800

    Create XlTickMark.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlTickMark.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlTickMark {
+
+	public static final int xlTickMarkCross = 4;
+	public static final int xlTickMarkInside = 2;
+	public static final int xlTickMarkNone = -4142;
+	public static final int xlTickMarkOutside = 3;
+}
