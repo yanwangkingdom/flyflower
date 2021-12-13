ad9f06cfb154b998c76cc569f6b032841f9c800f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:47:03 +0800

    Create XMLNodes.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XMLNodes.java
@@ -0,0 +1,118 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public class XMLNodes extends Dispatch {
+
+	public static final String componentName = "Word.XMLNodes";
+
+	public XMLNodes() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public XMLNodes(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public XMLNodes(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Application
+	 */
+	public Application getApplication() {
+		return new Application(Dispatch.get(this, "Application").toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCreator() {
+		return Dispatch.get(this, "Creator").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Object
+	 */
+	public Object getParent() {
+		return Dispatch.get(this, "Parent");
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type int
+	 * @return the result is of type XMLNode
+	 */
+	public XMLNode item(int index) {
+		return new XMLNode(Dispatch.call(this, "Item", new Variant(index)).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @param namespace an input-parameter of type String
+	 * @param range an input-parameter of type Variant
+	 * @return the result is of type XMLNode
+	 */
+	public XMLNode add(String name, String namespace, Variant range) {
+		return new XMLNode(Dispatch.call(this, "Add", name, namespace, range).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @param namespace an input-parameter of type String
+	 * @return the result is of type XMLNode
+	 */
+	public XMLNode add(String name, String namespace) {
+		return new XMLNode(Dispatch.call(this, "Add", name, namespace).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method and receiving the output-parameter(s).
+	 * @param name an input-parameter of type String
+	 * @param namespace an input-parameter of type String
+	 * @param range an input-parameter of type Variant
+	 * @return the result is of type XMLNode
+	 */
+	public XMLNode add(String name, String namespace, Variant range) {
+		XMLNode result_of_Add = new XMLNode(Dispatch.call(this, "Add", name, namespace, range).toDispatch());
+
+
+		return result_of_Add;
+	}
+
+}
