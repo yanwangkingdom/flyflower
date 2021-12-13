41988a4fd292660acd0739deabfe6c08db28d8b1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:35:48 +0800

    Create ISpMMSysAudio.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpMMSysAudio.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpMMSysAudio extends ISpAudio {
+
+	public static final String componentName = "SpeechLib.ISpMMSysAudio";
+
+	public ISpMMSysAudio() {
+		super(componentName);
+	}
+
+	public ISpMMSysAudio(Dispatch d) {
+		super(d);
+	}
+}
