0cf248aa6f0dc329039dfe1244840773e5d6fb09
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:47:23 +0800

    Create SpeechStreamSeekPositionType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechStreamSeekPositionType.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechStreamSeekPositionType {
+
+	public static final int SSSPTRelativeToStart = 0;
+	public static final int SSSPTRelativeToCurrentPosition = 1;
+	public static final int SSSPTRelativeToEnd = 2;
+}
