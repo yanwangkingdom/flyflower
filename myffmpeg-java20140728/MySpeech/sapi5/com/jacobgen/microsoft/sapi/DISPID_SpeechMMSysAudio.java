ee2b5967681b532032ea250e54cdc6bb1694dd1b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:22:57 +0800

    Create DISPID_SpeechMMSysAudio.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechMMSysAudio.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechMMSysAudio {
+
+	public static final int DISPID_SMSADeviceId = 300;
+	public static final int DISPID_SMSALineId = 301;
+	public static final int DISPID_SMSAMMHandle = 302;
+}
