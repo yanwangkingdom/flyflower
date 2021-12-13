440cbb8a6d8c98cb4d78a1df65f979356d0f6abd
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:43:18 +0800

    Create XlLegendPosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlLegendPosition.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlLegendPosition {
+
+	public static final int xlLegendPositionBottom = -4107;
+	public static final int xlLegendPositionCorner = 2;
+	public static final int xlLegendPositionLeft = -4131;
+	public static final int xlLegendPositionRight = -4152;
+	public static final int xlLegendPositionTop = -4160;
+	public static final int xlLegendPositionCustom = -4161;
+}
