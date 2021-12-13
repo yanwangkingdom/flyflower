5d7a26498f3958f4d3921c7599d3b59f687ff616
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:37:41 +0800

    Create ISpRecoGrammar2.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpRecoGrammar2.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpRecoGrammar2 extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpRecoGrammar2";
+
+	public ISpRecoGrammar2() {
+		super(componentName);
+	}
+
+	public ISpRecoGrammar2(Dispatch d) {
+		super(d);
+	}
+}
