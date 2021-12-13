3523779267dc5a4ee23a1c0cf828b62f26ef51b3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:44:01 +0800

    Create XlPivotFieldOrientation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlPivotFieldOrientation.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlPivotFieldOrientation {
+
+	public static final int xlColumnField = 2;
+	public static final int xlDataField = 4;
+	public static final int xlHidden = 0;
+	public static final int xlPageField = 3;
+	public static final int xlRowField = 1;
+}
