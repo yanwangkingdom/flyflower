8ab4d365c1e35a5592cbbd646ba646d90b3e6ef4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:36:35 +0800

    Create ISpPhoneticAlphabetSelection.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpPhoneticAlphabetSelection.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpPhoneticAlphabetSelection extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpPhoneticAlphabetSelection";
+
+	public ISpPhoneticAlphabetSelection() {
+		super(componentName);
+	}
+
+	public ISpPhoneticAlphabetSelection(Dispatch d) {
+		super(d);
+	}
+}
