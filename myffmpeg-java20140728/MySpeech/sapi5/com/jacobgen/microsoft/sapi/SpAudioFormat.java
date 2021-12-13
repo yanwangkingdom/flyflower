033710a350022a6e0df8b860b2a6e3e0970d28af
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:39:56 +0800

    Create SpAudioFormat.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpAudioFormat.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpAudioFormat extends ISpeechAudioFormat {
+
+	public static final String componentName = "clsid:{9EF96870-E160-4792-820D-48CF0649E4EC}";
+
+	public SpAudioFormat() {
+		super(componentName);
+	}
+
+	public SpAudioFormat(Dispatch d) {
+		super(d);
+	}
+}
