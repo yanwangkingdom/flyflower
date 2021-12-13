149b7d8c53d3e2d4bc4bb04477189a6321e72792
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:26:47 +0800

    Create DISPID_SpeechRecoResult2.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechRecoResult2.java
@@ -0,0 +1,12 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechRecoResult2 {
+
+	public static final int DISPID_SRRSetTextFeedback = 12;
+}
