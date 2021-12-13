56c4fce5b7f267c4fccd5bb5880c46a8e75dd799
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:43:36 +0800

    Create XlPattern.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlPattern.java
@@ -0,0 +1,33 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlPattern {
+
+	public static final int xlPatternAutomatic = -4105;
+	public static final int xlPatternChecker = 9;
+	public static final int xlPatternCrissCross = 16;
+	public static final int xlPatternDown = -4121;
+	public static final int xlPatternGray16 = 17;
+	public static final int xlPatternGray25 = -4124;
+	public static final int xlPatternGray50 = -4125;
+	public static final int xlPatternGray75 = -4126;
+	public static final int xlPatternGray8 = 18;
+	public static final int xlPatternGrid = 15;
+	public static final int xlPatternHorizontal = -4128;
+	public static final int xlPatternLightDown = 13;
+	public static final int xlPatternLightHorizontal = 11;
+	public static final int xlPatternLightUp = 14;
+	public static final int xlPatternLightVertical = 12;
+	public static final int xlPatternNone = -4142;
+	public static final int xlPatternSemiGray75 = 10;
+	public static final int xlPatternSolid = 1;
+	public static final int xlPatternUp = -4162;
+	public static final int xlPatternVertical = -4166;
+	public static final int xlPatternLinearGradient = 4000;
+	public static final int xlPatternRectangularGradient = 4001;
+}
