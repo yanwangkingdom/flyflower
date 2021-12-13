4c2967a5e86cdce2101a679dca3f7a9c64a92214
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:48 +0800

    Create IInternetSecurityMgrSite.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/IInternetSecurityMgrSite.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class IInternetSecurityMgrSite extends IUnknown {
+
+	public static final String componentName = "SpeechLib.IInternetSecurityMgrSite";
+
+	public IInternetSecurityMgrSite() {
+		super(componentName);
+	}
+
+	public IInternetSecurityMgrSite(Dispatch d) {
+		super(d);
+	}
+}
