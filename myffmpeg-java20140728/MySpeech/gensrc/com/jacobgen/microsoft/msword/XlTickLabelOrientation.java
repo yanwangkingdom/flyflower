b8712a8daffba70c44a73c03515f6c77ba30dd6c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:26 +0800

    Create XlTickLabelOrientation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlTickLabelOrientation.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlTickLabelOrientation {
+
+	public static final int xlTickLabelOrientationAutomatic = -4105;
+	public static final int xlTickLabelOrientationDownward = -4170;
+	public static final int xlTickLabelOrientationHorizontal = -4128;
+	public static final int xlTickLabelOrientationUpward = -4171;
+	public static final int xlTickLabelOrientationVertical = -4166;
+}
