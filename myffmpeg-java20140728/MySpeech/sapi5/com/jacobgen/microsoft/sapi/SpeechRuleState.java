b9f88f77c2b9626562f1140aaf786cdf9679c7c1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:45:31 +0800

    Create SpeechRuleState.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechRuleState.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechRuleState {
+
+	public static final int SGDSInactive = 0;
+	public static final int SGDSActive = 1;
+	public static final int SGDSActiveWithAutoPause = 3;
+	public static final int SGDSActiveUserDelimited = 4;
+}
