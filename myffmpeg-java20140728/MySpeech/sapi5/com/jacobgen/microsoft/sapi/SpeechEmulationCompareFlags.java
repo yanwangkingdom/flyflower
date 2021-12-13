1cb7b8fcc03f243f3777bbc7d75798f9752cd7fe
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:42:52 +0800

    Create SpeechEmulationCompareFlags.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechEmulationCompareFlags.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechEmulationCompareFlags {
+
+	public static final int SECFIgnoreCase = 1;
+	public static final int SECFIgnoreKanaType = 65536;
+	public static final int SECFIgnoreWidth = 131072;
+	public static final int SECFNoSpecialChars = 536870912;
+	public static final int SECFEmulateResult = 1073741824;
+	public static final int SECFDefault = 196609;
+}
