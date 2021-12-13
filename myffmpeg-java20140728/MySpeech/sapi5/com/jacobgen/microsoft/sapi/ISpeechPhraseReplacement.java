d5b5a586eaa62c008e0f1a61a5dbfaab7eac0bda
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:32:29 +0800

    Create ISpeechPhraseReplacement.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpeechPhraseReplacement.java
@@ -0,0 +1,66 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpeechPhraseReplacement extends Dispatch {
+
+	public static final String componentName = "SpeechLib.ISpeechPhraseReplacement";
+
+	public ISpeechPhraseReplacement() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public ISpeechPhraseReplacement(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public ISpeechPhraseReplacement(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getDisplayAttributes() {
+		return Dispatch.get(this, "DisplayAttributes").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type String
+	 */
+	public String getText() {
+		return Dispatch.get(this, "Text").toString();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getFirstElement() {
+		return Dispatch.get(this, "FirstElement").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getNumberOfElements() {
+		return Dispatch.get(this, "NumberOfElements").changeType(Variant.VariantInt).getInt();
+	}
+
+}
