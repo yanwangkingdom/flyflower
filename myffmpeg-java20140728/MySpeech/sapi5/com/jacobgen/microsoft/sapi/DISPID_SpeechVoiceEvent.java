aad5280be3fc58de7c73fea81d2fc467d9d4a5df
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:03 +0800

    Create DISPID_SpeechVoiceEvent.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechVoiceEvent.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechVoiceEvent {
+
+	public static final int DISPID_SVEStreamStart = 1;
+	public static final int DISPID_SVEStreamEnd = 2;
+	public static final int DISPID_SVEVoiceChange = 3;
+	public static final int DISPID_SVEBookmark = 4;
+	public static final int DISPID_SVEWord = 5;
+	public static final int DISPID_SVEPhoneme = 6;
+	public static final int DISPID_SVESentenceBoundary = 7;
+	public static final int DISPID_SVEViseme = 8;
+	public static final int DISPID_SVEAudioLevel = 9;
+	public static final int DISPID_SVEEnginePrivate = 10;
+}
