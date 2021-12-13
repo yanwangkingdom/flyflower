88eb49e0116c7bd8701080da263ae13b4e8ee3f7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:44:53 +0800

    Create SpeechRecognitionType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechRecognitionType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechRecognitionType {
+
+	public static final int SRTStandard = 0;
+	public static final int SRTAutopause = 1;
+	public static final int SRTEmulated = 2;
+	public static final int SRTSMLTimeout = 4;
+	public static final int SRTExtendableParse = 8;
+	public static final int SRTReSent = 16;
+}
