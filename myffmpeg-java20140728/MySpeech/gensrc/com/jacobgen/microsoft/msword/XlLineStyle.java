9f64d1c262fd987ae8155ac10122a36c0f4b9d52
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:43:23 +0800

    Create XlLineStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlLineStyle.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlLineStyle {
+
+	public static final int xlContinuous = 1;
+	public static final int xlDash = -4115;
+	public static final int xlDashDot = 4;
+	public static final int xlDashDotDot = 5;
+	public static final int xlDot = -4118;
+	public static final int xlDouble = -4119;
+	public static final int xlSlantDashDot = 13;
+	public static final int xlLineStyleNone = -4142;
+}
