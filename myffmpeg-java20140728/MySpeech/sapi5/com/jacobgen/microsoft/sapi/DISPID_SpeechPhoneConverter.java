1c0092880349edee658565f8f0fa57da7e96d3b4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:23:24 +0800

    Create DISPID_SpeechPhoneConverter.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhoneConverter.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhoneConverter {
+
+	public static final int DISPID_SPCLangId = 1;
+	public static final int DISPID_SPCPhoneToId = 2;
+	public static final int DISPID_SPCIdToPhone = 3;
+}
