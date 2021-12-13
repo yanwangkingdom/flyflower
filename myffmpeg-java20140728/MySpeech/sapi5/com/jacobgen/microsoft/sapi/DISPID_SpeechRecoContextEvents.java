b8d434f0989309882186c1de0438685740fc9336
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:25:46 +0800

    Create DISPID_SpeechRecoContextEvents.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechRecoContextEvents.java
@@ -0,0 +1,29 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechRecoContextEvents {
+
+	public static final int DISPID_SRCEStartStream = 1;
+	public static final int DISPID_SRCEEndStream = 2;
+	public static final int DISPID_SRCEBookmark = 3;
+	public static final int DISPID_SRCESoundStart = 4;
+	public static final int DISPID_SRCESoundEnd = 5;
+	public static final int DISPID_SRCEPhraseStart = 6;
+	public static final int DISPID_SRCERecognition = 7;
+	public static final int DISPID_SRCEHypothesis = 8;
+	public static final int DISPID_SRCEPropertyNumberChange = 9;
+	public static final int DISPID_SRCEPropertyStringChange = 10;
+	public static final int DISPID_SRCEFalseRecognition = 11;
+	public static final int DISPID_SRCEInterference = 12;
+	public static final int DISPID_SRCERequestUI = 13;
+	public static final int DISPID_SRCERecognizerStateChange = 14;
+	public static final int DISPID_SRCEAdaptation = 15;
+	public static final int DISPID_SRCERecognitionForOtherContext = 16;
+	public static final int DISPID_SRCEAudioLevel = 17;
+	public static final int DISPID_SRCEEnginePrivate = 18;
+}
