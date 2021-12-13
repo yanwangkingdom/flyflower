d33608fc3ea69f704b541521d9b4d76337e176df
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:37:46 +0800

    Create ISpRecoResult.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpRecoResult.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpRecoResult extends ISpPhrase {
+
+	public static final String componentName = "SpeechLib.ISpRecoResult";
+
+	public ISpRecoResult() {
+		super(componentName);
+	}
+
+	public ISpRecoResult(Dispatch d) {
+		super(d);
+	}
+}
