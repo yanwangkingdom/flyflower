fdeb75098baa08b582c67626e250db232c080cfc
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:29:08 +0800

    Create ISpDataKey.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpDataKey.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpDataKey extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpDataKey";
+
+	public ISpDataKey() {
+		super(componentName);
+	}
+
+	public ISpDataKey(Dispatch d) {
+		super(d);
+	}
+}
