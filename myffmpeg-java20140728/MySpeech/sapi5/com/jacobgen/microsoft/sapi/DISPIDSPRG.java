88489c46edd4e0ad3e0c6b745a78b15aca67371f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:24 +0800

    Create DISPIDSPRG.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPIDSPRG.java
@@ -0,0 +1,30 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPIDSPRG {
+
+	public static final int DISPID_SRGId = 1;
+	public static final int DISPID_SRGRecoContext = 2;
+	public static final int DISPID_SRGState = 3;
+	public static final int DISPID_SRGRules = 4;
+	public static final int DISPID_SRGReset = 5;
+	public static final int DISPID_SRGCommit = 6;
+	public static final int DISPID_SRGCmdLoadFromFile = 7;
+	public static final int DISPID_SRGCmdLoadFromObject = 8;
+	public static final int DISPID_SRGCmdLoadFromResource = 9;
+	public static final int DISPID_SRGCmdLoadFromMemory = 10;
+	public static final int DISPID_SRGCmdLoadFromProprietaryGrammar = 11;
+	public static final int DISPID_SRGCmdSetRuleState = 12;
+	public static final int DISPID_SRGCmdSetRuleIdState = 13;
+	public static final int DISPID_SRGDictationLoad = 14;
+	public static final int DISPID_SRGDictationUnload = 15;
+	public static final int DISPID_SRGDictationSetState = 16;
+	public static final int DISPID_SRGSetWordSequenceData = 17;
+	public static final int DISPID_SRGSetTextSelection = 18;
+	public static final int DISPID_SRGIsPronounceable = 19;
+}
