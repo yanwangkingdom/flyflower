f44a58fc3aa3974cebcaacd8d3f344e136586bf1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:12 +0800

    Create XlScaleType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlScaleType.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlScaleType {
+
+	public static final int xlScaleLinear = -4132;
+	public static final int xlScaleLogarithmic = -4133;
+}
