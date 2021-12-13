ec3d963741f24377d82f6b915f547fa1cb0a8f7a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:43:28 +0800

    Create XlMarkerStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlMarkerStyle.java
@@ -0,0 +1,23 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlMarkerStyle {
+
+	public static final int xlMarkerStyleAutomatic = -4105;
+	public static final int xlMarkerStyleCircle = 8;
+	public static final int xlMarkerStyleDash = -4115;
+	public static final int xlMarkerStyleDiamond = 2;
+	public static final int xlMarkerStyleDot = -4118;
+	public static final int xlMarkerStyleNone = -4142;
+	public static final int xlMarkerStylePicture = -4147;
+	public static final int xlMarkerStylePlus = 9;
+	public static final int xlMarkerStyleSquare = 1;
+	public static final int xlMarkerStyleStar = 5;
+	public static final int xlMarkerStyleTriangle = 3;
+	public static final int xlMarkerStyleX = -4168;
+}
