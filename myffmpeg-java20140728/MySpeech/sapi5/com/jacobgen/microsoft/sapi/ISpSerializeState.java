206bf8abae30db2a31d55082575a7d4e41ad73a3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:37:56 +0800

    Create ISpSerializeState.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpSerializeState.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpSerializeState extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpSerializeState";
+
+	public ISpSerializeState() {
+		super(componentName);
+	}
+
+	public ISpSerializeState(Dispatch d) {
+		super(d);
+	}
+}
