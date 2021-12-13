2e58d15a19b7b526ff48e0b4685fe8c9b37e89f2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:43:52 +0800

    Create XlPieSliceIndex.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlPieSliceIndex.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlPieSliceIndex {
+
+	public static final int xlOuterCounterClockwisePoint = 1;
+	public static final int xlOuterCenterPoint = 2;
+	public static final int xlOuterClockwisePoint = 3;
+	public static final int xlMidClockwiseRadiusPoint = 4;
+	public static final int xlCenterPoint = 5;
+	public static final int xlMidCounterClockwiseRadiusPoint = 6;
+	public static final int xlInnerClockwisePoint = 7;
+	public static final int xlInnerCenterPoint = 8;
+	public static final int xlInnerCounterClockwisePoint = 9;
+}
