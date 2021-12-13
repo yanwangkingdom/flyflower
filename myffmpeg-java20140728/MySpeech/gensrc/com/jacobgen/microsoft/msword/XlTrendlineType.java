e2ca66a777a6ad1afb33e25912308b761e6b8464
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:43 +0800

    Create XlTrendlineType.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlTrendlineType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlTrendlineType {
+
+	public static final int xlExponential = 5;
+	public static final int xlLinear = -4132;
+	public static final int xlLogarithmic = -4133;
+	public static final int xlMovingAvg = 6;
+	public static final int xlPolynomial = 3;
+	public static final int xlPower = 4;
+}
