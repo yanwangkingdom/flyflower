3a0bbe16e6b2ac34ca085307f80e14bf7ea1d2ed
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:40:47 +0800

    Create SPCATEGORYTYPE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPCATEGORYTYPE.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPCATEGORYTYPE {
+
+	public static final int SPCT_COMMAND = 0;
+	public static final int SPCT_DICTATION = 1;
+	public static final int SPCT_SLEEP = 2;
+	public static final int SPCT_SUB_COMMAND = 3;
+	public static final int SPCT_SUB_DICTATION = 4;
+}
