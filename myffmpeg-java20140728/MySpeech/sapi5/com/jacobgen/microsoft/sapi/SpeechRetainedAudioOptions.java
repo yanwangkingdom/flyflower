91b7c42f97bbfd7e3f02fa74a910500dde109c53
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:45:09 +0800

    Create SpeechRetainedAudioOptions.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechRetainedAudioOptions.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechRetainedAudioOptions {
+
+	public static final int SRAONone = 0;
+	public static final int SRAORetainAudio = 1;
+}
