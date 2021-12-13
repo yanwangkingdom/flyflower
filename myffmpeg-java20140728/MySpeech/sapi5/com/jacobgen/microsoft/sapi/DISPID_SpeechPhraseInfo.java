71efb677b49918d76258ad7fdf0da587cc2492c3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:24:57 +0800

    Create DISPID_SpeechPhraseInfo.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseInfo.java
@@ -0,0 +1,27 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseInfo {
+
+	public static final int DISPID_SPILanguageId = 1;
+	public static final int DISPID_SPIGrammarId = 2;
+	public static final int DISPID_SPIStartTime = 3;
+	public static final int DISPID_SPIAudioStreamPosition = 4;
+	public static final int DISPID_SPIAudioSizeBytes = 5;
+	public static final int DISPID_SPIRetainedSizeBytes = 6;
+	public static final int DISPID_SPIAudioSizeTime = 7;
+	public static final int DISPID_SPIRule = 8;
+	public static final int DISPID_SPIProperties = 9;
+	public static final int DISPID_SPIElements = 10;
+	public static final int DISPID_SPIReplacements = 11;
+	public static final int DISPID_SPIEngineId = 12;
+	public static final int DISPID_SPIEnginePrivateData = 13;
+	public static final int DISPID_SPISaveToMemory = 14;
+	public static final int DISPID_SPIGetText = 15;
+	public static final int DISPID_SPIGetDisplayAttributes = 16;
+}
