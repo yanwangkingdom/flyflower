173368cfe9bad92051badb371ad172b69f4e95dc
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:36:25 +0800

    Create ISpPhoneConverter.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpPhoneConverter.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpPhoneConverter extends ISpObjectWithToken {
+
+	public static final String componentName = "SpeechLib.ISpPhoneConverter";
+
+	public ISpPhoneConverter() {
+		super(componentName);
+	}
+
+	public ISpPhoneConverter(Dispatch d) {
+		super(d);
+	}
+}
