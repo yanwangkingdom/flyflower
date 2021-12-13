18fc5a8f4268f9312f383b233fbb5c3a9ec3b752
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:44:08 +0800

    Create SpeechGrammarWordType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechGrammarWordType.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechGrammarWordType {
+
+	public static final int SGDisplay = 0;
+	public static final int SGLexical = 1;
+	public static final int SGPronounciation = 2;
+	public static final int SGLexicalNoSpecialChars = 3;
+}
