27ed346d1eb68d4044bc7ef5cc65e71cf14d7b34
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:43 +0800

    Create IInternetSecurityManager.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/IInternetSecurityManager.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class IInternetSecurityManager extends IUnknown {
+
+	public static final String componentName = "SpeechLib.IInternetSecurityManager";
+
+	public IInternetSecurityManager() {
+		super(componentName);
+	}
+
+	public IInternetSecurityManager(Dispatch d) {
+		super(d);
+	}
+}
