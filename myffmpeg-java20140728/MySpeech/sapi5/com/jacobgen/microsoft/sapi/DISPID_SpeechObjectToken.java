40305e82e38ce5b5278dd4b40e221c00f24d9a28
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:23:03 +0800

    Create DISPID_SpeechObjectToken.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechObjectToken.java
@@ -0,0 +1,24 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechObjectToken {
+
+	public static final int DISPID_SOTId = 1;
+	public static final int DISPID_SOTDataKey = 2;
+	public static final int DISPID_SOTCategory = 3;
+	public static final int DISPID_SOTGetDescription = 4;
+	public static final int DISPID_SOTSetId = 5;
+	public static final int DISPID_SOTGetAttribute = 6;
+	public static final int DISPID_SOTCreateInstance = 7;
+	public static final int DISPID_SOTRemove = 8;
+	public static final int DISPID_SOTGetStorageFileName = 9;
+	public static final int DISPID_SOTRemoveStorageFileName = 10;
+	public static final int DISPID_SOTIsUISupported = 11;
+	public static final int DISPID_SOTDisplayUI = 12;
+	public static final int DISPID_SOTMatchesAttributes = 13;
+}
