7d434b852830d07b21cd2bdd91549ba031960c40
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:41:41 +0800

    Create SpeechAudioFormatType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechAudioFormatType.java
@@ -0,0 +1,81 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechAudioFormatType {
+
+	public static final int SAFTDefault = -1;
+	public static final int SAFTNoAssignedFormat = 0;
+	public static final int SAFTText = 1;
+	public static final int SAFTNonStandardFormat = 2;
+	public static final int SAFTExtendedAudioFormat = 3;
+	public static final int SAFT8kHz8BitMono = 4;
+	public static final int SAFT8kHz8BitStereo = 5;
+	public static final int SAFT8kHz16BitMono = 6;
+	public static final int SAFT8kHz16BitStereo = 7;
+	public static final int SAFT11kHz8BitMono = 8;
+	public static final int SAFT11kHz8BitStereo = 9;
+	public static final int SAFT11kHz16BitMono = 10;
+	public static final int SAFT11kHz16BitStereo = 11;
+	public static final int SAFT12kHz8BitMono = 12;
+	public static final int SAFT12kHz8BitStereo = 13;
+	public static final int SAFT12kHz16BitMono = 14;
+	public static final int SAFT12kHz16BitStereo = 15;
+	public static final int SAFT16kHz8BitMono = 16;
+	public static final int SAFT16kHz8BitStereo = 17;
+	public static final int SAFT16kHz16BitMono = 18;
+	public static final int SAFT16kHz16BitStereo = 19;
+	public static final int SAFT22kHz8BitMono = 20;
+	public static final int SAFT22kHz8BitStereo = 21;
+	public static final int SAFT22kHz16BitMono = 22;
+	public static final int SAFT22kHz16BitStereo = 23;
+	public static final int SAFT24kHz8BitMono = 24;
+	public static final int SAFT24kHz8BitStereo = 25;
+	public static final int SAFT24kHz16BitMono = 26;
+	public static final int SAFT24kHz16BitStereo = 27;
+	public static final int SAFT32kHz8BitMono = 28;
+	public static final int SAFT32kHz8BitStereo = 29;
+	public static final int SAFT32kHz16BitMono = 30;
+	public static final int SAFT32kHz16BitStereo = 31;
+	public static final int SAFT44kHz8BitMono = 32;
+	public static final int SAFT44kHz8BitStereo = 33;
+	public static final int SAFT44kHz16BitMono = 34;
+	public static final int SAFT44kHz16BitStereo = 35;
+	public static final int SAFT48kHz8BitMono = 36;
+	public static final int SAFT48kHz8BitStereo = 37;
+	public static final int SAFT48kHz16BitMono = 38;
+	public static final int SAFT48kHz16BitStereo = 39;
+	public static final int SAFTTrueSpeech_8kHz1BitMono = 40;
+	public static final int SAFTCCITT_ALaw_8kHzMono = 41;
+	public static final int SAFTCCITT_ALaw_8kHzStereo = 42;
+	public static final int SAFTCCITT_ALaw_11kHzMono = 43;
+	public static final int SAFTCCITT_ALaw_11kHzStereo = 44;
+	public static final int SAFTCCITT_ALaw_22kHzMono = 45;
+	public static final int SAFTCCITT_ALaw_22kHzStereo = 46;
+	public static final int SAFTCCITT_ALaw_44kHzMono = 47;
+	public static final int SAFTCCITT_ALaw_44kHzStereo = 48;
+	public static final int SAFTCCITT_uLaw_8kHzMono = 49;
+	public static final int SAFTCCITT_uLaw_8kHzStereo = 50;
+	public static final int SAFTCCITT_uLaw_11kHzMono = 51;
+	public static final int SAFTCCITT_uLaw_11kHzStereo = 52;
+	public static final int SAFTCCITT_uLaw_22kHzMono = 53;
+	public static final int SAFTCCITT_uLaw_22kHzStereo = 54;
+	public static final int SAFTCCITT_uLaw_44kHzMono = 55;
+	public static final int SAFTCCITT_uLaw_44kHzStereo = 56;
+	public static final int SAFTADPCM_8kHzMono = 57;
+	public static final int SAFTADPCM_8kHzStereo = 58;
+	public static final int SAFTADPCM_11kHzMono = 59;
+	public static final int SAFTADPCM_11kHzStereo = 60;
+	public static final int SAFTADPCM_22kHzMono = 61;
+	public static final int SAFTADPCM_22kHzStereo = 62;
+	public static final int SAFTADPCM_44kHzMono = 63;
+	public static final int SAFTADPCM_44kHzStereo = 64;
+	public static final int SAFTGSM610_8kHzMono = 65;
+	public static final int SAFTGSM610_11kHzMono = 66;
+	public static final int SAFTGSM610_22kHzMono = 67;
+	public static final int SAFTGSM610_44kHzMono = 68;
+}
