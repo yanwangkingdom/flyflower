8387eb33c71d8d3f32edd19d152401852eeefe12
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:45:04 +0800

    Create SpeechRecognizerState.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechRecognizerState.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechRecognizerState {
+
+	public static final int SRSInactive = 0;
+	public static final int SRSActive = 1;
+	public static final int SRSActiveAlways = 2;
+	public static final int SRSInactiveWithPurge = 3;
+}
