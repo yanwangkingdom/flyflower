ca48d9c2b136b64480a6667b681b74106557323b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:26:23 +0800

    Create DISPID_SpeechRecognizerStatus.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechRecognizerStatus.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechRecognizerStatus {
+
+	public static final int DISPID_SRSAudioStatus = 1;
+	public static final int DISPID_SRSCurrentStreamPosition = 2;
+	public static final int DISPID_SRSCurrentStreamNumber = 3;
+	public static final int DISPID_SRSNumberOfActiveRules = 4;
+	public static final int DISPID_SRSClsidEngine = 5;
+	public static final int DISPID_SRSSupportedLanguages = 6;
+}
