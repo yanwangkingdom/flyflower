1c04b6ea0734f93d81fb2aff04c3c4505f3403b6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:26:15 +0800

    Create DISPID_SpeechRecognizer.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechRecognizer.java
@@ -0,0 +1,31 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechRecognizer {
+
+	public static final int DISPID_SRRecognizer = 1;
+	public static final int DISPID_SRAllowAudioInputFormatChangesOnNextSet = 2;
+	public static final int DISPID_SRAudioInput = 3;
+	public static final int DISPID_SRAudioInputStream = 4;
+	public static final int DISPID_SRIsShared = 5;
+	public static final int DISPID_SRState = 6;
+	public static final int DISPID_SRStatus = 7;
+	public static final int DISPID_SRProfile = 8;
+	public static final int DISPID_SREmulateRecognition = 9;
+	public static final int DISPID_SRCreateRecoContext = 10;
+	public static final int DISPID_SRGetFormat = 11;
+	public static final int DISPID_SRSetPropertyNumber = 12;
+	public static final int DISPID_SRGetPropertyNumber = 13;
+	public static final int DISPID_SRSetPropertyString = 14;
+	public static final int DISPID_SRGetPropertyString = 15;
+	public static final int DISPID_SRIsUISupported = 16;
+	public static final int DISPID_SRDisplayUI = 17;
+	public static final int DISPID_SRGetRecognizers = 18;
+	public static final int DISPID_SVGetAudioInputs = 19;
+	public static final int DISPID_SVGetProfiles = 20;
+}
