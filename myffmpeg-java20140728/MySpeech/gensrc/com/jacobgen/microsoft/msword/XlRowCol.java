ea4c9fc46c613b11ff1f82b534cecdad8b9b05c5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:08 +0800

    Create XlRowCol.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlRowCol.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlRowCol {
+
+	public static final int xlColumns = 2;
+	public static final int xlRows = 1;
+}
