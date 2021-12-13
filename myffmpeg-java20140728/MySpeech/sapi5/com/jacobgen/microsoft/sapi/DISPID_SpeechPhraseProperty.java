5788d76d4b8e9f636927123f0fb0384158bc81ac
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:25:06 +0800

    Create DISPID_SpeechPhraseProperty.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseProperty.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseProperty {
+
+	public static final int DISPID_SPPName = 1;
+	public static final int DISPID_SPPId = 2;
+	public static final int DISPID_SPPValue = 3;
+	public static final int DISPID_SPPFirstElement = 4;
+	public static final int DISPID_SPPNumberOfElements = 5;
+	public static final int DISPID_SPPEngineConfidence = 6;
+	public static final int DISPID_SPPConfidence = 7;
+	public static final int DISPID_SPPParent = 8;
+	public static final int DISPID_SPPChildren = 9;
+}
