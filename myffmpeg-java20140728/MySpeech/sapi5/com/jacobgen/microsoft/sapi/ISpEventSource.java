f2cd018b0511af2e5a7d032cf8b8e3a48fb1377f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:35:32 +0800

    Create ISpEventSource.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpEventSource.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpEventSource extends ISpNotifySource {
+
+	public static final String componentName = "SpeechLib.ISpEventSource";
+
+	public ISpEventSource() {
+		super(componentName);
+	}
+
+	public ISpEventSource(Dispatch d) {
+		super(d);
+	}
+}
