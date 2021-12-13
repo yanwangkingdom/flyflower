c85b429c82dfeb9a6e0e231a721554cdc80adcc5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:39:23 +0800

    Create ISpVoice.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpVoice.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpVoice extends ISpEventSource {
+
+	public static final String componentName = "SpeechLib.ISpVoice";
+
+	public ISpVoice() {
+		super(componentName);
+	}
+
+	public ISpVoice(Dispatch d) {
+		super(d);
+	}
+}
