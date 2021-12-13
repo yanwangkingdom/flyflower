bdd0e4986085c9eda6a8f6fe5b154f29e250bdad
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:40:57 +0800

    Create SPCONTEXTSTATE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPCONTEXTSTATE.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPCONTEXTSTATE {
+
+	public static final int SPCS_DISABLED = 0;
+	public static final int SPCS_ENABLED = 1;
+}
