2a2fede588206ae053d98b4d7fd99c65288bb82e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:42:08 +0800

    Create SpeechDiscardType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechDiscardType.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechDiscardType {
+
+	public static final int SDTProperty = 1;
+	public static final int SDTReplacement = 2;
+	public static final int SDTRule = 4;
+	public static final int SDTDisplayText = 8;
+	public static final int SDTLexicalForm = 16;
+	public static final int SDTPronunciation = 32;
+	public static final int SDTAudio = 64;
+	public static final int SDTAlternates = 128;
+	public static final int SDTAll = 255;
+}
