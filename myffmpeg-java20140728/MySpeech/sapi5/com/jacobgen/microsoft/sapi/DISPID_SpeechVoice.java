42f1299dac29f86c8ad76e06557160f08691c246
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:27:20 +0800

    Create DISPID_SpeechVoice.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechVoice.java
@@ -0,0 +1,33 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechVoice {
+
+	public static final int DISPID_SVStatus = 1;
+	public static final int DISPID_SVVoice = 2;
+	public static final int DISPID_SVAudioOutput = 3;
+	public static final int DISPID_SVAudioOutputStream = 4;
+	public static final int DISPID_SVRate = 5;
+	public static final int DISPID_SVVolume = 6;
+	public static final int DISPID_SVAllowAudioOuputFormatChangesOnNextSet = 7;
+	public static final int DISPID_SVEventInterests = 8;
+	public static final int DISPID_SVPriority = 9;
+	public static final int DISPID_SVAlertBoundary = 10;
+	public static final int DISPID_SVSyncronousSpeakTimeout = 11;
+	public static final int DISPID_SVSpeak = 12;
+	public static final int DISPID_SVSpeakStream = 13;
+	public static final int DISPID_SVPause = 14;
+	public static final int DISPID_SVResume = 15;
+	public static final int DISPID_SVSkip = 16;
+	public static final int DISPID_SVGetVoices = 17;
+	public static final int DISPID_SVGetAudioOutputs = 18;
+	public static final int DISPID_SVWaitUntilDone = 19;
+	public static final int DISPID_SVSpeakCompleteEvent = 20;
+	public static final int DISPID_SVIsUISupported = 21;
+	public static final int DISPID_SVDisplayUI = 22;
+}
