5f1ac0e81e63893d6a99495035cafba6368173a8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:43:02 +0800

    Create SpeechFormatType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechFormatType.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechFormatType {
+
+	public static final int SFTInput = 0;
+	public static final int SFTSREngine = 1;
+}
