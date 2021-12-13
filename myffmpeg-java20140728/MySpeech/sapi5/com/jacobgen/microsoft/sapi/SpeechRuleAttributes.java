ca2daaaa17671173a519a59fcc6b8a3b29b23080
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:45:15 +0800

    Create SpeechRuleAttributes.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechRuleAttributes.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechRuleAttributes {
+
+	public static final int SRATopLevel = 1;
+	public static final int SRADefaultToActive = 2;
+	public static final int SRAExport = 4;
+	public static final int SRAImport = 8;
+	public static final int SRAInterpreter = 16;
+	public static final int SRADynamic = 32;
+	public static final int SRARoot = 64;
+}
