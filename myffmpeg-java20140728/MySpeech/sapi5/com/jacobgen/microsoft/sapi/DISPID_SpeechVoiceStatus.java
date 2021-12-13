85954f7c3f1e2e0fcbe20c5f506fc4fa44de9ab4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:09 +0800

    Create DISPID_SpeechVoiceStatus.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechVoiceStatus.java
@@ -0,0 +1,23 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechVoiceStatus {
+
+	public static final int DISPID_SVSCurrentStreamNumber = 1;
+	public static final int DISPID_SVSLastStreamNumberQueued = 2;
+	public static final int DISPID_SVSLastResult = 3;
+	public static final int DISPID_SVSRunningState = 4;
+	public static final int DISPID_SVSInputWordPosition = 5;
+	public static final int DISPID_SVSInputWordLength = 6;
+	public static final int DISPID_SVSInputSentencePosition = 7;
+	public static final int DISPID_SVSInputSentenceLength = 8;
+	public static final int DISPID_SVSLastBookmark = 9;
+	public static final int DISPID_SVSLastBookmarkId = 10;
+	public static final int DISPID_SVSPhonemeId = 11;
+	public static final int DISPID_SVSVisemeId = 12;
+}
