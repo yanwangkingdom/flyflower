ee6253f28a7750d78f49dc5425a4ffb73c59b415
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:25:01 +0800

    Create DISPID_SpeechPhraseProperties.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseProperties.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseProperties {
+
+	public static final int DISPID_SPPsCount = 1;
+	public static final int DISPID_SPPsItem = 0;
+	public static final int DISPID_SPPs_NewEnum = -4;
+}
