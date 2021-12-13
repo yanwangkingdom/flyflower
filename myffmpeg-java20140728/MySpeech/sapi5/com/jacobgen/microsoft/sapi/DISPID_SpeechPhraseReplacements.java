ea68da7a12c0be91cb995ac7707457ad315d04e3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:25:17 +0800

    Create DISPID_SpeechPhraseReplacements.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseReplacements.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseReplacements {
+
+	public static final int DISPID_SPRsCount = 1;
+	public static final int DISPID_SPRsItem = 0;
+	public static final int DISPID_SPRs_NewEnum = -4;
+}
