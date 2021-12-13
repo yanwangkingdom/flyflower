f842825de97a7ef431010124bf58750649be3d84
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:25:41 +0800

    Create DISPID_SpeechRecoContext.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechRecoContext.java
@@ -0,0 +1,28 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechRecoContext {
+
+	public static final int DISPID_SRCRecognizer = 1;
+	public static final int DISPID_SRCAudioInInterferenceStatus = 2;
+	public static final int DISPID_SRCRequestedUIType = 3;
+	public static final int DISPID_SRCVoice = 4;
+	public static final int DISPID_SRAllowVoiceFormatMatchingOnNextSet = 5;
+	public static final int DISPID_SRCVoicePurgeEvent = 6;
+	public static final int DISPID_SRCEventInterests = 7;
+	public static final int DISPID_SRCCmdMaxAlternates = 8;
+	public static final int DISPID_SRCState = 9;
+	public static final int DISPID_SRCRetainedAudio = 10;
+	public static final int DISPID_SRCRetainedAudioFormat = 11;
+	public static final int DISPID_SRCPause = 12;
+	public static final int DISPID_SRCResume = 13;
+	public static final int DISPID_SRCCreateGrammar = 14;
+	public static final int DISPID_SRCCreateResultFromMemory = 15;
+	public static final int DISPID_SRCBookmark = 16;
+	public static final int DISPID_SRCSetAdaptationData = 17;
+}
