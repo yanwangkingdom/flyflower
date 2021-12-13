90efa622e459bbfd2903541b63191567083174da
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:38:15 +0800

    Create ISpStreamFormatConverter.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpStreamFormatConverter.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpStreamFormatConverter extends ISpStreamFormat {
+
+	public static final String componentName = "SpeechLib.ISpStreamFormatConverter";
+
+	public ISpStreamFormatConverter() {
+		super(componentName);
+	}
+
+	public ISpStreamFormatConverter(Dispatch d) {
+		super(d);
+	}
+}
