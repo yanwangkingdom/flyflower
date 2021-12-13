e25339f70755fd494bc9524ed1d06ac67825c772
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:34 +0800

    Create IEnumSpObjectTokens.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/IEnumSpObjectTokens.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class IEnumSpObjectTokens extends IUnknown {
+
+	public static final String componentName = "SpeechLib.IEnumSpObjectTokens";
+
+	public IEnumSpObjectTokens() {
+		super(componentName);
+	}
+
+	public IEnumSpObjectTokens(Dispatch d) {
+		super(d);
+	}
+}
