7eee40819fc0bd74459afc346a0ca18dbb91bc80
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:23:49 +0800

    Create DISPID_SpeechPhraseElements.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseElements.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseElements {
+
+	public static final int DISPID_SPEsCount = 1;
+	public static final int DISPID_SPEsItem = 0;
+	public static final int DISPID_SPEs_NewEnum = -4;
+}
