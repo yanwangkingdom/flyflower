9b3b7b951db1f2dc2f53d6ccdf7cbec3f67d94df
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:44:02 +0800

    Create SpeechGrammarState.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechGrammarState.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechGrammarState {
+
+	public static final int SGSEnabled = 1;
+	public static final int SGSDisabled = 0;
+	public static final int SGSExclusive = 3;
+}
