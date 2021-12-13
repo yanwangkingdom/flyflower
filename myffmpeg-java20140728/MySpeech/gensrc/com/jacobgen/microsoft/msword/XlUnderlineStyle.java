ee566655f0ee28c39d73169feaf177e317013f24
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:48 +0800

    Create XlUnderlineStyle.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlUnderlineStyle.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlUnderlineStyle {
+
+	public static final int xlUnderlineStyleDouble = -4119;
+	public static final int xlUnderlineStyleDoubleAccounting = 5;
+	public static final int xlUnderlineStyleNone = -4142;
+	public static final int xlUnderlineStyleSingle = 2;
+	public static final int xlUnderlineStyleSingleAccounting = 4;
+}
