37c23881221d68b3b02fdd105deeefdfc2698146
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:44:13 +0800

    Create SpeechInterference.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechInterference.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechInterference {
+
+	public static final int SINone = 0;
+	public static final int SINoise = 1;
+	public static final int SINoSignal = 2;
+	public static final int SITooLoud = 3;
+	public static final int SITooQuiet = 4;
+	public static final int SITooFast = 5;
+	public static final int SITooSlow = 6;
+}
