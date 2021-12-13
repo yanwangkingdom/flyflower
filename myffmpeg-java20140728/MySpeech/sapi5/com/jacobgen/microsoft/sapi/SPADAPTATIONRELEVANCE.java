f5ad598367d72f6039defc6ceea9801895bf81a1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:39:51 +0800

    Create SPADAPTATIONRELEVANCE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPADAPTATIONRELEVANCE.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPADAPTATIONRELEVANCE {
+
+	public static final int SPAR_Unknown = 0;
+	public static final int SPAR_Low = 1;
+	public static final int SPAR_Medium = 2;
+	public static final int SPAR_High = 3;
+}
