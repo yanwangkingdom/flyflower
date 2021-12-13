be7db21abe7070a6467057436e0221855560e418
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:21:26 +0800

    Create DISPID_SpeechAudioStatus.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechAudioStatus.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechAudioStatus {
+
+	public static final int DISPID_SASFreeBufferSpace = 1;
+	public static final int DISPID_SASNonBlockingIO = 2;
+	public static final int DISPID_SASState = 3;
+	public static final int DISPID_SASCurrentSeekPosition = 4;
+	public static final int DISPID_SASCurrentDevicePosition = 5;
+}
