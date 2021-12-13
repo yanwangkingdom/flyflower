fd971b1d6c152ee7fe287b4a5239eda42ea37652
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:39:33 +0800

    Create IStream.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/IStream.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class IStream extends ISequentialStream {
+
+	public static final String componentName = "SpeechLib.IStream";
+
+	public IStream() {
+		super(componentName);
+	}
+
+	public IStream(Dispatch d) {
+		super(d);
+	}
+}
