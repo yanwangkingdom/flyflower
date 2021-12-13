e5e2378c1c225d0357452933623b0348a500b938
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:35:28 +0800

    Create ISpEventSink.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpEventSink.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpEventSink extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpEventSink";
+
+	public ISpEventSink() {
+		super(componentName);
+	}
+
+	public ISpEventSink(Dispatch d) {
+		super(d);
+	}
+}
