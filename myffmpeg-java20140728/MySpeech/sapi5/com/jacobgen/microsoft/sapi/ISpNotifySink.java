860db4dd6e28e1f27c1a33cd09f5799e51c605b4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:35:53 +0800

    Create ISpNotifySink.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpNotifySink.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpNotifySink extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpNotifySink";
+
+	public ISpNotifySink() {
+		super(componentName);
+	}
+
+	public ISpNotifySink(Dispatch d) {
+		super(d);
+	}
+}
