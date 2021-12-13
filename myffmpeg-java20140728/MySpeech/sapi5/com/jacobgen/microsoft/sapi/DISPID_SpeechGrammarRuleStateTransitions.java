c4fea6fe1a8689e027ee1432c8db21f8c0e2d54f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:22:09 +0800

    Create DISPID_SpeechGrammarRuleStateTransitions.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechGrammarRuleStateTransitions.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechGrammarRuleStateTransitions {
+
+	public static final int DISPID_SGRSTsCount = 1;
+	public static final int DISPID_SGRSTsItem = 0;
+	public static final int DISPID_SGRSTs_NewEnum = -4;
+}
