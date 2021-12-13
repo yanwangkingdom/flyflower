5fcd201ee7cf9f4a205d7fab212ad6f915c64173
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:53 +0800

    Create ISequentialStream.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISequentialStream.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISequentialStream extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISequentialStream";
+
+	public ISequentialStream() {
+		super(componentName);
+	}
+
+	public ISequentialStream(Dispatch d) {
+		super(d);
+	}
+}
