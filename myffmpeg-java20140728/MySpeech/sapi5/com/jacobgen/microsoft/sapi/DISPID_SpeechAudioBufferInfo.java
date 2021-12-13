831484554d691b2682d7c3d187be637680afa611
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:20:44 +0800

    Create DISPID_SpeechAudioBufferInfo.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechAudioBufferInfo.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechAudioBufferInfo {
+
+	public static final int DISPID_SABIMinNotification = 1;
+	public static final int DISPID_SABIBufferSize = 2;
+	public static final int DISPID_SABIEventBias = 3;
+}
