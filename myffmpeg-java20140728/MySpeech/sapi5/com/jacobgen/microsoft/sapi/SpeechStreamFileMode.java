c8643c91b05813e2930035d30a90ce87029ab9f9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:45:45 +0800

    Create SpeechStreamFileMode.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechStreamFileMode.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechStreamFileMode {
+
+	public static final int SSFMOpenForRead = 0;
+	public static final int SSFMOpenReadWrite = 1;
+	public static final int SSFMCreate = 2;
+	public static final int SSFMCreateForWrite = 3;
+}
