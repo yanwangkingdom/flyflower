e0dcddfaf075d05268d2eb6b984e2a24ee035075
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:40:04 +0800

    Create SPAUDIOOPTIONS.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPAUDIOOPTIONS.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPAUDIOOPTIONS {
+
+	public static final int SPAO_NONE = 0;
+	public static final int SPAO_RETAIN_AUDIO = 1;
+}
