e9fd1af7c6d56440abb0442c345a13448eff537e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:22:51 +0800

    Create DISPID_SpeechMemoryStream.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechMemoryStream.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechMemoryStream {
+
+	public static final int DISPID_SMSSetData = 100;
+	public static final int DISPID_SMSGetData = 101;
+}
