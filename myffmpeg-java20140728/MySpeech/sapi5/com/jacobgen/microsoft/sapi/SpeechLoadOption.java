5361f7ed944b5f769db5ff50e34c8223c5efdb7a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:44:22 +0800

    Create SpeechLoadOption.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechLoadOption.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechLoadOption {
+
+	public static final int SLOStatic = 0;
+	public static final int SLODynamic = 1;
+}
