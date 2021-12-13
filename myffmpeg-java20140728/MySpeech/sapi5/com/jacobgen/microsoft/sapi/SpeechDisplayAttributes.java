83c34a538a1ccb3ffc77262e51ea465394ed8eb8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:42:13 +0800

    Create SpeechDisplayAttributes.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechDisplayAttributes.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechDisplayAttributes {
+
+	public static final int SDA_No_Trailing_Space = 0;
+	public static final int SDA_One_Trailing_Space = 2;
+	public static final int SDA_Two_Trailing_Spaces = 4;
+	public static final int SDA_Consume_Leading_Spaces = 8;
+}
