af91242cd4bd26a586afa78244d88ded47e6be7e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:17 +0800

    Create XlSeriesNameLevel.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlSeriesNameLevel.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlSeriesNameLevel {
+
+	public static final int xlSeriesNameLevelNone = -3;
+	public static final int xlSeriesNameLevelCustom = -2;
+	public static final int xlSeriesNameLevelAll = -1;
+}
