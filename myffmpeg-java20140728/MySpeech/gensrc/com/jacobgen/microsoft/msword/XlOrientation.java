2314e78e4a3214d54fda663314b19fa6e34a14b1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:43:32 +0800

    Create XlOrientation.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlOrientation.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlOrientation {
+
+	public static final int xlDownward = -4170;
+	public static final int xlHorizontal = -4128;
+	public static final int xlUpward = -4171;
+	public static final int xlVertical = -4166;
+}
