41052aa5552fb21c82a3d7fe4e045f60edba5a66
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:43:57 +0800

    Create SpeechGrammarRuleStateTransitionType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechGrammarRuleStateTransitionType.java
@@ -0,0 +1,17 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechGrammarRuleStateTransitionType {
+
+	public static final int SGRSTTEpsilon = 0;
+	public static final int SGRSTTWord = 1;
+	public static final int SGRSTTRule = 2;
+	public static final int SGRSTTDictation = 3;
+	public static final int SGRSTTWildcard = 4;
+	public static final int SGRSTTTextBuffer = 5;
+}
