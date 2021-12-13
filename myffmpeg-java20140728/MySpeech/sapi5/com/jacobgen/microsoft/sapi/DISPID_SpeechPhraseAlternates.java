2f3001a5bb21659d4bc07a3fc79a94ea97ce4c8b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:23:34 +0800

    Create DISPID_SpeechPhraseAlternates.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseAlternates.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseAlternates {
+
+	public static final int DISPID_SPAsCount = 1;
+	public static final int DISPID_SPAsItem = 0;
+	public static final int DISPID_SPAs_NewEnum = -4;
+}
