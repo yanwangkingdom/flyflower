8b5929c4143ed3e95a6870805116cd80ef30925c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:36:48 +0800

    Create ISpPhraseAlt.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpPhraseAlt.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpPhraseAlt extends ISpPhrase {
+
+	public static final String componentName = "SpeechLib.ISpPhraseAlt";
+
+	public ISpPhraseAlt() {
+		super(componentName);
+	}
+
+	public ISpPhraseAlt(Dispatch d) {
+		super(d);
+	}
+}
