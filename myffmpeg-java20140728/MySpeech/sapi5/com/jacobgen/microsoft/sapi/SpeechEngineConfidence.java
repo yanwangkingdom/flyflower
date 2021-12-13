7bc8ec0ebe8a917d235744c94cbc683456e88d3a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:42:57 +0800

    Create SpeechEngineConfidence.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechEngineConfidence.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechEngineConfidence {
+
+	public static final int SECLowConfidence = -1;
+	public static final int SECNormalConfidence = 0;
+	public static final int SECHighConfidence = 1;
+}
