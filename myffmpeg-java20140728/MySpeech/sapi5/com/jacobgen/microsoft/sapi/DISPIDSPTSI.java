ce05fd76c5aef12bc80e63880ecfc00215637a16
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:29 +0800

    Create DISPIDSPTSI.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPIDSPTSI.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPIDSPTSI {
+
+	public static final int DISPIDSPTSI_ActiveOffset = 1;
+	public static final int DISPIDSPTSI_ActiveLength = 2;
+	public static final int DISPIDSPTSI_SelectionOffset = 3;
+	public static final int DISPIDSPTSI_SelectionLength = 4;
+}
