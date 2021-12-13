e4ffb9d92b21c1ede929909d36afa1f6b71796bd
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:29:03 +0800

    Create ISpAudio.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpAudio.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpAudio extends ISpStreamFormat {
+
+	public static final String componentName = "SpeechLib.ISpAudio";
+
+	public ISpAudio() {
+		super(componentName);
+	}
+
+	public ISpAudio(Dispatch d) {
+		super(d);
+	}
+}
