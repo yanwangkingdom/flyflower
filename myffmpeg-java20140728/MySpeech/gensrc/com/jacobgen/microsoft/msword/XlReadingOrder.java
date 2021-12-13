8533f9931bccd64adb619cb7a48e40b7c157e615
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:44:59 +0800

    Create XlReadingOrder.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlReadingOrder.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlReadingOrder {
+
+	public static final int xlContext = -5002;
+	public static final int xlLTR = -5003;
+	public static final int xlRTL = -5004;
+}
