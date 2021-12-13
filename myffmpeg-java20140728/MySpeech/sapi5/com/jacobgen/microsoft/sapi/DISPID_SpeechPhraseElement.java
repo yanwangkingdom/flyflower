4467de824059f442843890d9933d6c88daa6d086
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:23:43 +0800

    Create DISPID_SpeechPhraseElement.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseElement.java
@@ -0,0 +1,24 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseElement {
+
+	public static final int DISPID_SPEAudioTimeOffset = 1;
+	public static final int DISPID_SPEAudioSizeTime = 2;
+	public static final int DISPID_SPEAudioStreamOffset = 3;
+	public static final int DISPID_SPEAudioSizeBytes = 4;
+	public static final int DISPID_SPERetainedStreamOffset = 5;
+	public static final int DISPID_SPERetainedSizeBytes = 6;
+	public static final int DISPID_SPEDisplayText = 7;
+	public static final int DISPID_SPELexicalForm = 8;
+	public static final int DISPID_SPEPronunciation = 9;
+	public static final int DISPID_SPEDisplayAttributes = 10;
+	public static final int DISPID_SPERequiredConfidence = 11;
+	public static final int DISPID_SPEActualConfidence = 12;
+	public static final int DISPID_SPEEngineConfidence = 13;
+}
