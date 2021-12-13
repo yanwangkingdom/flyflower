5bbb108f7f6f0000d989cd1b75508040c64d3836
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:39 +0800

    Create IEnumString.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/IEnumString.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class IEnumString extends IUnknown {
+
+	public static final String componentName = "SpeechLib.IEnumString";
+
+	public IEnumString() {
+		super(componentName);
+	}
+
+	public IEnumString(Dispatch d) {
+		super(d);
+	}
+}
