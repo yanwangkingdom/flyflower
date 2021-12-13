60119840b0fd905bbbe2aa8d0803ec00a41ebbe1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:38 +0800

    Create XlTimeUnit.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlTimeUnit.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlTimeUnit {
+
+	public static final int xlDays = 0;
+	public static final int xlMonths = 1;
+	public static final int xlYears = 2;
+}
