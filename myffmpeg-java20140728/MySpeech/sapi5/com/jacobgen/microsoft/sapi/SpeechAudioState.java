3bb0086d8886b69b11db3a9511148d8a75dddb05
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:41:51 +0800

    Create SpeechAudioState.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechAudioState.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechAudioState {
+
+	public static final int SASClosed = 0;
+	public static final int SASStop = 1;
+	public static final int SASPause = 2;
+	public static final int SASRun = 3;
+}
