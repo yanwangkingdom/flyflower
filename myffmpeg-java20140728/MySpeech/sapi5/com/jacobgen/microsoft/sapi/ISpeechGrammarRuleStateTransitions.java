dc7f2e62d7f4f69cb5ed63d72cca3f4eb970f91e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:30:18 +0800

    Create ISpeechGrammarRuleStateTransitions.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpeechGrammarRuleStateTransitions.java
@@ -0,0 +1,59 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpeechGrammarRuleStateTransitions extends Dispatch {
+
+	public static final String componentName = "SpeechLib.ISpeechGrammarRuleStateTransitions";
+
+	public ISpeechGrammarRuleStateTransitions() {
+		super(componentName);
+	}
+
+	/**
+	* This constructor is used instead of a case operation to
+	* turn a Dispatch object into a wider object - it must exist
+	* in every wrapper class whose instances may be returned from
+	* method calls wrapped in VT_DISPATCH Variants.
+	*/
+	public ISpeechGrammarRuleStateTransitions(Dispatch d) {
+		// take over the IDispatch pointer
+		m_pDispatch = d.m_pDispatch;
+		// null out the input's pointer
+		d.m_pDispatch = 0;
+	}
+
+	public ISpeechGrammarRuleStateTransitions(String compName) {
+		super(compName);
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type int
+	 */
+	public int getCount() {
+		return Dispatch.get(this, "Count").changeType(Variant.VariantInt).getInt();
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @param index an input-parameter of type int
+	 * @return the result is of type ISpeechGrammarRuleStateTransition
+	 */
+	public ISpeechGrammarRuleStateTransition item(int index) {
+		return new ISpeechGrammarRuleStateTransition(Dispatch.call(this, "Item", new Variant(index)).toDispatch());
+	}
+
+	/**
+	 * Wrapper for calling the ActiveX-Method with input-parameter(s).
+	 * @return the result is of type Variant
+	 */
+	public Variant get_NewEnum() {
+		return Dispatch.get(this, "_NewEnum");
+	}
+
+}
