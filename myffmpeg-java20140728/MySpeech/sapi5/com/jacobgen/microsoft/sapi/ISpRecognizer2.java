ce1df20ce9f73097941a5a593230027cf1f2b4a4
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:37:26 +0800

    Create ISpRecognizer2.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpRecognizer2.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpRecognizer2 extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpRecognizer2";
+
+	public ISpRecognizer2() {
+		super(componentName);
+	}
+
+	public ISpRecognizer2(Dispatch d) {
+		super(d);
+	}
+}
