a4b84d734056953de3f822435d76be3cdaa151fa
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:25:31 +0800

    Create DISPID_SpeechPhraseRules.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechPhraseRules.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechPhraseRules {
+
+	public static final int DISPID_SPRulesCount = 1;
+	public static final int DISPID_SPRulesItem = 0;
+	public static final int DISPID_SPRules_NewEnum = -4;
+}
