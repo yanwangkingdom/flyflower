f55e71c0d89b0549a2db560890f21ac78c66a88c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:42:02 +0800

    Create SpeechDataKeyLocation.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechDataKeyLocation.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechDataKeyLocation {
+
+	public static final int SDKLDefaultLocation = 0;
+	public static final int SDKLCurrentUser = 1;
+	public static final int SDKLLocalMachine = 2;
+	public static final int SDKLCurrentConfig = 5;
+}
