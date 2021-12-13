7f5fa282727e635981194a27f291168a785b855b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:37:12 +0800

    Create ISpRecoContext2.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpRecoContext2.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpRecoContext2 extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpRecoContext2";
+
+	public ISpRecoContext2() {
+		super(componentName);
+	}
+
+	public ISpRecoContext2(Dispatch d) {
+		super(d);
+	}
+}
