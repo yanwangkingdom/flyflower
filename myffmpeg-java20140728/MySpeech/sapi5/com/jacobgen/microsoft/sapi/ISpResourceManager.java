ecb35478b583cae99a345ae880444e05a0933f1f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:37:51 +0800

    Create ISpResourceManager.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpResourceManager.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpResourceManager extends IServiceProvider {
+
+	public static final String componentName = "SpeechLib.ISpResourceManager";
+
+	public ISpResourceManager() {
+		super(componentName);
+	}
+
+	public ISpResourceManager(Dispatch d) {
+		super(d);
+	}
+}
