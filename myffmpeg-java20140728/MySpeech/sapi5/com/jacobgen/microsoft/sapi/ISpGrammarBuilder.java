a7b42b20a1b6d6df486325f8fc558a76b7f8e197
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:35:37 +0800

    Create ISpGrammarBuilder.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpGrammarBuilder.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpGrammarBuilder extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpGrammarBuilder";
+
+	public ISpGrammarBuilder() {
+		super(componentName);
+	}
+
+	public ISpGrammarBuilder(Dispatch d) {
+		super(d);
+	}
+}
