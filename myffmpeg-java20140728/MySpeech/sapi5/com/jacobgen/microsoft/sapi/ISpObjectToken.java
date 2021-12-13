8f535eef4b82663800263a5e813b7002ad2b8117
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:36:08 +0800

    Create ISpObjectToken.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpObjectToken.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpObjectToken extends ISpDataKey {
+
+	public static final String componentName = "SpeechLib.ISpObjectToken";
+
+	public ISpObjectToken() {
+		super(componentName);
+	}
+
+	public ISpObjectToken(Dispatch d) {
+		super(d);
+	}
+}
