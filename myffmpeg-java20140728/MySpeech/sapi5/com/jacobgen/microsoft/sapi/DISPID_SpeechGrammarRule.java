486ce2c88fc33b726a35b0c6c492d9902ecd00c6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:21:49 +0800

    Create DISPID_SpeechGrammarRule.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechGrammarRule.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechGrammarRule {
+
+	public static final int DISPID_SGRAttributes = 1;
+	public static final int DISPID_SGRInitialState = 2;
+	public static final int DISPID_SGRName = 3;
+	public static final int DISPID_SGRId = 4;
+	public static final int DISPID_SGRClear = 5;
+	public static final int DISPID_SGRAddResource = 6;
+	public static final int DISPID_SGRAddState = 7;
+}
