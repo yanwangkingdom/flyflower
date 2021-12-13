164b9b6842e786acff6e3817c29ab25648469ac0
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:36:40 +0800

    Create ISpPhrase.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpPhrase.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpPhrase extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpPhrase";
+
+	public ISpPhrase() {
+		super(componentName);
+	}
+
+	public ISpPhrase(Dispatch d) {
+		super(d);
+	}
+}
