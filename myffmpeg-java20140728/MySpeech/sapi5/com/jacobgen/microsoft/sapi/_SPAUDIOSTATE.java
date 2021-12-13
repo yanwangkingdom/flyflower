f299ab4b75561fa59277f470c63edfb6571159ad
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:20:32 +0800

    Create _SPAUDIOSTATE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/_SPAUDIOSTATE.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface _SPAUDIOSTATE {
+
+	public static final int SPAS_CLOSED = 0;
+	public static final int SPAS_STOP = 1;
+	public static final int SPAS_PAUSE = 2;
+	public static final int SPAS_RUN = 3;
+}
