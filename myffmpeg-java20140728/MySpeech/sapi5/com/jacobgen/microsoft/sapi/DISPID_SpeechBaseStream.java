aaa44b26ceb2ad7dfd3596620f7f690027ce7f60
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:21:31 +0800

    Create DISPID_SpeechBaseStream.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechBaseStream.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechBaseStream {
+
+	public static final int DISPID_SBSFormat = 1;
+	public static final int DISPID_SBSRead = 2;
+	public static final int DISPID_SBSWrite = 3;
+	public static final int DISPID_SBSSeek = 4;
+}
