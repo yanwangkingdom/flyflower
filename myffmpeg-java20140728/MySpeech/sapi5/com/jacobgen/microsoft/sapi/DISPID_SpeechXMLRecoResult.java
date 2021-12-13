1737da76f050161c9d264d4446710de6d0ff6667
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:19 +0800

    Create DISPID_SpeechXMLRecoResult.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechXMLRecoResult.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechXMLRecoResult {
+
+	public static final int DISPID_SRRGetXMLResult = 10;
+	public static final int DISPID_SRRGetXMLErrorInfo = 11;
+}
