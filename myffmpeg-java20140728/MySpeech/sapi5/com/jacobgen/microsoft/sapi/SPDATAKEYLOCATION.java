3dec2f95eeb3571ca563d216737b7e452fe9a9fa
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:41:08 +0800

    Create SPDATAKEYLOCATION.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPDATAKEYLOCATION.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPDATAKEYLOCATION {
+
+	public static final int SPDKL_DefaultLocation = 0;
+	public static final int SPDKL_CurrentUser = 1;
+	public static final int SPDKL_LocalMachine = 2;
+	public static final int SPDKL_CurrentConfig = 5;
+}
