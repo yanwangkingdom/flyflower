8aa35662b088b637015f7656b89c104fdd589361
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:35:58 +0800

    Create ISpNotifySource.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/ISpNotifySource.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class ISpNotifySource extends IUnknown {
+
+	public static final String componentName = "SpeechLib.ISpNotifySource";
+
+	public ISpNotifySource() {
+		super(componentName);
+	}
+
+	public ISpNotifySource(Dispatch d) {
+		super(d);
+	}
+}
