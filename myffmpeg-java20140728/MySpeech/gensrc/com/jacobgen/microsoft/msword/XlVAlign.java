edb6a7442fbb5db69ca3bf9e128480bf53614c5c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:53 +0800

    Create XlVAlign.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlVAlign.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlVAlign {
+
+	public static final int xlVAlignBottom = -4107;
+	public static final int xlVAlignCenter = -4108;
+	public static final int xlVAlignDistributed = -4117;
+	public static final int xlVAlignJustify = -4130;
+	public static final int xlVAlignTop = -4160;
+}
