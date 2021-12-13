d8e5aa0405602efa461350b3061b32e6cd7251b6
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:40:52 +0800

    Create SpCompressedLexicon.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpCompressedLexicon.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpCompressedLexicon extends ISpObjectWithToken {
+
+	public static final String componentName = "clsid:{90903716-2F42-11D3-9C26-00C04F8EF87C}";
+
+	public SpCompressedLexicon() {
+		super(componentName);
+	}
+
+	public SpCompressedLexicon(Dispatch d) {
+		super(d);
+	}
+}
