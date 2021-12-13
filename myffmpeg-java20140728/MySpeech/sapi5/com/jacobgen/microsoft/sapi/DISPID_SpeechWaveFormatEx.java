b79b2305dbe4730bf7ba59538b21010218d89ebe
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:28:14 +0800

    Create DISPID_SpeechWaveFormatEx.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechWaveFormatEx.java
@@ -0,0 +1,18 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechWaveFormatEx {
+
+	public static final int DISPID_SWFEFormatTag = 1;
+	public static final int DISPID_SWFEChannels = 2;
+	public static final int DISPID_SWFESamplesPerSec = 3;
+	public static final int DISPID_SWFEAvgBytesPerSec = 4;
+	public static final int DISPID_SWFEBlockAlign = 5;
+	public static final int DISPID_SWFEBitsPerSample = 6;
+	public static final int DISPID_SWFEExtraData = 7;
+}
