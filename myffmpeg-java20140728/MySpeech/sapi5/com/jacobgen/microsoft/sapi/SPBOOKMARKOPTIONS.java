873bc06c4471e99858afb400fdb6273591752dd3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:40:14 +0800

    Create SPBOOKMARKOPTIONS.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPBOOKMARKOPTIONS.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPBOOKMARKOPTIONS {
+
+	public static final int SPBO_NONE = 0;
+	public static final int SPBO_PAUSE = 1;
+	public static final int SPBO_AHEAD = 2;
+	public static final int SPBO_TIME_UNITS = 4;
+}
