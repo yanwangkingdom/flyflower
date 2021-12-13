e267110a42323206e9dd92cf677364719b7d0369
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 21:45:30 +0800

    Create XlTickLabelPosition.java

---
+++ myffmpeg-java20140728/MySpeech/gensrc/com/jacobgen/microsoft/msword/XlTickLabelPosition.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.msword;
+
+import com.jacob.com.*;
+
+public interface XlTickLabelPosition {
+
+	public static final int xlTickLabelPositionHigh = -4127;
+	public static final int xlTickLabelPositionLow = -4134;
+	public static final int xlTickLabelPositionNextToAxis = 4;
+	public static final int xlTickLabelPositionNone = -4142;
+}
