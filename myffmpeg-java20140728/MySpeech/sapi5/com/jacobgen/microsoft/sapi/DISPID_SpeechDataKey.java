a729e1bdeb123db9d3ecdf9812586eeb352fc0f1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:21:40 +0800

    Create DISPID_SpeechDataKey.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/DISPID_SpeechDataKey.java
@@ -0,0 +1,23 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface DISPID_SpeechDataKey {
+
+	public static final int DISPID_SDKSetBinaryValue = 1;
+	public static final int DISPID_SDKGetBinaryValue = 2;
+	public static final int DISPID_SDKSetStringValue = 3;
+	public static final int DISPID_SDKGetStringValue = 4;
+	public static final int DISPID_SDKSetLongValue = 5;
+	public static final int DISPID_SDKGetlongValue = 6;
+	public static final int DISPID_SDKOpenKey = 7;
+	public static final int DISPID_SDKCreateKey = 8;
+	public static final int DISPID_SDKDeleteKey = 9;
+	public static final int DISPID_SDKDeleteValue = 10;
+	public static final int DISPID_SDKEnumKeys = 11;
+	public static final int DISPID_SDKEnumValues = 12;
+}
