fa40d8078b69c5097ea23f1ff5662ba86bcf33b5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-13 22:40:09 +0800

    Create SPAUDIOSTATE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPAUDIOSTATE.java
@@ -0,0 +1,11 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPAUDIOSTATE extends _SPAUDIOSTATE {
+
+}
