2ffde22abda7905df8f537759a372ccd0333b6b9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:18:31 +0800

    Create SpeechWordPronounceable.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechWordPronounceable.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechWordPronounceable {
+
+	public static final int SWPUnknownWordUnpronounceable = 0;
+	public static final int SWPUnknownWordPronounceable = 1;
+	public static final int SWPKnownWordPronounceable = 2;
+}
