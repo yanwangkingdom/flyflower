ed1d9b9c7e67f1f7ed8d25a17b0bee7dc68835b1
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:52:07 +0800

    Create SpUnCompressedLexicon.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpUnCompressedLexicon.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpUnCompressedLexicon extends ISpPhoneticAlphabetSelection {
+
+	public static final String componentName = "clsid:{C9E37C15-DF92-4727-85D6-72E5EEB6995A}";
+
+	public SpUnCompressedLexicon() {
+		super(componentName);
+	}
+
+	public SpUnCompressedLexicon(Dispatch d) {
+		super(d);
+	}
+}
