7db4d8fdb1d2f5c99cf4210185084d4db3c64434
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:25:26 +0800

    Create SpShortcut.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpShortcut.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpShortcut extends ISpObjectWithToken {
+
+	public static final String componentName = "clsid:{0D722F1A-9FCF-4E62-96D8-6DF8F01A26AA}";
+
+	public SpShortcut() {
+		super(componentName);
+	}
+
+	public SpShortcut(Dispatch d) {
+		super(d);
+	}
+}
