fe790dc4f2bfde9059f390bfc1777f84846a0848
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:25:10 +0800

    Create SPSEMANTICFORMAT.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPSEMANTICFORMAT.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPSEMANTICFORMAT {
+
+	public static final int SPSMF_SAPI_PROPERTIES = 0;
+	public static final int SPSMF_SRGS_SEMANTICINTERPRETATION_MS = 1;
+	public static final int SPSMF_SRGS_SAPIPROPERTIES = 2;
+	public static final int SPSMF_UPS = 4;
+	public static final int SPSMF_SRGS_SEMANTICINTERPRETATION_W3C = 8;
+}
