0c05e87ad4b217ba6dd4ae9c43bf8fba658a6171
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:23:29 +0800

    Create SpMemoryStream.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpMemoryStream.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpMemoryStream extends ISpStream {
+
+	public static final String componentName = "clsid:{5FB7EF7D-DFF4-468A-B6B7-2FCBD188F994}";
+
+	public SpMemoryStream() {
+		super(componentName);
+	}
+
+	public SpMemoryStream(Dispatch d) {
+		super(d);
+	}
+}
