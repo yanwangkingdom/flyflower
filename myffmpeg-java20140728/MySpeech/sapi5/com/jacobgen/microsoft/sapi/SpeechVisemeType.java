93aeae293fe3e871cfe049f131ce8951cfea00aa
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:17:41 +0800

    Create SpeechVisemeType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechVisemeType.java
@@ -0,0 +1,33 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechVisemeType {
+
+	public static final int SVP_0 = 0;
+	public static final int SVP_1 = 1;
+	public static final int SVP_2 = 2;
+	public static final int SVP_3 = 3;
+	public static final int SVP_4 = 4;
+	public static final int SVP_5 = 5;
+	public static final int SVP_6 = 6;
+	public static final int SVP_7 = 7;
+	public static final int SVP_8 = 8;
+	public static final int SVP_9 = 9;
+	public static final int SVP_10 = 10;
+	public static final int SVP_11 = 11;
+	public static final int SVP_12 = 12;
+	public static final int SVP_13 = 13;
+	public static final int SVP_14 = 14;
+	public static final int SVP_15 = 15;
+	public static final int SVP_16 = 16;
+	public static final int SVP_17 = 17;
+	public static final int SVP_18 = 18;
+	public static final int SVP_19 = 19;
+	public static final int SVP_20 = 20;
+	public static final int SVP_21 = 21;
+}
