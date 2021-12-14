be31bc2f423b6293ec485704cfe51ef688d3706e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:19:36 +0800

    Create SPGRAMMARSTATE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPGRAMMARSTATE.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPGRAMMARSTATE {
+
+	public static final int SPGS_DISABLED = 0;
+	public static final int SPGS_ENABLED = 1;
+	public static final int SPGS_EXCLUSIVE = 3;
+}
