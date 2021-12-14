778afa6edf24b8d15068b5faa3e788818e3fb35f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:17:25 +0800

    Create SpeechTokenContext.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechTokenContext.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechTokenContext {
+
+	public static final int STCInprocServer = 1;
+	public static final int STCInprocHandler = 2;
+	public static final int STCLocalServer = 4;
+	public static final int STCRemoteServer = 16;
+	public static final int STCAll = 23;
+}
