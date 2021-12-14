bb08c80ea6e5604f678e8744aedd14b671be93bc
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:53:55 +0800

    Create SPWAVEFORMATTYPE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPWAVEFORMATTYPE.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPWAVEFORMATTYPE {
+
+	public static final int SPWF_INPUT = 0;
+	public static final int SPWF_SRENGINE = 1;
+}
