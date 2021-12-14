4ba1e81f58e5a7068516d2e209ba13135efec18b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:23:54 +0800

    Create SpNotifyTranslator.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpNotifyTranslator.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpNotifyTranslator extends ISpNotifyTranslator {
+
+	public static final String componentName = "clsid:{E2AE5372-5D40-11D2-960E-00C04F8EE628}";
+
+	public SpNotifyTranslator() {
+		super(componentName);
+	}
+
+	public SpNotifyTranslator(Dispatch d) {
+		super(d);
+	}
+}
