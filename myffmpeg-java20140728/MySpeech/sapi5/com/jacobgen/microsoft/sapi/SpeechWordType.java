ecab41e09c4dfec40f9799d8ab8303743d53a4ca
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:18:36 +0800

    Create SpeechWordType.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechWordType.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechWordType {
+
+	public static final int SWTAdded = 1;
+	public static final int SWTDeleted = 2;
+}
