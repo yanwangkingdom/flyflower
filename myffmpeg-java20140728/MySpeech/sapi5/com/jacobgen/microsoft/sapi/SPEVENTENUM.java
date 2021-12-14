85bdad4910680acb028809808bcacea8d28fcf00
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:18:42 +0800

    Create SPEVENTENUM.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPEVENTENUM.java
@@ -0,0 +1,51 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPEVENTENUM {
+
+	public static final int SPEI_UNDEFINED = 0;
+	public static final int SPEI_START_INPUT_STREAM = 1;
+	public static final int SPEI_END_INPUT_STREAM = 2;
+	public static final int SPEI_VOICE_CHANGE = 3;
+	public static final int SPEI_TTS_BOOKMARK = 4;
+	public static final int SPEI_WORD_BOUNDARY = 5;
+	public static final int SPEI_PHONEME = 6;
+	public static final int SPEI_SENTENCE_BOUNDARY = 7;
+	public static final int SPEI_VISEME = 8;
+	public static final int SPEI_TTS_AUDIO_LEVEL = 9;
+	public static final int SPEI_TTS_PRIVATE = 15;
+	public static final int SPEI_MIN_TTS = 1;
+	public static final int SPEI_MAX_TTS = 15;
+	public static final int SPEI_END_SR_STREAM = 34;
+	public static final int SPEI_SOUND_START = 35;
+	public static final int SPEI_SOUND_END = 36;
+	public static final int SPEI_PHRASE_START = 37;
+	public static final int SPEI_RECOGNITION = 38;
+	public static final int SPEI_HYPOTHESIS = 39;
+	public static final int SPEI_SR_BOOKMARK = 40;
+	public static final int SPEI_PROPERTY_NUM_CHANGE = 41;
+	public static final int SPEI_PROPERTY_STRING_CHANGE = 42;
+	public static final int SPEI_FALSE_RECOGNITION = 43;
+	public static final int SPEI_INTERFERENCE = 44;
+	public static final int SPEI_REQUEST_UI = 45;
+	public static final int SPEI_RECO_STATE_CHANGE = 46;
+	public static final int SPEI_ADAPTATION = 47;
+	public static final int SPEI_START_SR_STREAM = 48;
+	public static final int SPEI_RECO_OTHER_CONTEXT = 49;
+	public static final int SPEI_SR_AUDIO_LEVEL = 50;
+	public static final int SPEI_SR_RETAINEDAUDIO = 51;
+	public static final int SPEI_SR_PRIVATE = 52;
+	public static final int SPEI_ACTIVE_CATEGORY_CHANGED = 53;
+	public static final int SPEI_RESERVED5 = 54;
+	public static final int SPEI_RESERVED6 = 55;
+	public static final int SPEI_MIN_SR = 34;
+	public static final int SPEI_MAX_SR = 55;
+	public static final int SPEI_RESERVED1 = 30;
+	public static final int SPEI_RESERVED2 = 33;
+	public static final int SPEI_RESERVED3 = 63;
+}
