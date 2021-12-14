4aa02ae96592205a151fa0ec20e19622a4e0a04b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:21:13 +0800

    Create SpInprocRecognizer.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpInprocRecognizer.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpInprocRecognizer extends ISpSerializeState {
+
+	public static final String componentName = "clsid:{41B89B6B-9399-11D2-9623-00C04F8EE628}";
+
+	public SpInprocRecognizer() {
+		super(componentName);
+	}
+
+	public SpInprocRecognizer(Dispatch d) {
+		super(d);
+	}
+}
