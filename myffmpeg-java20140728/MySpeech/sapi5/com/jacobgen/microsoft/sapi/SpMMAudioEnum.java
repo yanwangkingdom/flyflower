94222a29df41ccd3c2dcc60a2bd0f78fbba90ba3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:23:34 +0800

    Create SpMMAudioEnum.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpMMAudioEnum.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpMMAudioEnum extends IEnumSpObjectTokens {
+
+	public static final String componentName = "clsid:{AB1890A0-E91F-11D2-BB91-00C04F8EE6C0}";
+
+	public SpMMAudioEnum() {
+		super(componentName);
+	}
+
+	public SpMMAudioEnum(Dispatch d) {
+		super(d);
+	}
+}
