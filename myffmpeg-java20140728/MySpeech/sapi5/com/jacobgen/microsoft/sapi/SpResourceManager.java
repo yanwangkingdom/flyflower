152c6201fb0c07e44fa41cb234aba67654346081
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:24:54 +0800

    Create SpResourceManager.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpResourceManager.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpResourceManager extends ISpResourceManager {
+
+	public static final String componentName = "clsid:{96749373-3391-11D2-9EE3-00C04F797396}";
+
+	public SpResourceManager() {
+		super(componentName);
+	}
+
+	public SpResourceManager(Dispatch d) {
+		super(d);
+	}
+}
