15e4a857419d23d284bbc29c7ffd6f4a1dca958e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:22:06 +0800

    Create SPLEXICONTYPE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPLEXICONTYPE.java
@@ -0,0 +1,43 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPLEXICONTYPE {
+
+	public static final int eLEXTYPE_USER = 1;
+	public static final int eLEXTYPE_APP = 2;
+	public static final int eLEXTYPE_VENDORLEXICON = 4;
+	public static final int eLEXTYPE_LETTERTOSOUND = 8;
+	public static final int eLEXTYPE_MORPHOLOGY = 16;
+	public static final int eLEXTYPE_RESERVED4 = 32;
+	public static final int eLEXTYPE_USER_SHORTCUT = 64;
+	public static final int eLEXTYPE_RESERVED6 = 128;
+	public static final int eLEXTYPE_RESERVED7 = 256;
+	public static final int eLEXTYPE_RESERVED8 = 512;
+	public static final int eLEXTYPE_RESERVED9 = 1024;
+	public static final int eLEXTYPE_RESERVED10 = 2048;
+	public static final int eLEXTYPE_PRIVATE1 = 4096;
+	public static final int eLEXTYPE_PRIVATE2 = 8192;
+	public static final int eLEXTYPE_PRIVATE3 = 16384;
+	public static final int eLEXTYPE_PRIVATE4 = 32768;
+	public static final int eLEXTYPE_PRIVATE5 = 65536;
+	public static final int eLEXTYPE_PRIVATE6 = 131072;
+	public static final int eLEXTYPE_PRIVATE7 = 262144;
+	public static final int eLEXTYPE_PRIVATE8 = 524288;
+	public static final int eLEXTYPE_PRIVATE9 = 1048576;
+	public static final int eLEXTYPE_PRIVATE10 = 2097152;
+	public static final int eLEXTYPE_PRIVATE11 = 4194304;
+	public static final int eLEXTYPE_PRIVATE12 = 8388608;
+	public static final int eLEXTYPE_PRIVATE13 = 16777216;
+	public static final int eLEXTYPE_PRIVATE14 = 33554432;
+	public static final int eLEXTYPE_PRIVATE15 = 67108864;
+	public static final int eLEXTYPE_PRIVATE16 = 134217728;
+	public static final int eLEXTYPE_PRIVATE17 = 268435456;
+	public static final int eLEXTYPE_PRIVATE18 = 536870912;
+	public static final int eLEXTYPE_PRIVATE19 = 1073741824;
+	public static final int eLEXTYPE_PRIVATE20 = -2147483648;
+}
