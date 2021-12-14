d897d872a9d59f94e1a9c5d704ce40566711a89a
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:25:16 +0800

    Create SpSharedRecoContext.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpSharedRecoContext.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpSharedRecoContext extends ISpPhoneticAlphabetSelection {
+
+	public static final String componentName = "clsid:{47206204-5ECA-11D2-960F-00C04F8EE628}";
+
+	public SpSharedRecoContext() {
+		super(componentName);
+	}
+
+	public SpSharedRecoContext(Dispatch d) {
+		super(d);
+	}
+}
