f32878c47c37ea510b5f56534933f3ebbf204c4e
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:19:44 +0800

    Create SPGRAMMARWORDTYPE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPGRAMMARWORDTYPE.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPGRAMMARWORDTYPE {
+
+	public static final int SPWT_DISPLAY = 0;
+	public static final int SPWT_LEXICAL = 1;
+	public static final int SPWT_PRONUNCIATION = 2;
+	public static final int SPWT_LEXICAL_NO_SPECIAL_CHARS = 3;
+}
