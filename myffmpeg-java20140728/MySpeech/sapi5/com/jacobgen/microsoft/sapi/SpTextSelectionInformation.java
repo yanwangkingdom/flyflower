6271282dc7359735249080927112b6c91af62b22
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:52:01 +0800

    Create SpTextSelectionInformation.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpTextSelectionInformation.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpTextSelectionInformation extends ISpeechTextSelectionInformation {
+
+	public static final String componentName = "clsid:{0F92030A-CBFD-4AB8-A164-FF5985547FF6}";
+
+	public SpTextSelectionInformation() {
+		super(componentName);
+	}
+
+	public SpTextSelectionInformation(Dispatch d) {
+		super(d);
+	}
+}
