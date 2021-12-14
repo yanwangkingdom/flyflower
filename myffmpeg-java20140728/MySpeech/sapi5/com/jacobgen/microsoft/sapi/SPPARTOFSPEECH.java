1264d3102341cc881464be7ebff6a5ba96c93b41
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:24:21 +0800

    Create SPPARTOFSPEECH.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPPARTOFSPEECH.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPPARTOFSPEECH {
+
+	public static final int SPPS_NotOverriden = -1;
+	public static final int SPPS_Unknown = 0;
+	public static final int SPPS_Noun = 4096;
+	public static final int SPPS_Verb = 8192;
+	public static final int SPPS_Modifier = 12288;
+	public static final int SPPS_Function = 16384;
+	public static final int SPPS_Interjection = 20480;
+	public static final int SPPS_Noncontent = 24576;
+	public static final int SPPS_LMA = 28672;
+	public static final int SPPS_SuppressWord = 61440;
+}
