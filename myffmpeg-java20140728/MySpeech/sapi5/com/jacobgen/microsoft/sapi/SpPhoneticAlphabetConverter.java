b1f3035fed21037ba864f1d40d3467a814b66d63
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:24:31 +0800

    Create SpPhoneticAlphabetConverter.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpPhoneticAlphabetConverter.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpPhoneticAlphabetConverter extends ISpPhoneticAlphabetConverter {
+
+	public static final String componentName = "clsid:{4F414126-DFE3-4629-99EE-797978317EAD}";
+
+	public SpPhoneticAlphabetConverter() {
+		super(componentName);
+	}
+
+	public SpPhoneticAlphabetConverter(Dispatch d) {
+		super(d);
+	}
+}
