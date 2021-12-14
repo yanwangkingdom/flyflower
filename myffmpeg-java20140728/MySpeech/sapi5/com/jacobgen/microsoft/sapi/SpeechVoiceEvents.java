e67fc7d9f57d04fe9cf8de25ceab72859856b44b
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:17:46 +0800

    Create SpeechVoiceEvents.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechVoiceEvents.java
@@ -0,0 +1,22 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechVoiceEvents {
+
+	public static final int SVEStartInputStream = 2;
+	public static final int SVEEndInputStream = 4;
+	public static final int SVEVoiceChange = 8;
+	public static final int SVEBookmark = 16;
+	public static final int SVEWordBoundary = 32;
+	public static final int SVEPhoneme = 64;
+	public static final int SVESentenceBoundary = 128;
+	public static final int SVEViseme = 256;
+	public static final int SVEAudioLevel = 512;
+	public static final int SVEPrivate = 32768;
+	public static final int SVEAllEvents = 33790;
+}
