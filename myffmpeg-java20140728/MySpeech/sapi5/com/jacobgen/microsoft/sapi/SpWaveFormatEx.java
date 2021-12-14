52b26ae5312593ab31ba6791f901238da71e9564
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:52:29 +0800

    Create SpWaveFormatEx.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpWaveFormatEx.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpWaveFormatEx extends ISpeechWaveFormatEx {
+
+	public static final String componentName = "clsid:{C79A574C-63BE-44B9-801F-283F87F898BE}";
+
+	public SpWaveFormatEx() {
+		super(componentName);
+	}
+
+	public SpWaveFormatEx(Dispatch d) {
+		super(d);
+	}
+}
