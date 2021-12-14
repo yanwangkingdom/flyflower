388e1523f294ffe59b018589c7de65c708cce5ae
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:21:36 +0800

    Create SPINTERFERENCE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPINTERFERENCE.java
@@ -0,0 +1,21 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPINTERFERENCE {
+
+	public static final int SPINTERFERENCE_NONE = 0;
+	public static final int SPINTERFERENCE_NOISE = 1;
+	public static final int SPINTERFERENCE_NOSIGNAL = 2;
+	public static final int SPINTERFERENCE_TOOLOUD = 3;
+	public static final int SPINTERFERENCE_TOOQUIET = 4;
+	public static final int SPINTERFERENCE_TOOFAST = 5;
+	public static final int SPINTERFERENCE_TOOSLOW = 6;
+	public static final int SPINTERFERENCE_LATENCY_WARNING = 7;
+	public static final int SPINTERFERENCE_LATENCY_TRUNCATE_BEGIN = 8;
+	public static final int SPINTERFERENCE_LATENCY_TRUNCATE_END = 9;
+}
