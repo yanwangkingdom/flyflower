9973fbfe1389579ea1f10d46b03c00fe1ea80b56
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:18:26 +0800

    Create SpeechVoiceSpeakFlags.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechVoiceSpeakFlags.java
@@ -0,0 +1,26 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechVoiceSpeakFlags {
+
+	public static final int SVSFDefault = 0;
+	public static final int SVSFlagsAsync = 1;
+	public static final int SVSFPurgeBeforeSpeak = 2;
+	public static final int SVSFIsFilename = 4;
+	public static final int SVSFIsXML = 8;
+	public static final int SVSFIsNotXML = 16;
+	public static final int SVSFPersistXML = 32;
+	public static final int SVSFNLPSpeakPunc = 64;
+	public static final int SVSFParseSapi = 128;
+	public static final int SVSFParseSsml = 256;
+	public static final int SVSFParseAutodetect = 0;
+	public static final int SVSFNLPMask = 64;
+	public static final int SVSFParseMask = 384;
+	public static final int SVSFVoiceMask = 511;
+	public static final int SVSFUnusedFlags = -512;
+}
