f9399befccacb811dcfbee424eee837c0997aa8f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:17:51 +0800

    Create SpeechVoicePriority.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechVoicePriority.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechVoicePriority {
+
+	public static final int SVPNormal = 0;
+	public static final int SVPAlert = 1;
+	public static final int SVPOver = 2;
+}
