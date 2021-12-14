3365d182f8e34af6f6e26455114314af28552d3f
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:22:01 +0800

    Create SpLexicon.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpLexicon.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpLexicon extends ISpPhoneticAlphabetSelection {
+
+	public static final String componentName = "clsid:{0655E396-25D0-11D3-9C26-00C04F8EF87C}";
+
+	public SpLexicon() {
+		super(componentName);
+	}
+
+	public SpLexicon(Dispatch d) {
+		super(d);
+	}
+}
