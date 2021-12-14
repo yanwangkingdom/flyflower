af40720ad9d4819a12a68327708b26f89fd81171
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:18:46 +0800

    Create SPFILEMODE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPFILEMODE.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPFILEMODE {
+
+	public static final int SPFM_OPEN_READONLY = 0;
+	public static final int SPFM_OPEN_READWRITE = 1;
+	public static final int SPFM_CREATE = 2;
+	public static final int SPFM_CREATE_ALWAYS = 3;
+	public static final int SPFM_NUM_MODES = 4;
+}
