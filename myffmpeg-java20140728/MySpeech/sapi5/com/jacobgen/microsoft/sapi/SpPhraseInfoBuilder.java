d39ec8321b6c107260fc91a3caac7f4deb5f27e9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:24:37 +0800

    Create SpPhraseInfoBuilder.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpPhraseInfoBuilder.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpPhraseInfoBuilder extends ISpeechPhraseInfoBuilder {
+
+	public static final String componentName = "clsid:{C23FC28D-C55F-4720-8B32-91F73C2BD5D1}";
+
+	public SpPhraseInfoBuilder() {
+		super(componentName);
+	}
+
+	public SpPhraseInfoBuilder(Dispatch d) {
+		super(d);
+	}
+}
