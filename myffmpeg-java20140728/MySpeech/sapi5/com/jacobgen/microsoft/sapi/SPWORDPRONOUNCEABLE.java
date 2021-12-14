3c071807ddcc2e9e7af329e8cbbcf9b6327a637c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:54:12 +0800

    Create SPWORDPRONOUNCEABLE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPWORDPRONOUNCEABLE.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPWORDPRONOUNCEABLE {
+
+	public static final int SPWP_UNKNOWN_WORD_UNPRONOUNCEABLE = 0;
+	public static final int SPWP_UNKNOWN_WORD_PRONOUNCEABLE = 1;
+	public static final int SPWP_KNOWN_WORD_PRONOUNCEABLE = 2;
+}
