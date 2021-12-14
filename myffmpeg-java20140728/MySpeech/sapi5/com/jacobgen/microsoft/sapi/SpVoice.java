6ea7ee916b25d7c0e1fa74114e6bdc846d649e44
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:52:17 +0800

    Create SpVoice.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpVoice.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpVoice extends ISpPhoneticAlphabetSelection {
+
+	public static final String componentName = "clsid:{96749377-3391-11D2-9EE3-00C04F797396}";
+
+	public SpVoice() {
+		super(componentName);
+	}
+
+	public SpVoice(Dispatch d) {
+		super(d);
+	}
+}
