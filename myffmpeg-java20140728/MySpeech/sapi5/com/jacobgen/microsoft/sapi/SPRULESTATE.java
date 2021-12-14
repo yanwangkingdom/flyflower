b7fde80dc123c2657260eacffbcce7247a426f6d
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:25:00 +0800

    Create SPRULESTATE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPRULESTATE.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPRULESTATE {
+
+	public static final int SPRS_INACTIVE = 0;
+	public static final int SPRS_ACTIVE = 1;
+	public static final int SPRS_ACTIVE_WITH_AUTO_PAUSE = 3;
+	public static final int SPRS_ACTIVE_USER_DELIMITED = 4;
+}
