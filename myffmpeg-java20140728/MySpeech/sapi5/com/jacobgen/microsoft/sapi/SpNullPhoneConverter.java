2c96526cebb0f281f5606beda10cefbfacc6c845
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:23:59 +0800

    Create SpNullPhoneConverter.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpNullPhoneConverter.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpNullPhoneConverter extends ISpPhoneConverter {
+
+	public static final String componentName = "clsid:{455F24E9-7396-4A16-9715-7C0FDBE3EFE3}";
+
+	public SpNullPhoneConverter() {
+		super(componentName);
+	}
+
+	public SpNullPhoneConverter(Dispatch d) {
+		super(d);
+	}
+}
