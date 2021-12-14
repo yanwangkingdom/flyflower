f828a15134eb41bdc2b22c8cf2db87e8b90041e3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:54:07 +0800

    Create SPWORDTYPE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPWORDTYPE.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPWORDTYPE {
+
+	public static final int eWORDTYPE_ADDED = 1;
+	public static final int eWORDTYPE_DELETED = 2;
+}
