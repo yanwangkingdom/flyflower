664664da8cca92ce3fd829e387a66a9258c2f85c
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:52:12 +0800

    Create SPVISEMES.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPVISEMES.java
@@ -0,0 +1,33 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPVISEMES {
+
+	public static final int SP_VISEME_0 = 0;
+	public static final int SP_VISEME_1 = 1;
+	public static final int SP_VISEME_2 = 2;
+	public static final int SP_VISEME_3 = 3;
+	public static final int SP_VISEME_4 = 4;
+	public static final int SP_VISEME_5 = 5;
+	public static final int SP_VISEME_6 = 6;
+	public static final int SP_VISEME_7 = 7;
+	public static final int SP_VISEME_8 = 8;
+	public static final int SP_VISEME_9 = 9;
+	public static final int SP_VISEME_10 = 10;
+	public static final int SP_VISEME_11 = 11;
+	public static final int SP_VISEME_12 = 12;
+	public static final int SP_VISEME_13 = 13;
+	public static final int SP_VISEME_14 = 14;
+	public static final int SP_VISEME_15 = 15;
+	public static final int SP_VISEME_16 = 16;
+	public static final int SP_VISEME_17 = 17;
+	public static final int SP_VISEME_18 = 18;
+	public static final int SP_VISEME_19 = 19;
+	public static final int SP_VISEME_20 = 20;
+	public static final int SP_VISEME_21 = 21;
+}
