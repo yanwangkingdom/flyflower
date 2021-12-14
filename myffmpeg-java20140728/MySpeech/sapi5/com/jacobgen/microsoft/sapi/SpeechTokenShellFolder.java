b83130554c8d5bc52c872002b8b7bd5029cf26a7
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:17:30 +0800

    Create SpeechTokenShellFolder.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechTokenShellFolder.java
@@ -0,0 +1,15 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechTokenShellFolder {
+
+	public static final int STSF_AppData = 26;
+	public static final int STSF_LocalAppData = 28;
+	public static final int STSF_CommonAppData = 35;
+	public static final int STSF_FlagCreate = 32768;
+}
