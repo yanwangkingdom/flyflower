a0c06b224e3bf3b87dcfca454153e80b3d052f48
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:19:51 +0800

    Create SpInProcRecoContext.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpInProcRecoContext.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpInProcRecoContext extends ISpPhoneticAlphabetSelection {
+
+	public static final String componentName = "clsid:{73AD6842-ACE0-45E8-A4DD-8795881A2C2A}";
+
+	public SpInProcRecoContext() {
+		super(componentName);
+	}
+
+	public SpInProcRecoContext(Dispatch d) {
+		super(d);
+	}
+}
