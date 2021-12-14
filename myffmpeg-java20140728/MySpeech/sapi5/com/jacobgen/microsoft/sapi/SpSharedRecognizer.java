9fd057d49aa6b33d82b2e6eac115d51d7c1dcca2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:25:21 +0800

    Create SpSharedRecognizer.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpSharedRecognizer.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpSharedRecognizer extends ISpSerializeState {
+
+	public static final String componentName = "clsid:{3BEE4890-4FE9-4A37-8C1E-5E7E12791C1F}";
+
+	public SpSharedRecognizer() {
+		super(componentName);
+	}
+
+	public SpSharedRecognizer(Dispatch d) {
+		super(d);
+	}
+}
