5c07f350f94dcad1db307018babf31aa5a3880f8
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:52:23 +0800

    Create SPVPRIORITY.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPVPRIORITY.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPVPRIORITY {
+
+	public static final int SPVPRI_NORMAL = 0;
+	public static final int SPVPRI_ALERT = 1;
+	public static final int SPVPRI_OVER = 2;
+}
