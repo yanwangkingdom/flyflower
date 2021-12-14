a14f65cc87d80e8fd5a4d66063a01e930e8c1fc9
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:23:50 +0800

    Create SpMMAudioOut.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpMMAudioOut.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpMMAudioOut extends ISpMMSysAudio {
+
+	public static final String componentName = "clsid:{A8C680EB-3D32-11D2-9EE7-00C04F797396}";
+
+	public SpMMAudioOut() {
+		super(componentName);
+	}
+
+	public SpMMAudioOut(Dispatch d) {
+		super(d);
+	}
+}
