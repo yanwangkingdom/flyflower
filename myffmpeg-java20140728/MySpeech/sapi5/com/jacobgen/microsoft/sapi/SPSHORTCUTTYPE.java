e568663296b2ac09ffafda6219a926e364ddd280
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:25:41 +0800

    Create SPSHORTCUTTYPE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPSHORTCUTTYPE.java
@@ -0,0 +1,19 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPSHORTCUTTYPE {
+
+	public static final int SPSHT_NotOverriden = -1;
+	public static final int SPSHT_Unknown = 0;
+	public static final int SPSHT_EMAIL = 4096;
+	public static final int SPSHT_OTHER = 8192;
+	public static final int SPPS_RESERVED1 = 12288;
+	public static final int SPPS_RESERVED2 = 16384;
+	public static final int SPPS_RESERVED3 = 20480;
+	public static final int SPPS_RESERVED4 = 61440;
+}
