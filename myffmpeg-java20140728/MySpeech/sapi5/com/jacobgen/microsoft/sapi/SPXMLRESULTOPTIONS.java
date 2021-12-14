55127b07c0ef5833fa8d3d525d6e676d234e85b5
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 20:54:23 +0800

    Create SPXMLRESULTOPTIONS.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPXMLRESULTOPTIONS.java
@@ -0,0 +1,13 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPXMLRESULTOPTIONS {
+
+	public static final int SPXRO_SML = 0;
+	public static final int SPXRO_Alternates_SML = 1;
+}
