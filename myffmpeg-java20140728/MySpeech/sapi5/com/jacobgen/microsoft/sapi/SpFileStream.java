c5872db524ec0d2b368903a614a2a9f88f34ed95
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:19:24 +0800

    Create SpFileStream.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpFileStream.java
@@ -0,0 +1,20 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public class SpFileStream extends ISpStream {
+
+	public static final String componentName = "clsid:{947812B3-2AE1-4644-BA86-9E90DED7EC91}";
+
+	public SpFileStream() {
+		super(componentName);
+	}
+
+	public SpFileStream(Dispatch d) {
+		super(d);
+	}
+}
