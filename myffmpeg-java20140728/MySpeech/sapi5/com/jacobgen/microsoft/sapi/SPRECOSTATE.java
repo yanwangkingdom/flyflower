67a88722c4d57c41a5118b2af94b1fbd888fdab3
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:24:49 +0800

    Create SPRECOSTATE.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SPRECOSTATE.java
@@ -0,0 +1,16 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SPRECOSTATE {
+
+	public static final int SPRST_INACTIVE = 0;
+	public static final int SPRST_ACTIVE = 1;
+	public static final int SPRST_ACTIVE_ALWAYS = 2;
+	public static final int SPRST_INACTIVE_WITH_PURGE = 3;
+	public static final int SPRST_NUM_STATES = 4;
+}
