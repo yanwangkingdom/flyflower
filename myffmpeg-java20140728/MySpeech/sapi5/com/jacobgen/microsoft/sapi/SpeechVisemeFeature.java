4ca5d449ea975ca1d93a61aa3d3d1b18ac7ca8e2
yanwangkingdom <yanwangkingdom@hotmail.com>
2021-12-14 18:17:35 +0800

    Create SpeechVisemeFeature.java

---
+++ myffmpeg-java20140728/MySpeech/sapi5/com/jacobgen/microsoft/sapi/SpeechVisemeFeature.java
@@ -0,0 +1,14 @@
+/**
+ * JacobGen generated file --- do not edit
+ *
+ * (http://www.sourceforge.net/projects/jacob-project */
+package com.jacobgen.microsoft.sapi;
+
+import com.jacob.com.*;
+
+public interface SpeechVisemeFeature {
+
+	public static final int SVF_None = 0;
+	public static final int SVF_Stressed = 1;
+	public static final int SVF_Emphasis = 2;
+}
